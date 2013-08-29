4d3d8273793dd718264801684d71d257f2469980
Florian Rappl <rappl.florian@gmail.com>
2013-08-30 00:41:57 +0200

    More playground - testing short samples

---
+++ ConsoleInteraction/Samples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AngleSharp;
+
+namespace ConsoleInteraction
+{
+    class Samples
+    {
+        public static void RunAll()
+        {
+            First();
+        }
+
+        static void First() 
+        {
+            var document = DocumentBuilder.Html("<h1>Some example source</h1><p>This is a paragraph element");
+            //Do something with document like the following
+
+            Console.WriteLine("Serializing the (original) document:");
+            Console.WriteLine(document.ToHtml());
+
+            var p = document.CreateElement("p");
+            p.TextContent = "This is another paragraph.";
+
+            Console.WriteLine("Inserting another element in the body ...");
+            document.Body.AppendChild(p);
+
+            Console.WriteLine("Serializing the document again:");
+            Console.WriteLine(document.ToHtml());
+        }
+    }
+}
