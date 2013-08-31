51813496771f23567ac5448eab8e8d8c39998726
Florian Rappl <rappl.florian@gmail.com>
2013-08-31 14:41:51 +0200

    Added tests for color blending

--- UnitTests/Library/Color.cs
+++ UnitTests/Library/Color.cs
@@ -65,5 +65,50 @@ namespace UnitTests
             var result = HtmlColor.FromHex(color);
             Assert.AreEqual(HtmlColor.FromRgb(250, 204, 54), result);
         }
+
+        [TestMethod]
+        public void ColorMixBlackOnWhite50Percent()
+        {
+            var color1 = HtmlColor.Black;
+            var color2 = HtmlColor.White;
+            var mix = HtmlColor.Mix(0.5, color1, color2);
+            Assert.AreEqual(HtmlColor.FromRgb(127, 127, 127), mix);
+        }
+
+        [TestMethod]
+        public void ColorMixRedOnWhite75Percent()
+        {
+            var color1 = HtmlColor.Red;
+            var color2 = HtmlColor.White;
+            var mix = HtmlColor.Mix(0.75, color1, color2);
+            Assert.AreEqual(HtmlColor.FromRgb(255, 63, 63), mix);
+        }
+
+        [TestMethod]
+        public void ColorMixBlueOnWhite10Percent()
+        {
+            var color1 = HtmlColor.Blue;
+            var color2 = HtmlColor.White;
+            var mix = HtmlColor.Mix(0.1, color1, color2);
+            Assert.AreEqual(HtmlColor.FromRgb(229, 229, 255), mix);
+        }
+
+        [TestMethod]
+        public void ColorMixGreenOnRed30Percent()
+        {
+            var color1 = HtmlColor.Green;
+            var color2 = HtmlColor.Red;
+            var mix = HtmlColor.Mix(0.3, color1, color2);
+            Assert.AreEqual(HtmlColor.FromRgb(178, 76, 0), mix);
+        }
+
+        [TestMethod]
+        public void ColorMixWhiteOnBlack20Percent()
+        {
+            var color1 = HtmlColor.White;
+            var color2 = HtmlColor.Black;
+            var mix = HtmlColor.Mix(0.2, color1, color2);
+            Assert.AreEqual(HtmlColor.FromRgb(51, 51, 51), mix);
+        }
     }
 }
--- UnitTests/Mocks/MockRequester.cs
+++ UnitTests/Mocks/MockRequester.cs
@@ -3,11 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UnitTests.Mocks
 {
     public class MockRequester : IHttpRequester
     {
+        public IHttpResponse Request(IHttpRequest request)
+        {
+            return null;
+        }
+
+        public Task<IHttpResponse> RequestAsync(IHttpRequest request)
+        {
+            return null;
+        }
+
+        public Task<IHttpResponse> RequestAsync(IHttpRequest request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+
+        public Dictionary<String, String> DefaultHeaders
+        {
+            get;
+            set;
+        }
+
+        public TimeSpan Timeout
+        {
+            get;
+            set;
+        }
     }
 }
