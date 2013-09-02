ef2397f13581c55641c19942f54328d9129d3265
Florian Rappl <rappl.florian@gmail.com>
2013-09-02 15:26:17 +0200

    More tests for the HTTP requester

---
+++ UnitTests/Helper.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Small (but quite useable) code to enable / disable some
+    /// test(s) depending on the current network status.
+    /// Taken from
+    /// http://stackoverflow.com/questions/520347/c-sharp-how-do-i-check-for-a-network-connection
+    /// </summary>
+    class Helper
+    {
+        /// <summary>
+        /// Indicates whether any network connection is available
+        /// Filter connections below a specified speed, as well as virtual network cards.
+        /// Additionally writes an inconclusive message if no network is available.
+        /// </summary>
+        /// <returns>True if a network connection is available; otherwise false.</returns>
+        public static Boolean IsNetworkAvailable()
+        {
+            if (IsNetworkAvailable(0))
+                return true;
+
+            Assert.Inconclusive("No network has been detected. Test skipped.");
+            return false;
+        }
+
+        /// <summary>
+        /// Indicates whether any network connection is available.
+        /// Filter connections below a specified speed, as well as virtual network cards.
+        /// </summary>
+        /// <param name="minimumSpeed">The minimum speed required. Passing 0 will not filter connection using speed.</param>
+        /// <returns>True if a network connection is available; otherwise false.</returns>
+        public static Boolean IsNetworkAvailable(Int64 minimumSpeed)
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+                return false;
+
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // discard because of standard reasons
+                if ((ni.OperationalStatus != OperationalStatus.Up) ||
+                    (ni.NetworkInterfaceType == NetworkInterfaceType.Loopback) ||
+                    (ni.NetworkInterfaceType == NetworkInterfaceType.Tunnel))
+                    continue;
+
+                // this allow to filter modems, serial, etc.
+                // I use 10000000 as a minimum speed for most cases
+                if (ni.Speed < minimumSpeed)
+                    continue;
+
+                // discard virtual cards (virtual box, virtual pc, etc.)
+                if ((ni.Description.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                    (ni.Name.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0))
+                    continue;
+
+                // discard "Microsoft Loopback Adapter", it will not show as NetworkInterfaceType.Loopback but as Ethernet Card.
+                if (ni.Description.Equals("Microsoft Loopback Adapter", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static Stream StreamFromString(String s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ UnitTests/Library/TestRequester.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngleSharp;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestRequester
+    {
+        [TestCleanup]
+        public void ResetChanges()
+        {
+            Configuration.Reset();
+        }
+
+        [TestMethod]
+        public void DefaultGetDefaultHttpRequester()
+        {
+            var requ = Configuration.GetHttpRequester();
+            Assert.IsNull(requ);
+        }
+
+        [TestMethod]
+        public void UseOnGetDefaultHttpRequester()
+        {
+            Configuration.UseDefaultHttpRequester = true;
+            var requ = Configuration.GetHttpRequester();
+            Assert.IsNotNull(requ);
+            Assert.IsInstanceOfType(requ, typeof(DefaultHttpRequester));
+        }
+
+        [TestMethod]
+        public void UseOffGetDefaultHttpRequester()
+        {
+            Configuration.UseDefaultHttpRequester = false;
+            var requ = Configuration.GetHttpRequester();
+            Assert.IsNull(requ);
+        }
+
+        [TestMethod]
+        public void SimpleHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/robots.txt");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+
+                var content = new StreamReader(response.Content);
+                Assert.AreEqual("User-agent: *\nDisallow: /deny\n", content.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public void StatusCode500OfHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/status/500");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(500, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void StatusCode400OfHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/status/400");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(400, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void StatusCode403OfHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/status/403");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(403, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void StatusCode404OfHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/status/404");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(404, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void SimpleHttpPostRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/post");
+                request.Method = HttpMethod.POST;
+                request.Content = Helper.StreamFromString("Hello world");
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+                
+                var stream = new StreamReader(response.Content);
+                Assert.IsNotNull(stream);
+
+                var content = stream.ReadToEnd();
+                Assert.IsTrue(content.Length > 0);
+                Assert.IsTrue(content.Contains("\"data\": \"Hello world\""));
+            }
+        }
+
+        [TestMethod]
+        public void SimpleHttpPutRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/put");
+                request.Method = HttpMethod.PUT;
+                request.Content = Helper.StreamFromString("PUT THIS THING BACK");
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+
+                var stream = new StreamReader(response.Content);
+                Assert.IsNotNull(stream);
+
+                var content = stream.ReadToEnd();
+                Assert.IsTrue(content.Length > 0);
+                Assert.IsTrue(content.Contains("\"data\": \"PUT THIS THING BACK\""));
+            }
+        }
+
+        [TestMethod]
+        public void SimpleHttpDeleteRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/delete");
+                request.Method = HttpMethod.DELETE;
+                request.Content = Helper.StreamFromString("Should be ignored");
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void MethodNotAllowedOnHttpDelete()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/get");
+                request.Method = HttpMethod.DELETE;
+                request.Content = Helper.StreamFromString("Should be ignored");
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(405, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void MethodNotAllowedOnHttpPut()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/get");
+                request.Method = HttpMethod.PUT;
+                request.Content = Helper.StreamFromString("Should be ignored");
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(405, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void RequestUserAgentString()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/user-agent");
+                request.Method = HttpMethod.GET;
+
+                var response = http.Request(request);
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, (int)response.StatusCode);
+                Assert.IsTrue(response.Content.CanRead);
+                Assert.IsTrue(response.Headers.Count > 0);
+
+                var stream = new StreamReader(response.Content);
+                Assert.IsNotNull(stream);
+
+                var content = stream.ReadToEnd();
+                Assert.IsTrue(content.Length > 0);
+                Assert.AreEqual("{\n  \"user-agent\": \"" + Info.Agent + "\"\n}", content);
+            }
+        }
+
+        [TestMethod]
+        public void AsyncHttpGetRequest()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var http = new DefaultHttpRequester();
+                var request = new DefaultHttpRequest();
+                request.Address = new Uri("http://httpbin.org/robots.txt");
+                request.Method = HttpMethod.GET;
+
+                var response = http.RequestAsync(request);
+                Assert.IsNotNull(response);
+                Assert.IsFalse(response.IsCompleted);
+
+                var result = response.Result;
+
+                Assert.IsTrue(response.IsCompleted);
+                Assert.IsTrue(result.Content.CanRead);
+                Assert.IsTrue(result.Headers.Count > 0);
+
+                var content = new StreamReader(result.Content);
+                Assert.AreEqual("User-agent: *\nDisallow: /deny\n", content.ReadToEnd());
+            }
+        }
+    }
+}
