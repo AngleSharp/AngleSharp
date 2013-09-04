0096158f34fc3ee4c6c8a0d72f5b12b6b6816900
Florian Rappl <rappl.florian@gmail.com>
2013-09-05 01:02:38 +0200

    Version number updated

--- AngleSharp/Properties/AssemblyInfo.cs
+++ AngleSharp/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleToAttribute("UnitTests")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.3.6.*")]
-[assembly: AssemblyFileVersion("0.3.6")]
+[assembly: AssemblyVersion("0.3.7.*")]
+[assembly: AssemblyFileVersion("0.3.7")]
