58025130ac10395dca8f2ef896564a8c743e93c4
Florian Rappl <rappl.florian@gmail.com>
2013-09-07 16:02:21 +0200

    Tests for the stringbuilder pool

---
+++ UnitTests/Library/OptimizationPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngleSharp;
+
+namespace UnitTests.Library
+{
+    [TestClass]
+    public class OptimizationPool
+    {
+        [TestMethod]
+        public void RecycleStringBuilderReused()
+        {
+            var str = "Test";
+            var sb1 = Pool.NewStringBuilder();
+            sb1.Append(str);
+            Assert.AreEqual(str, sb1.ToString());
+            var sb2 = Pool.NewStringBuilder();
+            Assert.AreEqual(String.Empty, sb2.ToString());
+            Assert.ReferenceEquals(sb1, sb2);
+        }
+
+        [TestMethod]
+        public void RecycleStringBuilderGetString()
+        {
+            var str = "Test";
+            var sb1 = Pool.NewStringBuilder();
+            sb1.Append(str);
+            Assert.AreEqual(str, sb1.ToPool());
+            var sb2 = Pool.NewStringBuilder();
+            Assert.AreEqual(String.Empty, sb2.ToPool());
+        }
+    }
+}
