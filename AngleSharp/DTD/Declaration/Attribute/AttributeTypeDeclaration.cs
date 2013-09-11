1376e5a680b13d333c6ed5cedb24a12a4e9b7c2a
Florian Rappl <rappl.florian@gmail.com>
2013-09-11 23:12:53 +0200

    Extracted classes to files

---
+++ AngleSharp/DTD/Declaration/Attribute/AttributeTypeDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using AngleSharp.DOM;
+using System;
+
+namespace AngleSharp.DTD
+{
+    abstract class AttributeTypeDeclaration
+    {
+        public AttributeDeclarationEntry Parent 
+        { 
+            get; 
+            set; 
+        }
+
+        public abstract Boolean Check(Element element);
+    }
+}
---
+++ AngleSharp/DTD/Declaration/Attribute/AttributeValueDeclaration.cs
@@ -0,0 +1,16 @@
+﻿using AngleSharp.DOM;
+using System;
+
+namespace AngleSharp.DTD
+{
+    abstract class AttributeValueDeclaration
+    {
+        public AttributeDeclarationEntry Parent 
+        { 
+            get; 
+            set; 
+        }
+
+        public abstract Boolean Apply(Element element);
+    }
+}
