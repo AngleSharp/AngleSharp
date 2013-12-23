98e02467458181c2609eec3d22eb6c9f537b2b54
Florian Rappl <rappl.florian@gmail.com>
2013-12-23 16:23:34 +0100

    Legacy extensions for convenient programming

---
+++ AngleSharp.Legacy/LegacyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace AngleSharp
+{
+    static class LegacyExtensions
+    {
+        public static Type GetTypeInfo(this Type type)
+        {
+            return type;
+        }
+
+        public static MethodInfo GetRuntimeMethod(this Type type, String name, Type[] parameters)
+        {
+            return type.GetMethod(name, parameters);
+        }
+
+        public static T GetCustomAttribute<T>(this ICustomAttributeProvider provider, Boolean inherit = false) where T : Attribute
+        {
+            var objs = provider.GetCustomAttributes(typeof(T), inherit);
+
+            if (objs.Length > 0)
+                return (T)objs[0];
+
+            return null;
+        }
+
+        public static Delegate CreateDelegate(this MethodInfo method, Type type, Object target)
+        {
+            return Delegate.CreateDelegate(type, target, method);
+        }
+    }
+}
