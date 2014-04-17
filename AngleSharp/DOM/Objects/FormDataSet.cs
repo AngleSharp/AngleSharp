b12e34f61e9653ca38e6bebb336315e3114e9e4e
Florian Rappl <rappl.florian@gmail.com>
2014-04-17 23:41:00 +0200

    Plaintext implementation, and FormDataSetEntry abstracted

--- AngleSharp/DOM/Objects/FormDataSet.cs
+++ AngleSharp/DOM/Objects/FormDataSet.cs
@@ -23,7 +23,7 @@
 
         public FormDataSet()
         {
-            _boundary = String.Concat("<-----?", Guid.NewGuid().ToString(), "?----->");
+            _boundary = Guid.NewGuid().ToString();
             _entries = new List<FormDataSetEntry>();
         }
 
@@ -48,20 +48,28 @@
         /// http://www.w3.org/html/wg/drafts/html/master/forms.html#multipart/form-data-encoding-algorithm
         /// </summary>
         /// <param name="encoding">(Optional) Explicit encoding.</param>
-        /// <returns></returns>
+        /// <returns>A stream containing the body.</returns>
         public Stream AsMultipart(Encoding encoding = null)
         {
             encoding = encoding ?? Encoding.UTF8;
-            var charset = encoding.WebName;
             var ms = new MemoryStream();
+            CheckBoundaries(encoding);
+            ReplaceCharset(encoding);
 
-            foreach (var entry in _entries)
+            using (var tw = new StreamWriter(ms, encoding))
             {
-                if (entry.Name.Equals("_charset_") && entry.Type.Equals("hidden", StringComparison.OrdinalIgnoreCase))
-                    entry.Value = charset;
+                tw.WriteLine();
 
-                //TODO Replace Characters in Name & Value that cannot be expressed by using current encoding with &#...; base-10 unicode point
-                //RFC 2388
+                foreach (var entry in _entries)
+                {
+                    tw.Write("--");
+                    tw.WriteLine(_boundary);
+                    entry.AsMultipart(tw);
+                }
+
+                tw.Write("--");
+                tw.Write(_boundary);
+                tw.Write("--");
             }
 
             ms.Position = 0;
@@ -73,13 +81,27 @@
         /// http://www.w3.org/html/wg/drafts/html/master/forms.html#application/x-www-form-urlencoded-encoding-algorithm
         /// </summary>
         /// <param name="encoding">(Optional) Explicit encoding.</param>
-        /// <returns></returns>
+        /// <returns>A stream containing the body.</returns>
         public Stream AsUrlEncoded(Encoding encoding = null)
         {
             encoding = encoding ?? Encoding.UTF8;
             var charset = encoding.WebName;
             var ms = new MemoryStream();
-            //TODO
+            CheckBoundaries(encoding);
+            ReplaceCharset(encoding);
+
+            using (var tw = new StreamWriter(ms, encoding))
+            {
+                tw.WriteLine();
+
+                foreach (var entry in _entries)
+                {
+                    tw.Write(entry.Name);
+                    tw.Write('=');
+                    entry.AsUrlEncoded(tw);
+                }
+            }
+
             ms.Position = 0;
             return ms;
         }
@@ -89,13 +111,27 @@
         /// http://www.w3.org/html/wg/drafts/html/master/forms.html#text/plain-encoding-algorithm
         /// </summary>
         /// <param name="encoding">(Optional) Explicit encoding.</param>
-        /// <returns></returns>
+        /// <returns>A stream containing the body.</returns>
         public Stream AsPlaintext(Encoding encoding = null)
         {
             encoding = encoding ?? Encoding.UTF8;
             var charset = encoding.WebName;
             var ms = new MemoryStream();
-            //TODO
+            CheckBoundaries(encoding);
+            ReplaceCharset(encoding);
+
+            using (var tw = new StreamWriter(ms, encoding))
+            {
+                tw.WriteLine();
+
+                foreach (var entry in _entries)
+                {
+                    tw.Write(entry.Name);
+                    tw.Write('=');
+                    entry.AsPlaintext(tw);
+                }
+            }
+
             ms.Position = 0;
             return ms;
         }
@@ -108,8 +144,7 @@
                 value = Normalize(value);
             }
 
-            CheckBoundary(value);
-            _entries.Add(new FormDataSetEntry { Name = name, Value = value, Type = type });
+            _entries.Add(new TextDataSetEntry { Name = name, Value = value, Type = type });
         }
 
         public void Append(String name, FileEntry value, String type)
@@ -117,19 +152,40 @@
             if (String.Compare(type, "file", StringComparison.OrdinalIgnoreCase) == 0)
                 name = Normalize(name);
 
-            CheckBoundary(value);
-            _entries.Add(new FormDataSetEntry { Name = name, Value = value, Type = type });
+            _entries.Add(new FileDataSetEntry { Name = name, Value = value, Type = type });
         }
 
         #endregion
 
         #region Helpers
 
-        void CheckBoundary(Object value)
+        void ReplaceCharset(Encoding encoding)
         {
-            //TODO
-            //Check if there is any collision with the boundary string - if there is:
-            //Re-Generate Boundary String until there are no collisions with any value
+            for (int i = 0; i < _entries.Count; i++ )
+            {
+                var entry = _entries[i];
+
+                if (entry.Name.Equals("_charset_") && entry.Type.Equals("hidden", StringComparison.OrdinalIgnoreCase))
+                    _entries[i] = new TextDataSetEntry { Name = entry.Name, Type = entry.Type, Value = encoding.WebName };
+            }
+        }
+
+        void CheckBoundaries(Encoding encoding)
+        {
+            var found = false;
+
+            do
+            {
+                for (int i = 0; i < _entries.Count; i++)
+                {
+                    if (_entries[i].Name.Contains(_boundary))
+                    { }
+                }
+
+                //TODO
+                //Check if there is any collision with the boundary string - if there is:
+                //Re-Generate Boundary String until there are no collisions with any value
+            } while (found);
         }
 
         /// <summary>
@@ -140,7 +196,7 @@
         /// </summary>
         /// <param name="value">The value to normalize.</param>
         /// <returns>The normalized string.</returns>
-        String Normalize(String value)
+        static String Normalize(String value)
         {
             var lines = value.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             return String.Join("\r\n", lines);
@@ -153,7 +209,7 @@
         /// <summary>
         /// Encapsulates the data contained in an entry.
         /// </summary>
-        sealed class FormDataSetEntry
+        abstract class FormDataSetEntry
         {
             /// <summary>
             /// Gets or sets the entry's name.
@@ -174,12 +230,84 @@
             }
 
             /// <summary>
+            /// Replaces characters in names and values that cannot be expressed by using the given
+            /// encoding with &#...; base-10 unicode point.
+            /// </summary>
+            /// <param name="value">The value to sanatize.</param>
+            /// <param name="encoding">The encoding to consider.</param>
+            /// <returns>The sanatized value.</returns>
+            protected static String Sanatize(String value, Encoding encoding)
+            {
+                return value;
+            }
+
+            public abstract void AsMultipart(StreamWriter stream);
+
+            public abstract void AsPlaintext(StreamWriter stream);
+
+            public abstract void AsUrlEncoded(StreamWriter stream);
+        }
+
+        sealed class TextDataSetEntry : FormDataSetEntry
+        {
+            /// <summary>
             /// Gets or sets the entry's value.
             /// </summary>
-            public Object Value
+            public String Value
             {
                 get;
                 set;
+            }
+
+            public override void AsMultipart(StreamWriter stream)
+            {
+                stream.WriteLine(String.Concat("content-disposition: form-data; name=\"", Sanatize(Name, stream.Encoding), "\""));
+                stream.WriteLine();
+                stream.WriteLine(Sanatize(Value, stream.Encoding));
+            }
+
+            public override void AsPlaintext(StreamWriter stream)
+            {
+                stream.WriteLine(Value);
+            }
+
+            public override void AsUrlEncoded(StreamWriter stream)
+            {
+                //TODO
+                throw new NotImplementedException();
+            }
+        }
+
+        sealed class FileDataSetEntry : FormDataSetEntry
+        {
+            /// <summary>
+            /// Gets or sets the entry's value.
+            /// </summary>
+            public FileEntry Value
+            {
+                get;
+                set;
+            }
+
+            public override void AsMultipart(StreamWriter stream)
+            {
+                stream.WriteLine("content-disposition: form-data; name=\"{0}\"; filename=\"{1}\"", Sanatize(Name, stream.Encoding), Value.FileName);
+                stream.WriteLine("content-type: " + Value.Type);
+                stream.WriteLine("content-transfer-encoding: binary");
+                stream.WriteLine();
+                stream.BaseStream.Write(Value.Body, 0, Value.Body.Length);
+                stream.WriteLine();
+            }
+
+            public override void AsPlaintext(StreamWriter stream)
+            {
+                stream.WriteLine(Value.FileName);
+            }
+
+            public override void AsUrlEncoded(StreamWriter stream)
+            {
+                //TODO
+                throw new NotImplementedException();
             }
         }
 
