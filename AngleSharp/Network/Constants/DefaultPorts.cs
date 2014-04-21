f1a2304f6e8693d14e583656ac8aa9dd7d03e83e
Florian Rappl <rappl.florian@gmail.com>
2014-04-21 18:05:35 +0200

    Added a collection of default ports

---
+++ AngleSharp/Network/Constants/DefaultPorts.cs
@@ -0,0 +1,45 @@
+﻿namespace AngleSharp
+{
+    using System;
+
+    /// <summary>
+    /// Contains a collection of default ports.
+    /// </summary>
+    static class DefaultPorts
+    {
+        /// <summary>
+        /// Gets the default port for the file transfer protocol.
+        /// </summary>
+        public static readonly String FtpPort = "21";
+
+        /// <summary>
+        /// Gets the default port for the local file protocol.
+        /// </summary>
+        public static readonly String FilePort = "";
+
+        /// <summary>
+        /// Gets the default port for the gopher protocol.
+        /// </summary>
+        public static readonly String GopherPort = "70";
+
+        /// <summary>
+        /// Gets the default port for the hyper-text transfer protocol.
+        /// </summary>
+        public static readonly String HttpPort = "80";
+
+        /// <summary>
+        /// Gets the default port for the secure hyper-text transfer protocol.
+        /// </summary>
+        public static readonly String HttpsPort = "443";
+
+        /// <summary>
+        /// Gets the default port for the websocket protocol.
+        /// </summary>
+        public static readonly String WsPort = "80";
+
+        /// <summary>
+        /// Gets the default port for the secure websocket protocol.
+        /// </summary>
+        public static readonly String WssPort = "443";
+    }
+}
