38aead15ff253ef5eacea17381be0649f33598a0
Florian Rappl <rappl.florian@gmail.com>
2014-04-22 20:20:59 +0200

    More unit tests for border-style properties added

--- UnitTests/Css/CssBorderProperty.cs
+++ UnitTests/Css/CssBorderProperty.cs
@@ -117,7 +117,7 @@ namespace UnitTests.Css
         {
             var snippet = "border-right-color: rgba(1, 1, 1, 0)";
             var property = CssParser.ParseDeclaration(snippet);
-            Assert.AreEqual("border-top-color", property.Name);
+            Assert.AreEqual("border-right-color", property.Name);
             Assert.IsFalse(property.Important);
             Assert.IsInstanceOfType(property, typeof(CSSBorderRightColorProperty));
             var concrete = (CSSBorderRightColorProperty)property;
@@ -229,6 +229,155 @@ namespace UnitTests.Css
             Assert.AreEqual(CssValueType.Inherit, concrete.Value.CssValueType);
             Assert.IsFalse(concrete.IsInherited);
             Assert.IsFalse(concrete.HasValue);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleDottedLegal()
+        {
+            var snippet = "border-style: dotted";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("dotted", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleInsetOutsetUpperLegal()
+        {
+            var snippet = "border-style: INSET   OUTset";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("INSET OUTset", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleDoubleGrooveLegal()
+        {
+            var snippet = "border-style: double   groove";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("double groove", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleRidgeSolidDashedLegal()
+        {
+            var snippet = "border-style: ridge solid dashed";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("ridge solid dashed", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleHiddenDottedNoneNoneLegal()
+        {
+            var snippet = "border-style   :   hidden  dotted  NONE   nONe";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("hidden dotted NONE nONe", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderStyleWavyIllegal()
+        {
+            var snippet = "border-style: wavy";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderStyleProperty));
+            var concrete = (CSSBorderStyleProperty)property;
+            Assert.AreEqual(CssValueType.Inherit, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsFalse(concrete.HasValue);
+        }
+
+        [TestMethod]
+        public void CssBorderBottomStyleGrooveLegal()
+        {
+            var snippet = "border-bottom-style: GROOVE";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-bottom-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderBottomStyleProperty));
+            var concrete = (CSSBorderBottomStyleProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("GROOVE", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderTopStyleNoneLegal()
+        {
+            var snippet = "border-top-style:none";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-top-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderTopStyleProperty));
+            var concrete = (CSSBorderTopStyleProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("none", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderRightStyleDoubleLegal()
+        {
+            var snippet = "border-right-style:double";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-right-style", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderRightStyleProperty));
+            var concrete = (CSSBorderRightStyleProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("double", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderLeftStyleHiddenLegal()
+        {
+            var snippet = "border-left-style: hidden  !important";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-left-style", property.Name);
+            Assert.IsTrue(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderLeftStyleProperty));
+            var concrete = (CSSBorderLeftStyleProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("hidden", concrete.Value.CssText);
         }
     }
 }
