b0701e98f601f06c0b29afa976dcb3e4a9c89c0d
Florian Rappl <rappl.florian@gmail.com>
2014-04-24 19:54:45 +0200

    Added tests for further border-image subproperties

--- UnitTests/Css/CssBorderImageProperty.cs
+++ UnitTests/Css/CssBorderImageProperty.cs
@@ -52,5 +52,183 @@ namespace UnitTests.Css
             Assert.IsTrue(concrete.HasValue);
             Assert.AreEqual("linear-gradient(0deg, rgba(255, 0, 0, 1) 0%, rgba(255, 255, 0, 1) 100%)", concrete.Value.CssText);
         }
+
+        [TestMethod]
+        public void CssBorderImageOutsetZeroLegal()
+        {
+            var snippet = "border-image-outset: 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-outset", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageOutsetProperty));
+            var concrete = (CSSBorderImageOutsetProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("0", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageOutsetLengthPercentLegal()
+        {
+            var snippet = "border-image-outset: 10px   25%";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-outset", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageOutsetProperty));
+            var concrete = (CSSBorderImageOutsetProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25%", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageOutsetLengthPercentZeroLegal()
+        {
+            var snippet = "border-image-outset: 10px   25% 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-outset", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageOutsetProperty));
+            var concrete = (CSSBorderImageOutsetProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25% 0", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageOutsetLengthPercentZeroPercentLegal()
+        {
+            var snippet = "border-image-outset: 10px   25% 0 10%";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-outset", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageOutsetProperty));
+            var concrete = (CSSBorderImageOutsetProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25% 0 10%", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageOutsetZerosIllegal()
+        {
+            var snippet = "border-image-outset: 0 0 0 0 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-outset", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageOutsetProperty));
+            var concrete = (CSSBorderImageOutsetProperty)property;
+            Assert.AreEqual(CssValueType.Inherit, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsFalse(concrete.HasValue);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthZeroLegal()
+        {
+            var snippet = "border-image-width: 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("0", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthAutoLegal()
+        {
+            var snippet = "border-image-width: auto";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("auto", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthMultipleLegal()
+        {
+            var snippet = "border-image-width: 5";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.PrimitiveValue, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("5", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthLengthPercentLegal()
+        {
+            var snippet = "border-image-width: 10px   25%";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25%", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthLengthPercentZeroLegal()
+        {
+            var snippet = "border-image-width: 10px   25% 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25% 0", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthLengthPercentAutoPercentLegal()
+        {
+            var snippet = "border-image-width: 10px   25% auto 10%";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.ValueList, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsTrue(concrete.HasValue);
+            Assert.AreEqual("10px 25% auto 10%", concrete.Value.CssText);
+        }
+
+        [TestMethod]
+        public void CssBorderImageWidthZerosIllegal()
+        {
+            var snippet = "border-image-width: 0 0 0 0 0";
+            var property = CssParser.ParseDeclaration(snippet);
+            Assert.AreEqual("border-image-width", property.Name);
+            Assert.IsFalse(property.Important);
+            Assert.IsInstanceOfType(property, typeof(CSSBorderImageWidthProperty));
+            var concrete = (CSSBorderImageWidthProperty)property;
+            Assert.AreEqual(CssValueType.Inherit, concrete.Value.CssValueType);
+            Assert.IsFalse(concrete.IsInherited);
+            Assert.IsFalse(concrete.HasValue);
+        }
     }
 }
