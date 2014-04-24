a91ccb4ffb6b745bfbce96773c4e8fc507fc0564
Florian Rappl <rappl.florian@gmail.com>
2014-04-24 20:05:56 +0200

    Implemented border-image-repeat CSS property

--- AngleSharp/DOM/Css/Properties/BorderImage/CSSBorderImageRepeatProperty.cs
+++ AngleSharp/DOM/Css/Properties/BorderImage/CSSBorderImageRepeatProperty.cs
@@ -1,6 +1,7 @@
 ﻿namespace AngleSharp.DOM.Css.Properties
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// More information available at:
@@ -10,14 +11,27 @@
     {
         #region Fields
 
+        static readonly Dictionary<String, RepeatMode> _modes = new Dictionary<String, RepeatMode>(StringComparer.OrdinalIgnoreCase);
+        RepeatMode _horizontal;
+        RepeatMode _vertical;
+
         #endregion
 
         #region ctor
+
+        static CSSBorderImageRepeatProperty()
+        {
+            _modes.Add("stretch", RepeatMode.Stretch);
+            _modes.Add("repeat", RepeatMode.Repeat);
+            _modes.Add("round", RepeatMode.Round);
+        }
 
         internal CSSBorderImageRepeatProperty()
             : base(PropertyNames.BorderImageRepeat)
         {
             _inherited = false;
+            _horizontal = RepeatMode.Stretch;
+            _vertical = RepeatMode.Stretch;
         }
 
         #endregion
@@ -30,10 +44,56 @@
 
         protected override Boolean IsValid(CSSValue value)
         {
-            if (value != CSSValue.Inherit)
+            RepeatMode mode;
+
+            if (value is CSSIdentifierValue && _modes.TryGetValue(((CSSIdentifierValue)value).Value, out mode))
+                _horizontal = _vertical = mode;
+            else if (value is CSSValueList)
+            {
+                var list = (CSSValueList)value;
+                var modes = new RepeatMode[2];
+
+                if (list.Length > 2)
+                    return false;
+
+                for (int i = 0; i < 2; i++)
+			    {
+                    if (list[i] is CSSIdentifierValue == false || !_modes.TryGetValue(((CSSIdentifierValue)list[i]).Value, out modes[i]))
+                        return false;
+			    }
+
+                _horizontal = modes[0];
+                _vertical = modes[1];               
+            }
+            else if (value != CSSValue.Inherit)
                 return false;
 
             return true;
+        }
+
+        #endregion
+
+        #region Repeat Enumeration
+
+        enum RepeatMode : ushort
+        {
+            /// <summary>
+            /// Keyword indicating that the image must be stretched to fill
+            /// the gap between the two borders.
+            /// </summary>
+            Stretch,
+            /// <summary>
+            /// Keyword indicating that the image must be repeated until it
+            /// fills the gap between the two borders.
+            /// </summary>
+            Repeat,
+            /// <summary>
+            /// Keyword indicating that the image must be repeated until it
+            /// fills the gap between the two borders. If the image doesn't fit
+            /// after being repeated an integral number of times, the image is
+            /// rescaled to fit.
+            /// </summary>
+            Round
         }
 
         #endregion
