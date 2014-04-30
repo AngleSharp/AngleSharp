2d92805ad4655ceba7ac52869bdce4c34cfd6bd8
Florian Rappl <rappl.florian@gmail.com>
2014-04-30 09:31:34 +0200

    Implemented animation-name

--- AngleSharp/DOM/Css/Properties/Animation/CSSAnimationNameProperty.cs
+++ AngleSharp/DOM/Css/Properties/Animation/CSSAnimationNameProperty.cs
@@ -1,6 +1,7 @@
 ﻿namespace AngleSharp.DOM.Css.Properties
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// More information available at:
@@ -8,12 +9,31 @@
     /// </summary>
     public sealed class CSSAnimationNameProperty : CSSProperty
     {
+        #region Fields
+
+        List<String> _names;
+
+        #endregion
+
         #region ctor
 
         internal CSSAnimationNameProperty()
             : base(PropertyNames.AnimationName)
         {
             _inherited = false;
+            _names = new List<String>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the names of the animations to trigger.
+        /// </summary>
+        public IEnumerable<String> Names
+        {
+            get { return _names; }
         }
 
         #endregion
@@ -22,7 +42,29 @@
 
         protected override Boolean IsValid(CSSValue value)
         {
-            return base.IsValid(value);
+            if (value.Is("none"))
+                _names.Clear();
+            else if (value is CSSIdentifierValue)
+            {
+                _names.Clear();
+                _names.Add(((CSSIdentifierValue)value).Value);
+            }
+            else if (value is CSSValueList)
+            {
+                var values = value.AsList<CSSIdentifierValue>();
+
+                if (values == null)
+                    return false;
+
+                _names.Clear();
+
+                foreach (var ident in values)
+                    _names.Add(ident.Value);
+            }
+            else if (value != CSSValue.Inherit)
+                return false;
+
+            return true;
         }
 
         #endregion
