cf17ed31bb78197c788ca4cc4e6cb0f1e62fb667
Florian Rappl <rappl.florian@gmail.com>
2014-05-01 13:13:41 +0200

    Implemented animation-direction

--- AngleSharp/DOM/Css/Properties/Animation/CSSAnimationDirectionProperty.cs
+++ AngleSharp/DOM/Css/Properties/Animation/CSSAnimationDirectionProperty.cs
@@ -1,6 +1,7 @@
 ﻿namespace AngleSharp.DOM.Css.Properties
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// More information available at:
@@ -8,12 +9,41 @@
     /// </summary>
     public sealed class CSSAnimationDirectionProperty : CSSProperty
     {
+        #region Fields
+
+        static readonly Dictionary<String, AnimationDirection> modes = new Dictionary<String, AnimationDirection>(StringComparer.OrdinalIgnoreCase);
+        List<AnimationDirection> _directions;
+
+        #endregion
+
         #region ctor
+
+        static CSSAnimationDirectionProperty()
+        {
+            modes.Add("normal", AnimationDirection.Normal);
+            modes.Add("reverse", AnimationDirection.Reverse);
+            modes.Add("alternate", AnimationDirection.Alternate);
+            modes.Add("alternate-reverse", AnimationDirection.AlternateReverse);
+        }
 
         internal CSSAnimationDirectionProperty()
             : base(PropertyNames.AnimationDirection)
         {
             _inherited = false;
+            _directions = new List<AnimationDirection>();
+            _directions.Add(AnimationDirection.Normal);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets an iteration over all defined directions.
+        /// </summary>
+        public IEnumerable<AnimationDirection> Directions
+        {
+            get { return _directions; }
         }
 
         #endregion
@@ -22,7 +52,28 @@
 
         protected override Boolean IsValid(CSSValue value)
         {
-            return base.IsValid(value);
+            var values = value.AsList<CSSIdentifierValue>();
+
+            if (values != null)
+            {
+                var fillModes = new List<AnimationDirection>();
+
+                foreach (var item in values)
+                {
+                    AnimationDirection mode;
+
+                    if (!modes.TryGetValue(item.Value, out mode))
+                        return false;
+
+                    fillModes.Add(mode);
+                }
+
+                _directions = fillModes;
+            }
+            else if (value != CSSValue.Inherit)
+                return false;
+
+            return true;
         }
 
         #endregion
