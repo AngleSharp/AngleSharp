6045b485134a203a1a638c7919b5c90121b33cb0
Florian Rappl <rappl.florian@gmail.com>
2014-05-11 14:02:39 +0200

    Added data scheme test

--- UnitTests/Library/Location.cs
+++ UnitTests/Library/Location.cs
@@ -16,13 +16,14 @@ namespace UnitTests
         {
             var url = "http://www.google.de";
             var location = new Location(url);
-            Assert.AreEqual(String.Empty, location.Hash);
-            Assert.AreEqual(String.Empty, location.Port);
-            Assert.AreEqual(String.Empty, location.Search);
+            Assert.AreEqual("", location.Hash);
+            Assert.AreEqual("", location.Port);
+            Assert.AreEqual("", location.Search);
             Assert.AreEqual("/", location.PathName);
             Assert.AreEqual("http:", location.Protocol);
             Assert.AreEqual("www.google.de", location.Host);
             Assert.AreEqual(url + "/", location.Href);
+            Assert.IsFalse(location.IsRelative);
         }
 
         [TestMethod]
@@ -30,13 +31,14 @@ namespace UnitTests
         {
             var url = "https://www.google.de/mypath";
             var location = new Location(url);
-            Assert.AreEqual(String.Empty, location.Hash);
-            Assert.AreEqual(String.Empty, location.Port);
-            Assert.AreEqual(String.Empty, location.Search);
+            Assert.AreEqual("", location.Hash);
+            Assert.AreEqual("", location.Port);
+            Assert.AreEqual("", location.Search);
             Assert.AreEqual("/mypath", location.PathName);
             Assert.AreEqual("https:", location.Protocol);
             Assert.AreEqual("www.google.de", location.Host);
             Assert.AreEqual(url, location.Href);
+            Assert.IsFalse(location.IsRelative);
         }
 
         [TestMethod]
@@ -44,13 +46,14 @@ namespace UnitTests
         {
             var url = "//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js";
             var location = new Location(url);
-            Assert.AreEqual(String.Empty, location.Hash);
-            Assert.AreEqual(String.Empty, location.Port);
-            Assert.AreEqual(String.Empty, location.Search);
+            Assert.AreEqual("", location.Hash);
+            Assert.AreEqual("", location.Port);
+            Assert.AreEqual("", location.Search);
             Assert.AreEqual("/ajax/libs/jquery/1.4.2/jquery.js", location.PathName);
-            Assert.AreEqual(String.Empty, location.Protocol);
+            Assert.AreEqual("", location.Protocol);
             Assert.AreEqual("ajax.googleapis.com", location.Host);
             Assert.AreEqual(url, location.Href);
+            Assert.IsTrue(location.IsRelative);
         }
 
         [TestMethod]
@@ -58,7 +61,7 @@ namespace UnitTests
         {
             var url = "http://localhost:8080/?mytest";
             var location = new Location(url);
-            Assert.AreEqual(String.Empty, location.Hash);
+            Assert.AreEqual("", location.Hash);
             Assert.AreEqual("8080", location.Port);
             Assert.AreEqual("?mytest", location.Search);
             Assert.AreEqual("/", location.PathName);
@@ -66,6 +69,23 @@ namespace UnitTests
             Assert.AreEqual("localhost:8080", location.Host);
             Assert.AreEqual("localhost", location.HostName);
             Assert.AreEqual(url, location.Href);
+            Assert.IsFalse(location.IsRelative);
+        }
+
+        [TestMethod]
+        public void DataLocationSchemeOnly()
+        {
+            var url = "data:";
+            var location = new Location(url);
+            Assert.AreEqual("", location.Hash);
+            Assert.AreEqual("", location.Port);
+            Assert.AreEqual("", location.Search);
+            Assert.AreEqual("/", location.PathName);
+            Assert.AreEqual("data:", location.Protocol);
+            Assert.AreEqual("", location.Host);
+            Assert.AreEqual("", location.HostName);
+            Assert.AreEqual(url, location.Href);
+            Assert.IsTrue(location.IsRelative);
         }
     }
 }
