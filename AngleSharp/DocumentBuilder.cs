c3af0bc0b08a8eb6ddeab050f53dfb1ba20dc678
Florian Rappl <rappl.florian@gmail.com>
2014-05-24 13:13:44 +0200

    Extended the document builder API

--- AngleSharp/DocumentBuilder.cs
+++ AngleSharp/DocumentBuilder.cs
@@ -11,6 +11,7 @@
     using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
+    using GlobalConfig = AngleSharp.Configuration;
 
     /// <summary>
     /// A handy helper to construct various kinds of documents
@@ -20,35 +21,20 @@
     {
         #region Fields
 
-        IParser parser;
+        readonly IConfiguration configuration;
 
         #endregion
 
         #region ctor
 
         /// <summary>
-        /// Creates a new builder with the specified source.
+        /// Creates a new builder and optionally sets the configuration.
         /// </summary>
-        /// <param name="source">The code manager.</param>
-        /// <param name="document">The document to fill.</param>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        DocumentBuilder(SourceManager source, HTMLDocument document, IConfiguration configuration)
+        /// <param name="defaultConfiguration">The configuration to use. If this
+        /// is not specified, then the default configuration will be used.</param>
+        public DocumentBuilder(IConfiguration defaultConfiguration = null)
         {
-            parser = new HtmlParser(document, source);
-			parser.ParseError += (s, e) => configuration.ReportError(e);
-        }
-
-        /// <summary>
-        /// Creates a new builder with the specified source.
-        /// </summary>
-        /// <param name="source">The code manager.</param>
-        /// <param name="sheet">The document to fill.</param>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        DocumentBuilder(SourceManager source, CSSStyleSheet sheet, IConfiguration configuration)
-        {
-            sheet.Options = configuration;
-            parser = new CssParser(sheet, source);
-            parser.ParseError += (s, e) => configuration.ReportError(e);
+            configuration = defaultConfiguration ?? GlobalConfig.Default;
         }
 
         #endregion
@@ -56,19 +42,141 @@
         #region Properties
 
         /// <summary>
-        /// Gets the result of an HTML parsing.
+        /// Gets the configuration to use.
         /// </summary>
-        public HTMLDocument HtmlResult
+        public IConfiguration Configuration
         {
-            get { return ((HtmlParser)parser).Result; }
+            get { return configuration; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Builds a new HTMLDocument with the given source code string.
+        /// </summary>
+        /// <param name="sourceCode">The string to use as source code.</param>
+        /// <returns>The constructed HTML document.</returns>
+        public HTMLDocument FromHtml(String sourceCode)
+        {
+            var source = new SourceManager(sourceCode, configuration.DefaultEncoding());
+            var doc = new HTMLDocument { Options = configuration };
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
         }
 
         /// <summary>
-        /// Gets the result of a CSS parsing.
+        /// Builds a new HTMLDocument with the given URL.
         /// </summary>
-        public CSSStyleSheet CssResult
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <returns>The constructed HTML document.</returns>
+        public HTMLDocument FromHtml(Uri url)
         {
-            get { return ((CssParser)parser).Result; }
+            return HtmlAsync(url).Result;
+        }
+
+        /// <summary>
+        /// Builds a new HTMLDocument by asynchronously requesting the given URL.
+        /// </summary>
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <returns>The task that constructs the HTML document.</returns>
+        public Task<HTMLDocument> FromHtmlAsync(Uri url)
+        {
+            return HtmlAsync(url, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Builds a new HTMLDocument by asynchronously requesting the given URL.
+        /// </summary>
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <param name="cancel">The cancellation token for cancelling the asynchronous request.</param>
+        /// <returns>The task that constructs the HTML document.</returns>
+        public async Task<HTMLDocument> FromHtmlAsync(Uri url, CancellationToken cancel)
+        {
+            var stream = await configuration.LoadAsync(url, cancel, force: true);
+            var source = new SourceManager(stream, configuration.DefaultEncoding());
+            var doc = new HTMLDocument { Options = configuration };
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
+        }
+
+        /// <summary>
+        /// Builds a new CSSStyleSheet with the given source code string.
+        /// </summary>
+        /// <param name="sourceCode">The string to use as source code.</param>
+        /// <returns>The constructed CSS stylesheet.</returns>
+        public CSSStyleSheet FromCss(String sourceCode)
+        {
+            var source = new SourceManager(sourceCode, configuration.DefaultEncoding());
+            var doc = new CSSStyleSheet { Options = configuration };
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
+        }
+
+        /// <summary>
+        /// Builds a new CSSStyleSheet with the given URL.
+        /// </summary>
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <returns>The constructed CSS stylesheet.</returns>
+        public CSSStyleSheet FromCss(Uri url)
+        {
+            return CssAsync(url).Result;
+        }
+
+        /// <summary>
+        /// Builds a new CSSStyleSheet asynchronously by requesting the given URL.
+        /// </summary>
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <returns>The task which constructs the CSS stylesheet.</returns>
+        public Task<CSSStyleSheet> FromCssAsync(Uri url)
+        {
+            return CssAsync(url, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Builds a new CSSStyleSheet asynchronously by requesting the given URL.
+        /// </summary>
+        /// <param name="url">The URL which points to the address containing the source code.</param>
+        /// <param name="cancel">The cancellation token for cancelling the asynchronous request.</param>
+        /// <returns>The task which constructs the CSS stylesheet.</returns>
+        public async Task<CSSStyleSheet> FromCssAsync(Uri url, CancellationToken cancel)
+        {
+            var stream = await configuration.LoadAsync(url, cancel, force: true);
+            var source = new SourceManager(stream, configuration.DefaultEncoding());
+            var doc = new CSSStyleSheet { Options = configuration };
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        /// <summary>
+        /// Creates a new parser with the specified source.
+        /// </summary>
+        /// <param name="source">The code manager.</param>
+        /// <param name="document">The document to fill.</param>
+        /// <param name="configuration">Options to use for the document generation.</param>
+        static HtmlParser Construct(SourceManager source, HTMLDocument document, IConfiguration configuration)
+        {
+            var parser = new HtmlParser(document, source);
+            parser.ParseError += (s, e) => configuration.ReportError(e);
+            return parser;
+        }
+
+        /// <summary>
+        /// Creates a new parser with the specified source.
+        /// </summary>
+        /// <param name="source">The code manager.</param>
+        /// <param name="sheet">The document to fill.</param>
+        /// <param name="configuration">Options to use for the document generation.</param>
+        static CssParser Construct(SourceManager source, CSSStyleSheet sheet, IConfiguration configuration)
+        {
+            var parser = new CssParser(sheet, source);
+            parser.ParseError += (s, e) => configuration.ReportError(e);
+            return parser;
         }
 
         #endregion
@@ -84,12 +192,12 @@
         public static HTMLDocument Html(String sourceCode, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var source = new SourceManager(sourceCode, configuration.DefaultEncoding());
             var doc = new HTMLDocument { Options = configuration };
-            var db = new DocumentBuilder(source, doc, configuration);
-            return db.HtmlResult;
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
         }
 
         /// <summary>
@@ -124,14 +232,14 @@
         public static async Task<HTMLDocument> HtmlAsync(Uri url, CancellationToken cancel, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var stream = await configuration.LoadAsync(url, cancel, force: true);
             var source = new SourceManager(stream, configuration.DefaultEncoding());
             var doc = new HTMLDocument { Options = configuration, DocumentUri = url.OriginalString };
-            var db = new DocumentBuilder(source, doc, configuration);
-            await db.parser.ParseAsync();
-            return db.HtmlResult;
+            var parser = Construct(source, doc, configuration);
+            await parser.ParseAsync();
+            return parser.Result;
         }
 
         /// <summary>
@@ -143,12 +251,12 @@
         public static HTMLDocument Html(Stream stream, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var source = new SourceManager(stream, configuration.DefaultEncoding());
             var doc = new HTMLDocument { Options = configuration };
-			var db = new DocumentBuilder(source, doc, configuration);
-            return db.HtmlResult;
+            var parser = Construct(source, doc, configuration);
+            return parser.Result;
         }
 
         /// <summary>
@@ -161,7 +269,7 @@
         public static NodeList HtmlFragment(String sourceCode, Node context = null, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var source = new SourceManager(sourceCode, configuration.DefaultEncoding());
             var doc = new HTMLDocument { Options = configuration };
@@ -169,19 +277,18 @@
             //Disable scripting for HTML fragments (security reasons)
             configuration.IsScripting = false;
 
-            var db = new DocumentBuilder(source, doc, configuration);
+            var parser = Construct(source, doc, configuration);
 
             if (context != null)
             {
                 if (context.OwnerDocument != null && context.OwnerDocument.QuirksMode != QuirksMode.Off)
                     doc.QuirksMode = context.OwnerDocument.QuirksMode;
 
-                var parser = (HtmlParser)db.parser;
                 parser.SwitchToFragment(context);
                 return parser.Result.DocumentElement.ChildNodes;
             }
 
-            return db.HtmlResult.ChildNodes;
+            return parser.Result.ChildNodes;
         }
 
         #endregion
@@ -197,12 +304,12 @@
         public static CSSStyleSheet Css(String sourceCode, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var source = new SourceManager(sourceCode, configuration.DefaultEncoding());
             var sheet = new CSSStyleSheet { Options = configuration };
-			var db = new DocumentBuilder(source, sheet, configuration);
-            return db.CssResult;
+            var parser = Construct(source, sheet, configuration);
+            return parser.Result;
         }
 
         /// <summary>
@@ -237,14 +344,14 @@
         public static async Task<CSSStyleSheet> CssAsync(Uri url, CancellationToken cancel, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var stream = await configuration.LoadAsync(url, cancel, force: true);
             var source = new SourceManager(stream, configuration.DefaultEncoding());
             var sheet = new CSSStyleSheet { Href = url.OriginalString, Options = configuration };
-            var db = new DocumentBuilder(source, sheet, configuration);
-            await db.parser.ParseAsync();
-            return db.CssResult;
+            var parser = Construct(source, sheet, configuration);
+            await parser.ParseAsync();
+            return parser.Result;
         }
 
         /// <summary>
@@ -256,12 +363,12 @@
         public static CSSStyleSheet Css(Stream stream, IConfiguration configuration = null)
         {
             if (configuration == null)
-                configuration = Configuration.Default;
+                configuration = GlobalConfig.Default;
 
             var source = new SourceManager(stream, configuration.DefaultEncoding());
             var sheet = new CSSStyleSheet { Options = configuration };
-			var db = new DocumentBuilder(source, sheet, configuration);
-            return db.CssResult;
+            var parser = Construct(source, sheet, configuration);
+            return parser.Result;
         }
 
         #endregion
