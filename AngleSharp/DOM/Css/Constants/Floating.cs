6930c358f81f63f2cada962282fc68f44824c0b9
Florian Rappl <rappl.florian@gmail.com>
2014-05-27 23:08:01 +0200

    Extracted the float enumeration

---
+++ AngleSharp/DOM/Css/Constants/Floating.cs
@@ -0,0 +1,21 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    /// <summary>
+    /// All possible values for taking an element out of its normal flow.
+    /// </summary>
+    public enum Floating : ushort
+    {
+        /// <summary>
+        /// Indicates that the element must not float.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Indicates that the element must float on the left side of its containing block.
+        /// </summary>
+        Left,
+        /// <summary>
+        /// Indicates that the element must float on the right side of its containing block.
+        /// </summary>
+        Right
+    }
+}
--- AngleSharp/DOM/Css/Properties/Flow/CSSFloatProperty.cs
+++ AngleSharp/DOM/Css/Properties/Flow/CSSFloatProperty.cs
@@ -11,8 +11,8 @@
     {
         #region Fields
 
-        static readonly Dictionary<String, FloatMode> modes = new Dictionary<String, FloatMode>(StringComparer.OrdinalIgnoreCase);
-        FloatMode _mode;
+        static readonly Dictionary<String, Floating> modes = new Dictionary<String, Floating>(StringComparer.OrdinalIgnoreCase);
+        Floating _mode;
 
         #endregion
 
@@ -20,16 +20,28 @@
 
         static CSSFloatProperty()
         {
-            modes.Add("none", new NoneFloatMode());
-            modes.Add("left", new LeftFloatMode());
-            modes.Add("right", new RightFloatMode());
+            modes.Add("none", Floating.None);
+            modes.Add("left", Floating.Left);
+            modes.Add("right", Floating.Right);
         }
 
         internal CSSFloatProperty()
             : base(PropertyNames.Float)
         {
-            _mode = modes["none"];
+            _mode = Floating.None;
             _inherited = false;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the value of the floating property.
+        /// </summary>
+        public Floating State
+        {
+            get { return _mode; }
         }
 
         #endregion
@@ -43,7 +55,7 @@
         /// <returns>True if the state is valid, otherwise false.</returns>
         protected override Boolean IsValid(CSSValue value)
         {
-            FloatMode mode;
+            Floating mode;
 
             if (value is CSSIdentifierValue && modes.TryGetValue(((CSSIdentifierValue)value).Value, out mode))
                 _mode = mode;
@@ -51,27 +63,6 @@
                 return false;
 
             return true;
-        }
-
-        #endregion
-
-        #region Modes
-
-        abstract class FloatMode
-        {
-            //TODO Add members that make sense
-        }
-
-        class NoneFloatMode : FloatMode
-        {
-        }
-
-        class LeftFloatMode : FloatMode
-        {
-        }
-
-        class RightFloatMode : FloatMode
-        {
         }
 
         #endregion
