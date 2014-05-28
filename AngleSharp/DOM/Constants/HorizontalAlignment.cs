e27752219c86fcbb977ecd32143244dd12c5dbdf
Florian Rappl <rappl.florian@gmail.com>
2014-05-28 19:20:36 +0200

    Added CSS version of VerticalAlignment

--- AngleSharp/DOM/Constants/HorizontalAlignment.cs
+++ AngleSharp/DOM/Constants/HorizontalAlignment.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// The list of possible horizontal alignments.
     /// </summary>
-    public enum HorizontalAlignment
+    public enum HorizontalAlignment : ushort
     {
         /// <summary>
         /// The inline contents are aligned to the left edge of the line box.
--- AngleSharp/DOM/Constants/VerticalAlignment.cs
+++ AngleSharp/DOM/Constants/VerticalAlignment.cs
@@ -1,9 +1,9 @@
 ﻿namespace AngleSharp.DOM
 {
     /// <summary>
-    /// The list of possible vertical alignments.
+    /// The list of possible vertical alignments (use in HTML).
     /// </summary>
-    public enum VerticalAlignment
+    public enum VerticalAlignment : ushort
     {
         /// <summary>
         /// Cell data is flush with the top of the cell.
@@ -24,5 +24,57 @@
         /// lines in these cells.
         /// </summary>
         Baseline
+    }
+
+    namespace Css
+    {
+        /// <summary>
+        /// The list of possible vertical alignments (extended by CSS).
+        /// </summary>
+        public enum VerticallAlignment : ushort
+        {
+            /// <summary>
+            /// Aligns the baseline of the element with the baseline of its parent.
+            /// The baseline of some replaced elements, like textarea is not specified
+            /// by the HTML specification, meaning that their behavior with this keyword
+            /// may change from one browser to the other.
+            /// </summary>
+            Baseline,
+            /// <summary>
+            /// Aligns the baseline of the element with the subscript-baseline
+            /// of its parent.
+            /// </summary>
+            Sub,
+            /// <summary>
+            /// Aligns the baseline of the element with the superscript-baseline
+            /// of its parent.
+            /// </summary>
+            Super,
+            /// <summary>
+            /// Aligns the top of the element with the top of the parent
+            /// element's font.
+            /// </summary>
+            TextTop,
+            /// <summary>
+            /// Aligns the bottom of the element with the bottom of the parent
+            /// element's font.
+            /// </summary>
+            TextBottom,
+            /// <summary>
+            /// Aligns the middle of the element with the middle of lowercase
+            /// letters in the parent.
+            /// </summary>
+            Middle,
+            /// <summary>
+            /// Align the top of the element and its descendants with the top
+            /// of the entire line.
+            /// </summary>
+            Top,
+            /// <summary>
+            /// Align the bottom of the element and its descendants with the
+            /// bottom of the entire line.
+            /// </summary>
+            Bottom,
+        }
     }
 }
