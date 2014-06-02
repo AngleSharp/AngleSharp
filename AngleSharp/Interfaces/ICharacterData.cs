886562c3be2cf7c044279afc3a1e05e3b8cc9c2e
Florian Rappl <rappl.florian@gmail.com>
2014-06-02 23:27:13 +0200

    Added the character data IDL file

---
+++ AngleSharp/Interfaces/ICharacterData.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.DOM
+{
+    using System;
+
+    [DOM("CharacterData")]
+    interface ICharacterData : INode
+    {
+        [DOM("data")]
+        String Value { get; set; }
+
+        [DOM("length")]
+        Int32 Length { get; }
+
+        [DOM("substringData")]
+        String Substring(Int32 offset, Int32 count);
+
+        [DOM("appendData")]
+        void Append(String value);
+
+        [DOM("insertData")]
+        void Insert(Int32 offset, String value);
+
+        [DOM("deleteData")]
+        void Delete(Int32 offset, Int32 count);
+
+        [DOM("replaceData")]
+        void Replace(Int32 offset, Int32 count, String value);
+    }
+}
