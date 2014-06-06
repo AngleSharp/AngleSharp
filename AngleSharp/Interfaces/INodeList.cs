8a871bbe7ad0a4e1b1c51106e72f735a02246884
Florian Rappl <rappl.florian@gmail.com>
2014-06-06 19:17:35 +0200

    Added documentation.

--- AngleSharp/Interfaces/INodeList.cs
+++ AngleSharp/Interfaces/INodeList.cs
@@ -1,13 +1,25 @@
 ﻿namespace AngleSharp.DOM
 {
     using System;
+    using System.Collections.Generic;
 
+    /// <summary>
+    /// NodeList objects are collections of nodes.
+    /// </summary>
     [DOM("NodeList")]
-    interface INodeList
+    public interface INodeList : IEnumerable<INode>
     {
+        /// <summary>
+        /// Returns an item in the list by its index, or null if out-of-bounds. 
+        /// </summary>
+        /// <param name="index">The 0-based index.</param>
+        /// <returns>The element if it exists, otherwise false.</returns>
         [DOM("item")]
         INode this[Int32 index] { get; }
 
+        /// <summary>
+        /// Gets the number of nodes in the NodeList.
+        /// </summary>
         [DOM("length")]
         Int32 Length { get; }
     }
