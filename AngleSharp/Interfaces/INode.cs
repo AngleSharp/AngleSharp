f867ad059dff80ae1a960b8a3efe1979b4772dc6
Florian Rappl <rappl.florian@gmail.com>
2014-06-09 09:37:03 +0200

    Updated the IDL for Node

--- AngleSharp/Interfaces/INode.cs
+++ AngleSharp/Interfaces/INode.cs
@@ -3,35 +3,89 @@
     using System;
     using AngleSharp.DOM.Collections;
 
+    /// <summary>
+    /// A Node is an interface from which a number of DOM types inherit, 
+    /// and allows these various types to be treated similarly.
+    /// </summary>
     [DomName("Node")]
     public interface INode
     {
-        Node AppendChild(Node child);
+        [DomName("baseURI")]
         String BaseURI { get; }
-        NodeList ChildNodes { get; }
-        Node CloneNode(Boolean deep = true);
-        DocumentPosition CompareDocumentPosition(Node otherNode);
-        Boolean Contains(Node otherNode);
-        Node FirstChild { get; }
-        Boolean HasChildNodes { get; }
-        Node InsertBefore(Node newElement, Node referenceElement);
-        Node InsertChild(Int32 index, Node child);
-        Boolean IsDefaultNamespace(String namespaceURI);
-        Boolean IsEqualNode(Node otherNode);
-        Node LastChild { get; }
-        String LookupNamespaceURI(String prefix);
-        String LookupPrefix(String namespaceURI);
-        Node NextSibling { get; }
+
+        [DomName("nodeName")]
         String NodeName { get; }
-        NodeType NodeType { get; }
-        String NodeValue { get; set; }
+
+        [DomName("childNodes")]
+        NodeList ChildNodes { get; }
+
+        [DomName("cloneNode")]
+        Node CloneNode(Boolean deep = true);
+
+        [DomName("isEqualNode")]
+        Boolean IsEqualNode(Node otherNode);
+
+        [DomName("compareDocumentPosition")]
+        DocumentPosition CompareDocumentPosition(Node otherNode);
+
+        [DomName("normalize")]
         Node Normalize();
+
+        [DomName("ownerDocument")]
         Document OwnerDocument { get; }
+
+        [DomName("parentElement")]
         Element ParentElement { get; }
+
+        [DomName("parentNode")]
         Node ParentNode { get; }
+
+        [DomName("contains")]
+        Boolean Contains(Node otherNode);
+
+        [DomName("firstChild")]
+        Node FirstChild { get; }
+
+        [DomName("lastChild")]
+        Node LastChild { get; }
+
+        [DomName("nextSibling")]
+        Node NextSibling { get; }
+
+        [DomName("previousSibling")]
         Node PreviousSibling { get; }
-        Node RemoveChild(Node child);
-        Node ReplaceChild(Node newChild, Node oldChild);
+
+        [DomName("isDefaultNamespace")]
+        Boolean IsDefaultNamespace(String namespaceURI);
+
+        [DomName("lookupNamespaceURI")]
+        String LookupNamespaceURI(String prefix);
+
+        [DomName("lookupPrefix")]
+        String LookupPrefix(String namespaceURI);
+
+        [DomName("nodeType")]
+        NodeType NodeType { get; }
+
+        [DomName("nodeValue")]
+        String NodeValue { get; set; }
+
+        [DomName("textContent")]
         String TextContent { get; set; }
+
+        [DomName("hasChildNodes")]
+        Boolean HasChildNodes { get; }
+
+        [DomName("appendChild")]
+        Node AppendChild(Node child);
+
+        [DomName("insertBefore")]
+        Node InsertBefore(Node newElement, Node referenceElement);
+
+        [DomName("removeChild")]
+        Node RemoveChild(Node child);
+
+        [DomName("replaceChild")]
+        Node ReplaceChild(Node newChild, Node oldChild);
     }
 }
