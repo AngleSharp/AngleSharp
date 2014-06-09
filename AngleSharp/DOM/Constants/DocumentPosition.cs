0abfa4c9e2ba4ea30b1143ef4e61bd785e7b09a4
Florian Rappl <rappl.florian@gmail.com>
2014-06-09 09:25:28 +0200

    Moved NodeType and decorated DocumentPosition

--- AngleSharp/DOM/Constants/DocumentPosition.cs
+++ AngleSharp/DOM/Constants/DocumentPosition.cs
@@ -15,26 +15,32 @@
         /// <summary>
         /// There is no relation.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_DISCONNECTED")]
         Disconnected = 0x01,
         /// <summary>
         /// The node preceeds the other element.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_PRECEDING")]
         Preceding = 0x02,
         /// <summary>
         /// The node follows the other element.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_FOLLOWING")]
         Following = 0x04,
         /// <summary>
         /// The node is contains the other element.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_CONTAINS")]
         Contains = 0x08,
         /// <summary>
         /// The node is contained in the other element.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_CONTAINED_BY")]
         ContainedBy = 0x10,
         /// <summary>
         /// The relation is implementation specific.
         /// </summary>
+        [DomName("DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC")]
         ImplementationSpecific = 0x20
     }
 }
