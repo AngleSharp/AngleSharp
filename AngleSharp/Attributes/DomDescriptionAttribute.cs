775179c2221c4296e47b860681c83049703c5417
Florian Rappl <rappl.florian@gmail.com>
2014-06-14 14:47:28 +0200

    Added the description attribute

---
+++ AngleSharp/Attributes/DomDescriptionAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace AngleSharp
+{
+    using System;
+
+    /// <summary>
+    /// This attribute is used to place a description on some object.
+    /// The description can then be read out at runtime.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Delegate)]
+    public sealed class DomDescriptionAttribute : Attribute
+    {
+        internal DomDescriptionAttribute(String description)
+        {
+            Description = description;
+        }
+
+        /// <summary>
+        /// Gets the official name of the given class,
+        /// method or property.
+        /// </summary>
+        public String Description
+        {
+            get;
+            private set;
+        }
+    }
+}
