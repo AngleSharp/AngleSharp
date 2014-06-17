b21d6e1ae1374e0b2434202c5f198eb136b9bdc4
Florian Rappl <rappl.florian@gmail.com>
2014-06-17 22:09:17 +0200

    Decorated the readiness

--- AngleSharp/DOM/Constants/Readiness.cs
+++ AngleSharp/DOM/Constants/Readiness.cs
@@ -3,19 +3,23 @@
     /// <summary>
     /// Defines the document readiness.
     /// </summary>
+    [DomName("DocumentReadyState")]
     public enum Readiness : ushort
     {
         /// <summary>
         /// The document is still loading.
         /// </summary>
+        [DomName("loading")]
         Loading,
         /// <summary>
         /// The document is interactive, i.e. interaction possible.
         /// </summary>
+        [DomName("interactive")]
         Interactive,
         /// <summary>
         /// Loading is complete.
         /// </summary>
+        [DomName("complete")]
         Complete
     }
 }
