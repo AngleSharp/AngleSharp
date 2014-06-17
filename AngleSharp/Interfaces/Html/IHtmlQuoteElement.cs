e20db75f0be860bf8896173a5c649c7823181a50
Florian Rappl <rappl.florian@gmail.com>
2014-06-17 22:04:40 +0200

    Added more HTML IDL definitions

---
+++ AngleSharp/Interfaces/Html/IHtmlQuoteElement.cs
@@ -0,0 +1,11 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    [DomName("HTMLQuoteElement")]
+    interface IHtmlQuoteElement : IHtmlElement
+    {
+        [DomName("cite")]
+        String Citation { get; set; }
+    }
+}
---
+++ AngleSharp/Interfaces/Html/IHtmlScriptElement.cs
@@ -0,0 +1,26 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    [DomName("HTMLScriptElement")]
+    interface IHtmlScriptElement : IHtmlElement
+    {
+        [DomName("src")]
+        String Source { get; set; }
+
+        [DomName("async")]
+        Boolean IsAsync { get; set; }
+
+        [DomName("defer")]
+        Boolean IsDeferred { get; set; }
+
+        [DomName("type")]
+        String Type { get; set; }
+
+        [DomName("charset")]
+        String CharacterSet { get; set; }
+
+        [DomName("text")]
+        String Text { get; set; }
+    }
+}
