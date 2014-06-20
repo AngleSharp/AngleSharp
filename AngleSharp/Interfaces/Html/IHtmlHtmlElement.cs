ffe40dde4c2fa4a5854bd48ea50eba4cf1b2c9b9
Florian Rappl <rappl.florian@gmail.com>
2014-06-20 18:11:27 +0200

    Added the HTML element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlHtmlElement.cs
@@ -0,0 +1,10 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    /// <summary>
+    /// Represents the html HTML element.
+    /// </summary>
+    [DomName("HTMLHtmlElement")]
+    interface IHtmlHtmlElement : IHtmlElement
+    {
+    }
+}
