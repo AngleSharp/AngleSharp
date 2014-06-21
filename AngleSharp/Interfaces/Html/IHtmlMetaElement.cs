f71ceeeff1d27685fa1b6a5da3a76f72939a9fda
Florian Rappl <rappl.florian@gmail.com>
2014-06-21 10:00:10 +0200

    Added the HTML meta element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlMetaElement.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the meta HTML element.
+    /// </summary>
+    [DomName("HTMLMetaElement")]
+    interface IHtmlMetaElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the name of the meta element.
+        /// </summary>
+        [DomName("name")]
+        String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the http-equiv attribute of the meta element.
+        /// </summary>
+        [DomName("httpEquiv")]
+        String HttpEquiv { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the content attribute of the meta element.
+        /// </summary>
+        [DomName("content")]
+        String Content { get; set; }
+    }
+}
