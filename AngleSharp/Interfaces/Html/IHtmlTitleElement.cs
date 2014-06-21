c574aa3c6b3bcd07e777c44059e9625e73797acf
Florian Rappl <rappl.florian@gmail.com>
2014-06-21 09:55:32 +0200

    Added the HTML title element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlTitleElement.cs
@@ -0,0 +1,17 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the title HTML element.
+    /// </summary>
+    [DomName("HTMLTitleElement")]
+    interface IHtmlTitleElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the text of the title.
+        /// </summary>
+        [DomName("text")]
+        String Text { get; set; }
+    }
+}
