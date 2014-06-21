7eeada17518f6483d6209d9a937d378f2300812f
Florian Rappl <rappl.florian@gmail.com>
2014-06-21 09:57:31 +0200

    Added the base element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlBaseElement.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the base HTML element.
+    /// </summary>
+    [DomName("HTMLBaseElement")]
+    interface IHtmlBaseElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the hyperreference to the base URL.
+        /// </summary>
+        [DomName("href")]
+        String Href { get; set; }
+
+        /// <summary>
+        /// Gets or sets the base target.
+        /// </summary>
+        [DomName("Target")]
+        String Target { get; set; }
+    }
+}
