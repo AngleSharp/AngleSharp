9bf2d6777e2db5d357f7e018869eced9df046c36
Florian Rappl <rappl.florian@gmail.com>
2014-06-22 11:27:17 +0200

    Added documentation

--- AngleSharp/Interfaces/Html/IHtmlLinkElement.cs
+++ AngleSharp/Interfaces/Html/IHtmlLinkElement.cs
@@ -2,30 +2,57 @@
 {
     using System;
 
+    /// <summary>
+    /// Represents a link HTML element.
+    /// </summary>
     [DomName("HTMLLinkElement")]
     public interface IHtmlLinkElement : IHtmlElement
     {
+        /// <summary>
+        /// Gets or sets if the stylesheet is enabled or disabled.
+        /// </summary>
         [DomName("disabled")]
         Boolean Disabled { get; set; }
 
+        /// <summary>
+        /// Gets or sets the URI for the target resource.
+        /// </summary>
         [DomName("href")]
         String Href { get; set; }
 
+        /// <summary>
+        /// Gets or sets the forward relationship of the linked resource from the document to the resource.
+        /// </summary>
         [DomName("rel")]
         String Rel { get; set; }
 
+        /// <summary>
+        /// Gets the list of relations contained in the rel attribute.
+        /// </summary>
         [DomName("relList")]
         ITokenList RelList { get; }
 
+        /// <summary>
+        /// Gets or sets the use with one or more target media.
+        /// </summary>
         [DomName("media")]
         String Media { get; set; }
 
+        /// <summary>
+        /// Gets or sets the language code for the linked resource.
+        /// </summary>
         [DomName("hreflang")]
         String Hreflang { get; set; }
 
+        /// <summary>
+        /// Gets or sets the content type of the style sheet language.
+        /// </summary>
         [DomName("type")]
         String Type { get; set; }
 
+        /// <summary>
+        /// Gets the list of sizes defined in the sizes attribute.
+        /// </summary>
         [DomName("sizes")]
         ISettableTokenList Sizes { get; }
     }
