d61f8136476895757321db02ce3180cae6bc17c2
Florian Rappl <rappl.florian@gmail.com>
2014-06-22 11:16:15 +0200

    Added the HTML link IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlLinkElement.cs
@@ -0,0 +1,32 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    [DomName("HTMLLinkElement")]
+    public interface IHtmlLinkElement : IHtmlElement
+    {
+        [DomName("disabled")]
+        Boolean Disabled { get; set; }
+
+        [DomName("href")]
+        String Href { get; set; }
+
+        [DomName("rel")]
+        String Rel { get; set; }
+
+        [DomName("relList")]
+        ITokenList RelList { get; }
+
+        [DomName("media")]
+        String Media { get; set; }
+
+        [DomName("hreflang")]
+        String Hreflang { get; set; }
+
+        [DomName("type")]
+        String Type { get; set; }
+
+        [DomName("sizes")]
+        ISettableTokenList Sizes { get; }
+    }
+}
