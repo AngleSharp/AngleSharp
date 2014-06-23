84ff0312bc93e2908026255f70eade36653c78f0
Florian Rappl <rappl.florian@gmail.com>
2014-06-23 19:34:55 +0200

    Removed public modifier, implemented IDL

--- AngleSharp/DOM/Html/Semantic/HTMLHeadingElement.cs
+++ AngleSharp/DOM/Html/Semantic/HTMLHeadingElement.cs
@@ -5,8 +5,7 @@
     /// <summary>
     /// Represents the an HTML heading element (h1, h2, h3, h4, h5, h6).
     /// </summary>
-    [DomName("HTMLHeadingElement")]
-    public sealed class HTMLHeadingElement : HTMLElement
+    sealed class HTMLHeadingElement : HTMLElement, IHtmlHeadingElement
     {
         internal HTMLHeadingElement()
         { }
