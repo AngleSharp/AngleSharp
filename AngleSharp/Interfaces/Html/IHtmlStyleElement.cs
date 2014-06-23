d7a8948153e1fa62e30741c207d99dda5f30b4d3
Florian Rappl <rappl.florian@gmail.com>
2014-06-23 19:21:22 +0200

    Added the style element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlStyleElement.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents a style HTML element.
+    /// </summary>
+    [DomName("HTMLStyleElement")]
+    public interface IHtmlStyleElement : IHtmlElement
+    {
+        [DomName("disabled")]
+        Boolean Disabled { get; set; }
+
+        [DomName("media")]
+        String Media { get; set; }
+
+        [DomName("type")]
+        String Type { get; set; }
+
+        [DomName("scoped")]
+        Boolean Scoped { get; set; }
+    }
+}
