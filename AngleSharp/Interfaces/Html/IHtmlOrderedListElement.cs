793cdcc34acd1bd3b4219fd19ad28b70b875810e
Florian Rappl <rappl.florian@gmail.com>
2014-06-24 19:34:25 +0200

    Added the ol IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlOrderedListElement.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the ol HTML element.
+    /// </summary>
+    [DomName("HTMLOListElement")]
+    public interface IHtmlOrderedListElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets if the order is reversed.
+        /// </summary>
+        [DomName("reversed")]
+        Boolean Reversed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lowest number.
+        /// </summary>
+        [DomName("start")]
+        Int32 Start { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of enumeration.
+        /// </summary>
+        [DomName("type")]
+        String Type { get; set; }
+    }
+}
