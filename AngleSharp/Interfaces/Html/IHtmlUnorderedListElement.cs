8dca80c2881d36b3ca624285b05cd3b0c45dcbdc
Florian Rappl <rappl.florian@gmail.com>
2014-06-24 19:31:23 +0200

    Added the ul element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlUnorderedListElement.cs
@@ -0,0 +1,10 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    /// <summary>
+    /// Represents an ul HTML element.
+    /// </summary>
+    [DomName("HTMLUListElement")]
+    public interface IHtmlUnorderedListElement : IHtmlElement
+    {
+    }
+}
