4bd2562d6a700f9e36e091d8e9c77efff7d3be31
Florian Rappl <rappl.florian@gmail.com>
2014-06-27 22:13:42 +0200

    Added the a element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlAnchorElement.cs
@@ -0,0 +1,35 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the a HTML element.
+    /// </summary>
+    [DomName("HTMLAnchorElement")]
+    public interface IHtmlAnchorElement : IHtmlElement, IUrlUtilities
+    {
+        [DomName("target")]
+        String Target { get; set; }
+
+        [DomName("download")]
+        String Download { get; set; }
+
+        [DomName("ping")]
+        ISettableTokenList Ping { get; }
+
+        [DomName("rel")]
+        String Rel { get; set; }
+
+        [DomName("relList")]
+        ITokenList RelList { get; }
+
+        [DomName("hreflang")]
+        String HrefLang { get; set; }
+
+        [DomName("type")]
+        String Type { get; }
+
+        [DomName("text")]
+        String Text { get; }
+    }
+}
