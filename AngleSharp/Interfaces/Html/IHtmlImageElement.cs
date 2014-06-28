5fde5fff34ef8664b99d6fec54168638d48ad466
Florian Rappl <rappl.florian@gmail.com>
2014-06-28 13:45:58 +0200

    Changed documentation

--- AngleSharp/Interfaces/Html/IHtmlImageElement.cs
+++ AngleSharp/Interfaces/Html/IHtmlImageElement.cs
@@ -8,34 +8,66 @@
     [DomName("HTMLImageElement")]
     public interface IHtmlImageElement : IHtmlElement
     {
+        /// <summary>
+        /// Gets or sets the alternative text.
+        /// </summary>
         [DomName("alt")]
-        String alt { get; set; }
+        String Alt { get; set; }
 
+        /// <summary>
+        /// Gets or sets the image source.
+        /// </summary>
         [DomName("src")]
-        String src { get; set; }
+        String Src { get; set; }
 
+        /// <summary>
+        /// Gets or sets the cross-origin attribute.
+        /// </summary>
         [DomName("crossOrigin")]
-        String crossOrigin { get; set; }
+        String CrossOrigin { get; set; }
 
+        /// <summary>
+        /// Gets or sets the usemap attribute, which indicates that the image has an associated image map.
+        /// </summary>
         [DomName("useMap")]
-        String useMap { get; set; }
+        String UseMap { get; set; }
 
+        /// <summary>
+        /// Gets or sets if the image element is a map.
+        /// The attribute must not be specified on an element that does not
+        /// have an ancestor a element with an href attribute.
+        /// </summary>
         [DomName("isMap")]
-        Boolean isMap { get; set; }
+        Boolean IsMap { get; set; }
 
+        /// <summary>
+        /// Gets or sets the displayed width of the image element.
+        /// </summary>
         [DomName("width")]
-        Int32 width { get; set; }
+        UInt32 Width { get; set; }
 
+        /// <summary>
+        /// Gets or sets the displayed width of the image element.
+        /// </summary>
         [DomName("height")]
-        Int32 height { get; set; }
+        UInt32 Height { get; set; }
 
+        /// <summary>
+        /// Gets the width of the image.
+        /// </summary>
         [DomName("naturalWidth")]
-        Int32 naturalWidth { get; }
+        UInt32 NaturalWidth { get; }
 
+        /// <summary>
+        /// Gets the height of the image.
+        /// </summary>
         [DomName("naturalHeight")]
-        Int32 naturalHeight { get; }
+        UInt32 NaturalHeight { get; }
 
+        /// <summary>
+        /// Gets if the image is completely available.
+        /// </summary>
         [DomName("complete")]
-        Boolean complete { get; }
+        Boolean Complete { get; }
     }
 }
