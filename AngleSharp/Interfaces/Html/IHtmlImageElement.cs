ece10f647a3238dbf32b2ad615476dca5fd3d094
Florian Rappl <rappl.florian@gmail.com>
2014-06-28 13:43:00 +0200

    Added the HTML image IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlImageElement.cs
@@ -0,0 +1,41 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the image HTML element.
+    /// </summary>
+    [DomName("HTMLImageElement")]
+    public interface IHtmlImageElement : IHtmlElement
+    {
+        [DomName("alt")]
+        String alt { get; set; }
+
+        [DomName("src")]
+        String src { get; set; }
+
+        [DomName("crossOrigin")]
+        String crossOrigin { get; set; }
+
+        [DomName("useMap")]
+        String useMap { get; set; }
+
+        [DomName("isMap")]
+        Boolean isMap { get; set; }
+
+        [DomName("width")]
+        Int32 width { get; set; }
+
+        [DomName("height")]
+        Int32 height { get; set; }
+
+        [DomName("naturalWidth")]
+        Int32 naturalWidth { get; }
+
+        [DomName("naturalHeight")]
+        Int32 naturalHeight { get; }
+
+        [DomName("complete")]
+        Boolean complete { get; }
+    }
+}
