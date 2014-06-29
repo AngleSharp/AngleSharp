d4113ff8c24413f4deb389ba3a7657db8da8ced4
Florian Rappl <rappl.florian@gmail.com>
2014-06-29 08:00:54 +0200

    Added the WindowProxy interface.

---
+++ AngleSharp/Interfaces/IWindowProxy.cs
@@ -0,0 +1,11 @@
+﻿namespace AngleSharp.DOM
+{
+    /// <summary>
+    /// Indistinguishable from that Window object in every way
+    /// until the browsing context is navigated.
+    /// </summary>
+    [DomName("WindowProxy")]
+    public interface IWindowProxy
+    {
+    }
+}
