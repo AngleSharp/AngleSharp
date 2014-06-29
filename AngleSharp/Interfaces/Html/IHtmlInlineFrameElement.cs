d573a16eba6fb499d2d4a96b2c64c76394217a8b
Florian Rappl <rappl.florian@gmail.com>
2014-06-29 08:04:01 +0200

    Added the iframe IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlInlineFrameElement.cs
@@ -0,0 +1,41 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the iframe HTML element.
+    /// </summary>
+    [DomName("HTMLIFrameElement")]
+    interface IHtmlInlineFrameElement : IHtmlElement
+    {
+        [DomName("src")]
+        String Src { get; set; }
+
+        [DomName("srcdoc")]
+        String SrcDoc { get; set; }
+
+        [DomName("name")]
+        String Name { get; set; }
+
+        [DomName("sandbox")]
+        ISettableTokenList Sandbox { get; }
+
+        [DomName("seamless")]
+        Boolean Seamless { get; set; }
+
+        [DomName("allowFullscreen")]
+        Boolean AllowFullscreen { get; set; }
+
+        [DomName("width")]
+        String Width { get; set; }
+
+        [DomName("height")]
+        String Height { get; set; }
+
+        [DomName("contentDocument")]
+        IDocument ContentDocument { get; }
+
+        [DomName("contentWindow")]
+        IWindowProxy ContentWindow { get; }
+    }
+}
