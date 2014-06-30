828cb17e2029bd694154aeb81c93482316558a82
Florian Rappl <rappl.florian@gmail.com>
2014-06-30 21:43:02 +0200

    Added the td element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlTableCellElement.cs
@@ -0,0 +1,36 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Reprsents the td HTML element.
+    /// </summary>
+    [DomName("HTMLTableCellElement")]
+    public interface IHtmlTableCellElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the number of columns spanned by cell. 
+        /// </summary>
+        [DomName("colSpan")]
+        UInt32 ColSpan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of rows spanned by cell. 
+        /// </summary>
+        [DomName("rowSpan")]
+        UInt32 RowSpan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of id attribute values for header cells. 
+        /// </summary>
+        [DomName("headers")]
+        ISettableTokenList Headers { get; }
+
+        /// <summary>
+        /// Gets the index of this cell in the row, starting from 0.
+        /// This index is in document tree order and not display order.
+        /// </summary>
+        [DomName("cellIndex")]
+        Int32 CellIndex { get; }
+    }
+}
