29db5daba8b751e3c86de671b4ace9f1fe07579d
Florian Rappl <rappl.florian@gmail.com>
2014-06-30 10:38:05 +0200

    Added the HTML param element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlParamElement.cs
@@ -0,0 +1,17 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the param HTML element.
+    /// </summary>
+    [DomName("HTMLParamElement")]
+    public interface IHtmlParamElement : IHtmlElement
+    {
+        [DomName("name")]
+        String Name { get; set; }
+
+        [DomName("value")]
+        String Value { get; set; }
+    }
+}
