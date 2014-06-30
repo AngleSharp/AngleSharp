18bbdf39fe824a71c47ca23ce472a9d416bd0260
Florian Rappl <rappl.florian@gmail.com>
2014-06-30 10:38:48 +0200

    Added documentation.

--- AngleSharp/Interfaces/Html/IHtmlParamElement.cs
+++ AngleSharp/Interfaces/Html/IHtmlParamElement.cs
@@ -8,9 +8,15 @@
     [DomName("HTMLParamElement")]
     public interface IHtmlParamElement : IHtmlElement
     {
+        /// <summary>
+        /// Gets or sets the name of the parameter.
+        /// </summary>
         [DomName("name")]
         String Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of the parameter.
+        /// </summary>
         [DomName("value")]
         String Value { get; set; }
     }
