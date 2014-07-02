bb973a0804e53a89f37baef3156eceb62d36c6f4
Florian Rappl <rappl.florian@gmail.com>
2014-07-02 09:31:58 +0200

    Added the map element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlMapElement.cs
@@ -0,0 +1,31 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the map HTML element.
+    /// </summary>
+    [DomName("HTMLMapElement")]
+    public interface IHtmlMapElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [DomName("name")]
+        String Name { get; set; }
+
+        /// <summary>
+        /// Gets a collection representing the area elements
+        /// associated to this map.
+        /// </summary>
+        [DomName("areas")]
+        IHtmlCollection Areas { get; }
+
+        /// <summary>
+        /// Gets a collection representing the img and object
+        /// elements associated to this element.
+        /// </summary>
+        [DomName("images")]
+        IHtmlCollection Images { get; }
+    }
+}
