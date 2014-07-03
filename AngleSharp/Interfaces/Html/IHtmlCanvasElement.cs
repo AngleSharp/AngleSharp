bf11cfe07decef121337edaaf50d66966a198372
Florian Rappl <rappl.florian@gmail.com>
2014-07-03 08:31:41 +0200

    Organized usings

--- AngleSharp/Interfaces/Html/IHtmlCanvasElement.cs
+++ AngleSharp/Interfaces/Html/IHtmlCanvasElement.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AngleSharp.DOM.Html
+﻿namespace AngleSharp.DOM.Html
 {
+    using System;
+
     /// <summary>
     /// Represents the canvas HTML element.
     /// </summary>
