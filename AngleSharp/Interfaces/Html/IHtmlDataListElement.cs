b58e70e464d8db49ed8bf287075c0e90fff364a3
Florian Rappl <rappl.florian@gmail.com>
2014-07-03 08:20:52 +0200

    Added the datalist IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlDataListElement.cs
@@ -0,0 +1,15 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    /// <summary>
+    /// Represents the datalist HTML element.
+    /// </summary>
+    [DomName("HTMLDataListElement")]
+    public interface IHtmlDataListElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets the associated options.
+        /// </summary>
+        [DomName("options")]
+        IHtmlCollection Options { get; }
+    }
+}
