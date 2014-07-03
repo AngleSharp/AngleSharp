8809e7c3a4574d1d53d80de512c477d2134156ac
Florian Rappl <rappl.florian@gmail.com>
2014-07-03 08:23:58 +0200

    Added the optgroup IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlOptionsGroupElement.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the optgroup HTML element.
+    /// </summary>
+    [DomName("HTMLOptGroupElement")]
+    public interface IHtmlOptionsGroupElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets if the optgroup is enabled or disabled.
+        /// </summary>
+        [DomName("disabled")]
+        Boolean Disabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the label.
+        /// </summary>
+        [DomName("label")]
+        String Label { get; set; }
+    }
+}
