25ef5a7e0ce1fab901c05b00174dd29651297b4a
Florian Rappl <rappl.florian@gmail.com>
2014-07-04 08:27:17 +0200

    Added the progress IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlProgressElement.cs
@@ -0,0 +1,35 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the progress HTML element.
+    /// </summary>
+    [DomName("HTMLProgressElement")]
+    public interface IHtmlProgressElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the current value.
+        /// </summary>
+        [DomName("value")]
+        Double Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value.
+        /// </summary>
+        [DomName("max")]
+        Double Max { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        [DomName("position")]
+        Double Position { get; }
+
+        /// <summary>
+        /// Gets the list of assigned labels.
+        /// </summary>
+        [DomName("labels")]
+        INodeList Labels { get; }
+    }
+}
