9e71fceb9368024666d9102b18344c55d79aed3c
Florian Rappl <rappl.florian@gmail.com>
2014-07-05 10:31:18 +0200

    Implemented the dialog IDL, removed public modifier

--- AngleSharp/DOM/Html/Semantic/HTMLDialogElement.cs
+++ AngleSharp/DOM/Html/Semantic/HTMLDialogElement.cs
@@ -5,13 +5,24 @@
     /// <summary>
     /// Represents the object for HTML dialog elements.
     /// </summary>
-    [DomName("HTMLDialogElement")]
-    public sealed class HTMLDialogElement : HTMLElement
+    sealed class HTMLDialogElement : HTMLElement, IHtmlDialogElement
     {
+        #region Fields
+
+        String _returnValue;
+
+        #endregion
+
+        #region ctor
+
         internal HTMLDialogElement()
         {
             _name = Tags.Dialog;
         }
+
+        #endregion
+
+        #region Internal Properties
 
         /// <summary>
         /// Gets if the node is in the special category.
@@ -20,5 +31,41 @@
         {
             get { return false; }
         }
+
+        #endregion
+
+        #region Properties
+
+        public Boolean Open
+        {
+            get { return GetAttribute(AttributeNames.Open) != null; }
+            set { SetAttribute(AttributeNames.Open, value ? String.Empty : null); }
+        }
+
+        public String ReturnValue
+        {
+            get { return _returnValue; }
+            set { _returnValue = value; }
+        }
+
+        public void Show(IElement anchor = null)
+        {
+            Open = true;
+            //TODO
+        }
+
+        public void ShowModal(IElement anchor = null)
+        {
+            Open = true;
+            //TODO
+        }
+
+        public void Close(String returnValue = null)
+        {
+            Open = false;
+            ReturnValue = returnValue;
+        }
+
+        #endregion
     }
 }
