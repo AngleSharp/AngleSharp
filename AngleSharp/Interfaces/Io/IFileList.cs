4fc5f4c1418205f3704dd7a1505700ca32439406
Florian Rappl <rappl.florian@gmail.com>
2014-07-09 09:40:48 +0200

    Added file API IDL

---
+++ AngleSharp/Interfaces/Io/IFileList.cs
@@ -0,0 +1,26 @@
+﻿namespace AngleSharp.DOM.Io
+{
+    using System;
+
+    /// <summary>
+    /// Represents a list of files.
+    /// http://dev.w3.org/2006/webapi/FileAPI/#dfn-filelist
+    /// </summary>
+    [DomName("FileList")]
+    public interface IFileList
+    {
+        /// <summary>
+        /// Gets the file at the specified index.
+        /// </summary>
+        /// <param name="index">The index of the file.</param>
+        /// <returns>The file at the provided index.</returns>
+        [DomName("item")]
+        IFile this[Int32 index] { get; }
+
+        /// <summary>
+        /// Gets the number of files in the list.
+        /// </summary>
+        [DomName("length")]
+        Int32 Length { get; }
+    }
+}
