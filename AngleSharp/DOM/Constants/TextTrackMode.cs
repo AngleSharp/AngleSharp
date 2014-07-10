97cf4442aba2681b219892abbe107fdcf9d07e32
Florian Rappl <rappl.florian@gmail.com>
2014-07-10 11:07:08 +0200

    Added the text track mode enumeration

---
+++ AngleSharp/DOM/Constants/TextTrackMode.cs
@@ -0,0 +1,25 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    /// <summary>
+    /// The modes of a text track.
+    /// </summary>
+    [DomName("TextTrackMode")]
+    public enum TextTrackMode
+    {
+        /// <summary>
+        /// The text track is disabled.
+        /// </summary>
+        [DomName("disabled")]
+        Disabled,
+        /// <summary>
+        /// The text track is hidden.
+        /// </summary>
+        [DomName("hidden")]
+        Hidden,
+        /// <summary>
+        /// The text track is actually shown.
+        /// </summary>
+        [DomName("showing")]
+        Showing
+    }
+}
