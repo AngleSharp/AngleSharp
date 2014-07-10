94e17094962c7cfadeca66d0d65e6339f7d87258
Florian Rappl <rappl.florian@gmail.com>
2014-07-10 10:49:23 +0200

    Added the source element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlSourceElement.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the source HTML element.
+    /// </summary>
+    [DomName("HTMLSourceElement")]
+    public interface IHtmlSourceElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the URL for the media resource.
+        /// </summary>
+        [DomName("src")]
+        String Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the media source.
+        /// </summary>
+        [DomName("type")]
+        String Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the intended type of the media resource.
+        /// </summary>
+        [DomName("media")]
+        String Media { get; set; }
+    }
+}
