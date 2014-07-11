797c6f761b47891e38c31dad3483e9ba9cde2fe0
Florian Rappl <rappl.florian@gmail.com>
2014-07-11 13:19:57 +0200

    Added the video tracklist IDL

---
+++ AngleSharp/Interfaces/Html/Media/IVideoTrackList.cs
@@ -0,0 +1,56 @@
+﻿namespace AngleSharp.DOM.Html.Media
+{
+    using System;
+
+    /// <summary>
+    /// Represents a list of video tracks.
+    /// </summary>
+    [DomName("VideoTrackList")]
+    public interface IVideoTrackList : IEventTarget
+    {
+        /// <summary>
+        /// Gets the number of tracks.
+        /// </summary>
+        [DomName("length")]
+        Int32 Length { get; }
+
+        /// <summary>
+        /// Gets the currently selected index.
+        /// </summary>
+        [DomName("selectedIndex")]
+        Int32 SelectedIndex { get; }
+
+        /// <summary>
+        /// Gets the track at the given index.
+        /// </summary>
+        /// <param name="index">The 0-based track index.</param>
+        /// <returns>The track at the position.</returns>
+        IVideoTrack this[Int32 index] { get; }
+
+        /// <summary>
+        /// Gets the track with the specified id.
+        /// </summary>
+        /// <param name="id">The HTML id of the track.</param>
+        /// <returns>The track with the given id, if any.</returns>
+        [DomName("getTrackById")]
+        IVideoTrack GetTrackById(String id);
+
+        /// <summary>
+        /// Event triggered after changing contents.
+        /// </summary>
+        [DomName("onchange")]
+        event EventListener Changed;
+
+        /// <summary>
+        /// Event triggered after adding a track.
+        /// </summary>
+        [DomName("onaddtrack")]
+        event EventListener TrackAdded;
+
+        /// <summary>
+        /// Event triggered after removing a track.
+        /// </summary>
+        [DomName("onremovetrack")]
+        event EventListener TrackRemoved;
+    }
+}
