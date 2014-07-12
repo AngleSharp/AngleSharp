45c53551d47d5cb3ae4d5a65a3f1c02d8b55a0fe
Florian Rappl <rappl.florian@gmail.com>
2014-07-12 12:10:09 +0200

    Added the media enumerations and added DomName attributes.

---
+++ AngleSharp/DOM/Constants/MediaNetworkState.cs
@@ -0,0 +1,33 @@
+﻿namespace AngleSharp.DOM.Media
+{
+    /// <summary>
+    /// An enumeration of possible network states.
+    /// </summary>
+    public enum MediaNetworkState : ushort
+    {
+        /// <summary>
+        /// The element has not yet been initialized.
+        /// Everything is in initial state.
+        /// </summary>
+        [DomName("NETWORK_EMPTY")]
+        Empty = 0,
+        /// <summary>
+        /// The element's resource selection alg. is active.
+        /// No network usage at the moment, but nothing
+        /// loaded.
+        /// </summary>
+        [DomName("NETWORK_IDLE")]
+        Idle = 1,
+        /// <summary>
+        /// The download is in progress.
+        /// </summary>
+        [DomName("NETWORK_LOADING")]
+        Loading = 2,
+        /// <summary>
+        /// The element's resource selection alg. is active,
+        /// but has not yet found a resource to use.
+        /// </summary>
+        [DomName("NETWORK_NO_SOURCE")]
+        NoSource = 3
+    }
+}
--- AngleSharp/DOM/Html/Media/HTMLMediaElement.cs
+++ AngleSharp/DOM/Html/Media/HTMLMediaElement.cs
@@ -1,5 +1,6 @@
 ﻿namespace AngleSharp.DOM.Html
 {
+    using AngleSharp.DOM.Media;
     using System;
 
     /// <summary>
@@ -173,68 +174,6 @@
         public void Pause()
         {
             //TODO
-        }
-
-        #endregion
-
-        #region Enumerations
-
-        /// <summary>
-        /// An enumeration of possible network states.
-        /// </summary>
-        public enum MediaNetworkState : ushort
-        {
-            /// <summary>
-            /// The element has not yet been initialized.
-            /// Everything is in initial state.
-            /// </summary>
-            Empty = 0,
-            /// <summary>
-            /// The element's resource selection alg. is active.
-            /// No network usage at the moment, but nothing
-            /// loaded.
-            /// </summary>
-            Idle = 1,
-            /// <summary>
-            /// The download is in progress.
-            /// </summary>
-            Loading = 2,
-            /// <summary>
-            /// The element's resource selection alg. is active,
-            /// but has not yet found a resource to use.
-            /// </summary>
-            NoSource = 3
-        }
-
-        /// <summary>
-        /// An enumeration of media ready states.
-        /// </summary>
-        public enum MediaReadyState : ushort
-        {
-            /// <summary>
-            /// No information is available.
-            /// </summary>
-            Nothing = 0,
-            /// <summary>
-            /// Enough information obtained such that the duration of the
-            /// resource is available.
-            /// </summary>
-            Metadata = 1,
-            /// <summary>
-            /// Data for immediate playback is available, but not enough
-            /// to advance.
-            /// </summary>
-            CurrentData = 2,
-            /// <summary>
-            /// Enough data for the current and further positions are 
-            /// available.
-            /// </summary>
-            FutureData = 3,
-            /// <summary>
-            /// All conditions are met and playback should immediately
-            /// execute.
-            /// </summary>
-            EnoughData = 4
         }
 
         #endregion
