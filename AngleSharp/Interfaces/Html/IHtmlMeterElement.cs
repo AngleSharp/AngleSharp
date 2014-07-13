2762982bac48e9259c474b70078af62316f2e15c
Florian Rappl <rappl.florian@gmail.com>
2014-07-13 10:07:52 +0200

    Added the meter element IDL

---
+++ AngleSharp/Interfaces/Html/IHtmlMeterElement.cs
@@ -0,0 +1,53 @@
+﻿namespace AngleSharp.DOM.Html
+{
+    using System;
+
+    /// <summary>
+    /// Represents the meter HTML element.
+    /// </summary>
+    [DomName("HTMLMeterElement")]
+    public interface IHtmlMeterElement : IHtmlElement
+    {
+        /// <summary>
+        /// Gets or sets the current value.
+        /// </summary>
+        [DomName("value")]
+        Double Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum value.
+        /// </summary>
+        [DomName("min")]
+        Double Minimum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value.
+        /// </summary>
+        [DomName("max")]
+        Double Maximum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the low value.
+        /// </summary>
+        [DomName("low")]
+        Double Low { get; set; }
+
+        /// <summary>
+        /// Gets or sets the high value.
+        /// </summary>
+        [DomName("high")]
+        Double High { get; set; }
+
+        /// <summary>
+        /// Gets or sets the optimum value.
+        /// </summary>
+        [DomName("optimum")]
+        Double Optimum { get; set; }
+
+        /// <summary>
+        /// Gets the list of assigned labels.
+        /// </summary>
+        [DomName("labels")]
+        INodeList Labels { get; }
+    }
+}
