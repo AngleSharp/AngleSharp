5dd29d35ffa184c9fece01cfd4c5cf207701598d
Florian Rappl <rappl.florian@gmail.com>
2014-07-14 23:04:14 +0200

    Made the quote element public

--- AngleSharp/Interfaces/Html/IHtmlQuoteElement.cs
+++ AngleSharp/Interfaces/Html/IHtmlQuoteElement.cs
@@ -2,9 +2,15 @@
 {
     using System;
 
+    /// <summary>
+    /// Represents the q HTML element.
+    /// </summary>
     [DomName("HTMLQuoteElement")]
-    interface IHtmlQuoteElement : IHtmlElement
+    public interface IHtmlQuoteElement : IHtmlElement
     {
+        /// <summary>
+        /// Gets or sets the citation of the element.
+        /// </summary>
         [DomName("cite")]
         String Citation { get; set; }
     }
