980d78387b0ecc40c0589c63901b585d368506ca
Florian Rappl <rappl.florian@gmail.com>
2014-07-18 20:42:37 +0200

    Implemented the XMLDocument IDL

--- AngleSharp/DOM/Xml/XMLDocument.cs
+++ AngleSharp/DOM/Xml/XMLDocument.cs
@@ -11,5 +11,31 @@
         {
             _contentType = MimeTypes.Xml;
         }
+
+        Boolean IXmlDocument.Load(String url)
+        {
+            Uri uri;
+            _location.Href = url;
+            Cookie = String.Empty;
+
+            if (!Uri.TryCreate(url, UriKind.Absolute, out uri))
+                throw new ArgumentException("The given URL is not valid as an absolute URL.");
+
+            var task = Options.LoadAsync(uri);
+
+            var result = task.ContinueWith(m =>
+            {
+                if (m.IsCompleted && !m.IsFaulted)
+                {
+                    Load(m.Result);
+                    return true;
+                }
+
+                return false;
+            });
+
+            result.Wait();
+            return result.Result;
+        }
     }
 }
--- AngleSharp/Interfaces/Xml/IXmlDocument.cs
+++ AngleSharp/Interfaces/Xml/IXmlDocument.cs
@@ -1,5 +1,7 @@
 ﻿namespace AngleSharp.DOM.Xml
 {
+    using System;
+
     /// <summary>
     /// The XMLDocument interface represent an XML document. It inherits from
     /// the generic Document and do not add any specific methods or properties
@@ -9,5 +11,13 @@
     [DomName("XMLDocument")]
     public interface IXmlDocument : IDocument
     {
+        /// <summary>
+        /// Loads the given URL as an XmlDocument. Clears the current document
+        /// structure and re-fills it with the contents from the provided URL.
+        /// </summary>
+        /// <param name="url">The URL to get the new DOM from.</param>
+        /// <returns>True if the URL could be loaded successfully, otherwise false.</returns>
+        [DomName("load")]
+        Boolean Load(String url);
     }
 }
