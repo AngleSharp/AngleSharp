3d51b6fa8366d63aff0596213ee034164c416781
Florian Rappl <rappl.florian@gmail.com>
2014-07-19 14:10:00 +0200

    Extended the style engine interface and implemented it in the default CSS engine

---
+++ AngleSharp/Infrastructure/CssStyleEngine.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharp.Infrastructure
+{
+    using AngleSharp.DOM;
+    using AngleSharp.DOM.Css;
+    using AngleSharp.Parser.Css;
+    using System;
+
+    /// <summary>
+    /// The CSS style engine for creating CSSStyleSheet instances.
+    /// </summary>
+    public class CssStyleEngine : IStyleEngine
+    {
+        /// <summary>
+        /// Gets the type for the CSS style engine.
+        /// </summary>
+        public String Type
+        {
+            get { return MimeTypes.Css; }
+        }
+
+        /// <summary>
+        /// Creates a style sheet for the given source.
+        /// </summary>
+        /// <param name="source">The source code describing the style sheet.</param>
+        /// <returns>The created style sheet.</returns>
+        public StyleSheet CreateStyleSheetFor(String source)
+        {
+            var style = new CSSStyleSheet();
+            var parser = new CssParser(style, source);
+            parser.Parse();
+            return style;
+        }
+    }
+}
--- AngleSharp/Infrastructure/IStyleEngine.cs
+++ AngleSharp/Infrastructure/IStyleEngine.cs
@@ -1,5 +1,6 @@
 ﻿namespace AngleSharp.Infrastructure
 {
+    using AngleSharp.DOM;
     using System;
 
     /// <summary>
@@ -12,5 +13,12 @@
         /// The type of the styling set.
         /// </summary>
         String Type { get; }
+
+        /// <summary>
+        /// Creates a style sheet for the given source.
+        /// </summary>
+        /// <param name="source">The source code describing the style sheet.</param>
+        /// <returns>The created style sheet.</returns>
+        StyleSheet CreateStyleSheetFor(String source);
     }
 }
