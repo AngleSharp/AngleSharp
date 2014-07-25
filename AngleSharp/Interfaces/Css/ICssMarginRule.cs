c8fed771d2bf26bc61832e124eae321973203843
Florian Rappl <rappl.florian@gmail.com>
2014-07-26 00:44:42 +0200

    Added the CSSMarginRule and CSSNamespaceRule IDL

---
+++ AngleSharp/Interfaces/Css/ICssMarginRule.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents a margin CSS rule (e.g. in an @page at-rule).
+    /// </summary>
+    [DomName("CSSMarginRule")]
+    public interface ICssMarginRule : ICssRule
+    {
+        /// <summary>
+        /// Gets the name of the margin rule.
+        /// </summary>
+        [DomName("name")]
+        String Name { get; }
+
+        /// <summary>
+        /// Gets the style object for the margin at-rule.
+        /// </summary>
+        [DomName("style")]
+        CSSStyleDeclaration Style { get; }
+    }
+}
---
+++ AngleSharp/Interfaces/Css/ICssNamespaceRule.cs
@@ -0,0 +1,24 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents a namespace declaration CSS rule.
+    /// </summary>
+    [DomName("CSSNamespaceRule")]
+    public interface ICssNamespaceRule : ICssRule
+    {
+        /// <summary>
+        /// Gets the URI of the given namespace.
+        /// </summary>
+        [DomName("namespaceURI")]
+        String NamespaceUri { get; set; }
+
+        /// <summary>
+        /// Gets the prefix associated to this namespace.
+        /// If there is no such prefix, returns null.
+        /// </summary>
+        [DomName("prefix")]
+        String Prefix { get; set; }
+    }
+}
