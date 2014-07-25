d8a39fe8f76a9a9ec4df85be2c132a104dec3825
Florian Rappl <rappl.florian@gmail.com>
2014-07-26 00:33:18 +0200

    Added the CSSMediaRule and CSSPageRule IDLs

---
+++ AngleSharp/Interfaces/Css/ICssMediaRule.cs
@@ -0,0 +1,15 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    /// <summary>
+    /// Represents a @media CSS rule.
+    /// </summary>
+    [DomName("CSSMediaRule")]
+    public interface ICssMediaRule : ICssGroupingRule
+    {
+        /// <summary>
+        /// Gets a list of media types for this rule.
+        /// </summary>
+        [DomName("media")]
+        IMediaList Media { get; }
+    }
+}
---
+++ AngleSharp/Interfaces/Css/ICssPageRule.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents the @page CSS rule.
+    /// </summary>
+    [DomName("CSSPageRule")]
+    public interface ICssPageRule : ICssGroupingRule
+    {
+        /// <summary>
+        /// Gets or sets the textual representation of the selector for this rule, e.g. "h1,h2".
+        /// </summary>
+        [DomName("selectorText")]
+        String SelectorText { get; set; }
+
+        /// <summary>
+        /// Gets the CSSStyleDeclaration object for the rule.
+        /// </summary>
+        [DomName("style")]
+        CSSStyleDeclaration Style { get; }
+    }
+}
