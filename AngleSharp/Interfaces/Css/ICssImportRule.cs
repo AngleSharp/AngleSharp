00f2978537124ea0b403f9b326b4bc2784882090
Florian Rappl <rappl.florian@gmail.com>
2014-07-25 23:33:14 +0200

    Added the MediaList and CSSImportRule IDL

---
+++ AngleSharp/Interfaces/Css/ICssImportRule.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents the @import CSS rule.
+    /// </summary>
+    [DomName("CSSImportRule")]
+    public interface ICssImportRule : ICssRule
+    {
+        /// <summary>
+        /// Gets the location of the style sheet to be imported. 
+        /// </summary>
+        [DomName("href")]
+        String Href { get; }
+
+        /// <summary>
+        /// Gets a list of media types for which this style sheet may be used.
+        /// </summary>
+        [DomName("media")]
+        IMediaList Media { get; }
+
+        /// <summary>
+        /// Gets the style sheet referred to by this rule, if it has been loaded. 
+        /// </summary>
+        [DomName("styleSheet")]
+        ICssStyleSheet Sheet { get; }
+    }
+}
