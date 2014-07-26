e6aa8f39d19c7b2b482392bc93533c853ea7c73a
Florian Rappl <rappl.florian@gmail.com>
2014-07-26 13:17:09 +0200

    Added the CSSFontFeatureValuesRule IDL

---
+++ AngleSharp/Interfaces/Css/ICssFontFeatureValuesRule.cs
@@ -0,0 +1,21 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents a @font-feature-values CSS rule.
+    /// </summary>
+    [DomName("CSSFontFeatureValuesRule")]
+    public interface ICssFontFeatureValuesRule : ICssRule
+    {
+        /// <summary>
+        /// Gets or sets the list of one or more font families for
+        /// which a given set of feature values is defined.
+        /// </summary>
+        [DomName("fontFamily")]
+        String Family { get; set; }
+
+        //More information available at:
+        //http://dev.w3.org/csswg/css-fonts/#om-fontfeaturevalues
+    }
+}
