554c86baa60bbdff48ae4e3bfbd24bc99d6a4060
Florian Rappl <rappl.florian@gmail.com>
2014-07-26 13:24:18 +0200

    Added the CSSDocumentRule IDL

---
+++ AngleSharp/Interfaces/Css/ICssDocumentRule.cs
@@ -0,0 +1,10 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    /// <summary>
+    /// Represents a document CSS rule.
+    /// </summary>
+    [DomName("CSSDocumentRule")]
+    public interface ICssDocumentRule : ICssConditionRule
+    {
+    }
+}
