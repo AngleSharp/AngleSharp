af788f2564e56a53891fa8b223f90480daa58bd9
Florian Rappl <rappl.florian@gmail.com>
2014-07-26 17:18:04 +0200

    Added the HTMLAllCollection IDL

---
+++ AngleSharp/Interfaces/IHtmlAllCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace AngleSharp.DOM
+{
+    using System;
+
+    /// <summary>
+    /// An HTMLAllCollection is always rooted at document and
+    /// matching all elements. It represents the tree of elements
+    /// in a one-dimensional fashion.
+    /// </summary>
+    [DomName("HTMLAllCollection")]
+    public interface IHtmlAllCollection : IHtmlCollection
+    {
+    }
+}
