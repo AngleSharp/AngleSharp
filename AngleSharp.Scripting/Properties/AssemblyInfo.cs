1c4c8d29b19e84824818b3bb4a0f30946099aee6
Florian Rappl <rappl.florian@gmail.com>
2014-08-04 23:21:48 +0200

    Renamed AngleSharp.Jurassic to AngleSharp.Scripting

---
+++ AngleSharp.Scripting/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Scripting")]
+[assembly: AssemblyDescription("Extension for AngleSharp to enable JavaScript scripts using the Jurassic JavaScript to MSIL compiler.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngleSharp.Scripting")]
+[assembly: AssemblyCopyright("Copyright © Florian Rappl et al. 2013-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("be485498-5294-46d0-b1ba-54f095eba40d")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
