6f405bcce31101f86773bbee3c02f7597e1d42d4
Florian Rappl <rappl.florian@gmail.com>
2014-08-06 00:09:12 +0200

    Replaced Jurassic by JsInt due to global object and PCL

---
+++ AngleSharp.Scripting/JavaScriptEngine.cs
@@ -0,0 +1,47 @@
+﻿namespace AngleSharp.Scripting
+{
+    using AngleSharp.Infrastructure;
+    using Jint;
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class JavaScriptEngine : IScriptEngine
+    {
+        readonly Engine _engine;
+        readonly Window _window;
+
+        public JavaScriptEngine()
+        {
+            _engine = new Engine();
+        }
+
+        public String Type
+        {
+            get { return "text/javascript"; }
+        }
+
+        public void Evaluate(String source, ScriptOptions options)
+        {
+            SetOptions(options);
+            _engine.EnterExecutionContext(_engine.GlobalEnvironment, _engine.GlobalEnvironment, _window);
+            _engine.Execute(source);
+            _engine.LeaveExecutionContext();
+
+        }
+
+        public void Evaluate(Stream source, ScriptOptions options)
+        {
+            var reader = new StreamReader(source, options.Encoding ?? Encoding.UTF8, true);
+            var content = reader.ReadToEnd();
+            reader.Close();
+            Evaluate(content, options);
+        }
+
+        void SetOptions(ScriptOptions options)
+        {
+            _window.Document = options.Document;
+            _window.Window = options.Context;
+        }
+    }
+}
--- AngleSharp.Scripting/Window.cs
+++ AngleSharp.Scripting/Window.cs
@@ -1,85 +1,29 @@
 ﻿namespace AngleSharp.Scripting
 {
     using AngleSharp.DOM;
-    using AngleSharp.DOM.Css;
-    using AngleSharp.DOM.Navigator;
-    using Jurassic;
-    using Jurassic.Library;
-    using System;
+    using Jint;
+    using Jint.Native.Object;
+    using Jint.Runtime.Descriptors;
 
-    public class Window : ObjectInstance, IWindow
+    public class Window : ObjectInstance
     {
-        public Window(ScriptEngine engine)
+        public Window(Engine engine)
             : base(engine)
         {
+            this.DefineOwnProperty("name", new PropertyDescriptor(), true);
             //this.DefineProperty("name", new PropertyDescriptor(new ), true);
+        }
+
+        public IWindow Window
+        {
+            get;
+            set;
         }
 
         public IDocument Document
         {
-            get { throw new NotImplementedException(); }
-        }
-
-        public ICssStyleDeclaration GetComputedStyle(IElement element, String pseudo = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public String Name
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public INavigator Navigator
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Int32 OuterHeight
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Int32 OuterWidth
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public IWindowProxy Proxy
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Int32 ScreenX
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Int32 ScreenY
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public void AddEventListener(String type, EventListener callback = null, Boolean capture = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Boolean Dispatch(IEvent ev)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveEventListener(String type, EventListener callback = null, Boolean capture = false)
-        {
-            throw new NotImplementedException();
+            get;
+            set;
         }
     }
 }
