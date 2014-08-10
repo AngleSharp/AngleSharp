38d0dc2133e594652b305f575594284fa8b68588
Florian Rappl <rappl.florian@gmail.com>
2014-08-10 14:06:13 +0200

    Added enumerations for mouse event arguments

---
+++ AngleSharp/DOM/Constants/WheelMode.cs
@@ -0,0 +1,24 @@
+﻿namespace AngleSharp.DOM
+{
+    /// <summary>
+    /// Enumeration with the various mouse wheel modes.
+    /// </summary>
+    public enum WheelMode
+    {
+        /// <summary>
+        /// The unit of change is pixels.
+        /// </summary>
+        [DomName("DOM_DELTA_PIXEL")]
+        Pixel = 0x0,
+        /// <summary>
+        /// The unit of change is lines.
+        /// </summary>
+        [DomName("DOM_DELTA_LINE")]
+        Line = 0x1,
+        /// <summary>
+        /// The unit of change is pages.
+        /// </summary>
+        [DomName("DOM_DELTA_PAGE")]
+        Page = 0x2
+    }
+}
