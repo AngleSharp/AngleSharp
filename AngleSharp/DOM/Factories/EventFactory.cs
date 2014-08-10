3b8eae8f0c5747c40f2f98c629505672f30e1075
Florian Rappl <rappl.florian@gmail.com>
2014-08-10 13:32:27 +0200

    Added focus to the factory

--- AngleSharp/DOM/Factories/EventFactory.cs
+++ AngleSharp/DOM/Factories/EventFactory.cs
@@ -14,6 +14,7 @@
         {
             //The real deal
             AddEventConstructor("event", () => new Event());
+            AddEventConstructor("focusevent", () => new Event());//TODO FocusEvent
             AddEventConstructor("keyboardevent", () => new Event());//TODO KeyboardEvent
             AddEventConstructor("messageevent", () => new Event());//TODO MessageEvent
             AddEventConstructor("mouseevent", () => new Event());//TODO MouseEvent
