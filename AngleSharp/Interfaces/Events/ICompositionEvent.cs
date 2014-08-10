50851ee6f6cc27687547f10d642617bf5f55dd37
Florian Rappl <rappl.florian@gmail.com>
2014-08-10 14:21:30 +0200

    Added CompositionEvent, MouseEvent and WheelEvent IDL

---
+++ AngleSharp/Interfaces/Events/ICompositionEvent.cs
@@ -0,0 +1,28 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents the event arguments for a composed event.
+    /// </summary>
+    [DomName("CompositionEvent")]
+    public interface ICompositionEvent : IUiEvent
+    {
+        /// <summary>
+        /// Gets the associated data.
+        /// </summary>
+        [DomName("data")]
+        String Data { get; }
+
+        /// <summary>
+        /// Initializes the composition event.
+        /// </summary>
+        /// <param name="type">The type of event.</param>
+        /// <param name="bubbles">Determines if the event bubbles.</param>
+        /// <param name="cancelable">Determines if the event is cancelable.</param>
+        /// <param name="view">Sets the associated view for the UI event.</param>
+        /// <param name="data">Sets the data to carry.</param>
+        [DomName("initCompositionEvent")]
+        void Init(String type, Boolean bubbles, Boolean cancelable, IWindow view, String data);
+    }
+}
