bf409a084f0f3ab5411defadd152d8cdf7c80d6e
Florian Rappl <rappl.florian@gmail.com>
2014-08-10 12:23:09 +0200

    Added default implementation of track event

--- AngleSharp/DOM/Events/CustomEvent.cs
+++ AngleSharp/DOM/Events/CustomEvent.cs
@@ -5,7 +5,6 @@
     /// <summary>
     /// Represents a custom event that provides an additional details property.
     /// </summary>
-    /// <typeparam name="T">The type of the details.</typeparam>
     sealed class CustomEvent : Event, ICustomEvent
     {
         #region Fields
---
+++ AngleSharp/DOM/Events/TrackEvent.cs
@@ -0,0 +1,35 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents a track that provides an additional track information.
+    /// </summary>
+    sealed class TrackEvent : Event, ITrackEvent
+    {
+        #region Fields
+
+        Object _track;
+
+        #endregion
+
+        #region Properties
+
+        public Object Track
+        {
+            get { return _track; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Init(String type, Boolean bubbles, Boolean cancelable, Object track)
+        {
+            _track = track;
+            Init(type, bubbles, cancelable);
+        }
+
+        #endregion
+    }
+}
