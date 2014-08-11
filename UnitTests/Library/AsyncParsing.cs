f63c7cd35b1aaf2d500aea7a167a8a3c1f14525a
Florian Rappl <rappl.florian@gmail.com>
2014-08-11 21:14:12 +0200

    Added more async tests

--- UnitTests/Library/AsyncParsing.cs
+++ UnitTests/Library/AsyncParsing.cs
@@ -2,6 +2,8 @@
 using AngleSharp.Parser.Css;
 using AngleSharp.Parser.Html;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
 
 namespace UnitTests
 {
@@ -9,14 +11,14 @@ namespace UnitTests
     public class AsyncParsingTests
     {
         [TestMethod]
-        public void TestAsyncCssParsing()
+        public void TestAsyncCssParsingFromStream()
         {
             var source = "h1 { color: red; } h2 { color: blue; } p { font-family: Arial; } div { margin: 10 }";
-            var parser = new CssParser(source, Configuration.Default);
+            var content = Encoding.UTF8.GetBytes(source);
+            var parser = new CssParser(new MemoryStream(content), Configuration.Default);
             var task = parser.ParseAsync();
             Assert.IsFalse(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
-            Assert.IsFalse(task.IsCompleted);
             task.Wait();
             Assert.IsTrue(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
@@ -24,14 +26,44 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void TestAsyncHtmlParsing()
+        public void TestAsyncHtmlParsingFromStream()
+        {
+            var source = "<html><head><title>My test</title></head><body><p>Some text</p></body></html>";
+            var content = Encoding.UTF8.GetBytes(source);
+            var parser = new HtmlParser(new MemoryStream(content), Configuration.Default);
+            var task = parser.ParseAsync();
+            Assert.IsFalse(task.IsCompleted);
+            Assert.IsNotNull(parser.Result);
+            task.Wait();
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsNotNull(parser.Result);
+            Assert.AreEqual("My test", parser.Result.Title);
+            Assert.AreEqual(1, parser.Result.Body.ChildElementCount);
+            Assert.AreEqual("Some text", parser.Result.Body.Children[0].TextContent);
+        }
+
+        [TestMethod]
+        public void TestAsyncCssParsingFromString()
+        {
+            var source = "h1 { color: red; } h2 { color: blue; } p { font-family: Arial; } div { margin: 10 }";
+            var parser = new CssParser(source, Configuration.Default);
+            var task = parser.ParseAsync();
+            Assert.IsFalse(task.IsCompleted);
+            Assert.IsNotNull(parser.Result);
+            task.Wait();
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsNotNull(parser.Result);
+            Assert.AreEqual(4, parser.Result.Rules.Length);
+        }
+
+        [TestMethod]
+        public void TestAsyncHtmlParsingFromString()
         {
             var source = "<html><head><title>My test</title></head><body><p>Some text</p></body></html>";
             var parser = new HtmlParser(source, Configuration.Default);
             var task = parser.ParseAsync();
             Assert.IsFalse(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
-            Assert.IsFalse(task.IsCompleted);
             task.Wait();
             Assert.IsTrue(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
