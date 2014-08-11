b2b42af72fcf193377caa4d1a375b0cc48433a92
Florian Rappl <rappl.florian@gmail.com>
2014-08-12 00:06:05 +0200

    Added event listener sample

--- ConsoleInteraction/Samples.cs
+++ ConsoleInteraction/Samples.cs
@@ -16,6 +16,7 @@ namespace ConsoleInteraction
             Construction();
             SimpleScriptingSample();
             ExtendedScriptingSample();
+            EventScriptingExample();
         }
 
         static void FirstExample() 
@@ -192,6 +193,53 @@ div {
 
             //HTML will have changed completely (e.g., no more script element)
             Console.WriteLine(document.DocumentElement.OuterHtml);
+        }
+
+        public static void EventScriptingExample()
+        {
+            //We require a custom configuration
+            var config = new Configuration();
+
+            //Including a script engine
+            config.Register(new JavaScriptEngine());
+
+            //And enabling scripting + styling (should be enabled anyway)
+            config.IsScripting = true;
+            config.IsStyling = true;
+
+            //This is our sample source, we will trigger the load event
+            var source = @"<!doctype html>
+<html>
+<head><title>Event sample</title></head>
+<body>
+<script>
+console.log('Before setting the handler!');
+
+document.addEventListener('load', function() {
+    console.log('Document loaded!');
+});
+
+document.addEventListener('hello', function() {
+    console.log('hello world from JavaScript!');
+});
+
+console.log('After setting the handler!');
+</script>
+</body>";
+            var document = DocumentBuilder.Html(source, config);
+
+            //HTML should be output in the end
+            Console.WriteLine(document.DocumentElement.OuterHtml);
+
+            //Register Hello event listener from C# (we also have one in JS)
+            document.AddEventListener("hello", (s, ev) =>
+            {
+                Console.WriteLine("hello world from C#!");
+            });
+
+            var e = document.CreateEvent("event");
+            e.Init("hello", false, false);
+            document.Dispatch(e);
         }
     }
 }
