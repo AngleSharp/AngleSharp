79634a1017050ee9eeb844d3854c8397c5c40d64
Florian Rappl <rappl.florian@gmail.com>
2014-08-15 13:57:06 +0200

    Changed namespace and added qualified name detection

--- AngleSharp/Foundation/Extensions/XmlExtensions.cs
+++ AngleSharp/Foundation/Extensions/XmlExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace AngleSharp.Xml
+﻿namespace AngleSharp
 {
     using System;
     using System.Diagnostics;
@@ -50,6 +50,64 @@
         {
             return c.IsXmlNameStart() || c.IsDigit() || c == Specification.Minus || c == Specification.Dot || c == 0xB7 ||
                    c.IsInRange(0x300, 0x36F) || c.IsInRange(0x203F, 0x2040);
+        }
+
+        /// <summary>
+        /// Determines if the given string is a valid (local or qualified) name.
+        /// </summary>
+        /// <param name="str">The string to examine.</param>
+        /// <returns>The result of the test.</returns>
+        public static Boolean IsXmlName(this String str)
+        {
+            if (str.Length > 0 && str[0].IsXmlNameStart())
+            {
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (!str[i].IsXmlName())
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if the given string is a valid qualified name.
+        /// </summary>
+        /// <param name="str">The string to examine.</param>
+        /// <returns>The result of the test.</returns>
+        public static Boolean IsQualifiedName(this String str)
+        {
+            var colon = str.IndexOf(Specification.Colon);
+
+            if (colon == -1)
+                return str.IsXmlName();
+
+            if (colon > 0 && str[0].IsXmlNameStart())
+            {
+                for (int i = 1; i < colon; i++)
+                {
+                    if (!str[i].IsXmlName())
+                        return false;
+                }
+
+                colon++;
+            }
+
+            if (str.Length > colon && str[colon++].IsXmlNameStart())
+            {
+                for (int i = colon; i < str.Length; i++)
+                {
+                    if (str[i] == Specification.Colon || !str[i].IsXmlName())
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
 
         /// <summary>
