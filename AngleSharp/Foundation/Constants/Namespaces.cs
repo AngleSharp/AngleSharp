cda5f5b2247ffd322d0b10c2f58e404c4cdf72b9
Florian Rappl <rappl.florian@gmail.com>
2014-08-15 20:09:33 +0200

    Renamed and extended namespace constants

--- AngleSharp/Foundation/Constants/Namespaces.cs
+++ AngleSharp/Foundation/Constants/Namespaces.cs
@@ -19,12 +19,12 @@
         static Namespaces()
         {
             _namespaces = new Dictionary<String, String>();
-            _namespaces.Add("html", Html);
-            _namespaces.Add("xlink", XLink);
-            _namespaces.Add("xml", Xml);
-            _namespaces.Add("xmlns", XmlNS);
-            _namespaces.Add("svg", Svg);
-            _namespaces.Add("mathml", MathML);
+            _namespaces.Add(HtmlPrefix, HtmlUri);
+            _namespaces.Add(XLinkPrefix, XLinkUri);
+            _namespaces.Add(XmlPrefix, XmlUri);
+            _namespaces.Add(XmlNsPrefix, XmlNsUri);
+            _namespaces.Add(SvgPrefix, SvgUri);
+            _namespaces.Add(MathMlPrefix, MathMlUri);
         }
 
         #endregion
@@ -32,39 +32,64 @@
         #region Properties
 
         /// <summary>
-        /// Gets the namespace declaration (xmlns).
-        /// </summary>
-        public static readonly String Declaration = "xmlns";
-
-        /// <summary>
         /// Gets the namespace for HTML elements.
         /// </summary>
-        public static readonly String Html = "http://www.w3.org/1999/xhtml";
+        public static readonly String HtmlUri = "http://www.w3.org/1999/xhtml";
 
         /// <summary>
         /// Gets the namespace for XMLNS elements.
         /// </summary>
-        public static readonly String XmlNS = "http://www.w3.org/2000/xmlns/";
+        public static readonly String XmlNsUri = "http://www.w3.org/2000/xmlns/";
 
         /// <summary>
         /// Gets the namespace for XMLNS elements.
         /// </summary>
-        public static readonly String XLink = "http://www.w3.org/1999/xlink";
+        public static readonly String XLinkUri = "http://www.w3.org/1999/xlink";
 
         /// <summary>
         /// Gets the namespace for XML elements.
         /// </summary>
-        public static readonly String Xml = "http://www.w3.org/XML/1998/namespace";
+        public static readonly String XmlUri = "http://www.w3.org/XML/1998/namespace";
 
         /// <summary>
         /// Gets the namespace for SVG elements.
         /// </summary>
-        public static readonly String Svg = "http://www.w3.org/2000/svg";
+        public static readonly String SvgUri = "http://www.w3.org/2000/svg";
 
         /// <summary>
         /// Gets the namespace for MathML elements.
         /// </summary>
-        public static readonly String MathML = "http://www.w3.org/1998/Math/MathML";
+        public static readonly String MathMlUri = "http://www.w3.org/1998/Math/MathML";
+
+        /// <summary>
+        /// Gets the prefix for HTML elements.
+        /// </summary>
+        public static readonly String HtmlPrefix = "html";
+
+        /// <summary>
+        /// Gets the prefix for XMLNS elements.
+        /// </summary>
+        public static readonly String XmlNsPrefix = "xmlns";
+
+        /// <summary>
+        /// Gets the prefix for XMLNS elements.
+        /// </summary>
+        public static readonly String XLinkPrefix = "xlink";
+
+        /// <summary>
+        /// Gets the prefix for XML elements.
+        /// </summary>
+        public static readonly String XmlPrefix = "xml";
+
+        /// <summary>
+        /// Gets the prefix for SVG elements.
+        /// </summary>
+        public static readonly String SvgPrefix = "svg";
+
+        /// <summary>
+        /// Gets the prefix for MathML elements.
+        /// </summary>
+        public static readonly String MathMlPrefix = "mathml";
 
         #endregion
 
@@ -78,9 +103,9 @@
         public static String DeclarationFor(String prefix)
         {
             if(String.IsNullOrEmpty(prefix))
-                return Declaration;
+                return XmlNsPrefix;
 
-            return Declaration + ":" + prefix;
+            return String.Concat(XmlNsPrefix, ":", prefix);
         }
 
         /// <summary>
@@ -88,7 +113,7 @@
         /// </summary>
         /// <param name="prefix">The prefix to look for.</param>
         /// <returns>The namespace URI if any, otherwise NULL.</returns>
-        public static String LookupNamespaceURI(String prefix)
+        public static String LookupNamespaceUri(String prefix)
         {
             if (_namespaces.ContainsKey(prefix))
                 return _namespaces[prefix];
