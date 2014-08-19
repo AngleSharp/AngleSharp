21783402d80fe11f697a46581f2b0d97a9c9cdc8
Florian Rappl <rappl.florian@gmail.com>
2014-08-19 09:54:39 +0200

    Added input event IDL

---
+++ AngleSharp/Interfaces/Events/IInputEvent.cs
@@ -0,0 +1,27 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents the event arguments for an input event.
+    /// </summary>
+    [DomName("InputEvent")]
+    public interface IInputEvent : IEvent
+    {
+        /// <summary>
+        /// Gets the data that has been entered.
+        /// </summary>
+        [DomName("data")]
+        String Data { get; }
+
+        /// <summary>
+        /// Initializes the input event.
+        /// </summary>
+        /// <param name="type">The type of event.</param>
+        /// <param name="bubbles">Determines if the event bubbles.</param>
+        /// <param name="cancelable">Determines if the event is cancelable.</param>
+        /// <param name="data">Sets the data for the input event.</param>
+        [DomName("initInputEvent")]
+        void Init(String type, Boolean bubbles, Boolean cancelable, String data);
+    }
+}
