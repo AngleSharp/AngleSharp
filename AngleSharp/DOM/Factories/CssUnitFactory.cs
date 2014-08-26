70e1d89c9d4e18e1bf297711a98f73a72d70d253
Florian Rappl <rappl.florian@gmail.com>
2014-08-26 12:23:01 +0200

    Extracted unit names

--- AngleSharp/DOM/Factories/CssUnitFactory.cs
+++ AngleSharp/DOM/Factories/CssUnitFactory.cs
@@ -9,31 +9,31 @@
 
         static CssUnitFactory()
         {
-            values.Add("em", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Em)));
-            values.Add("cm", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Cm)));
-            values.Add("ex", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Ex)));
-            values.Add("in", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.In)));
-            values.Add("mm", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Mm)));
-            values.Add("pc", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Pc)));
-            values.Add("pt", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Pt)));
-            values.Add("px", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Px)));
-            values.Add("rem", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Rem)));
-            values.Add("ch", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Ch)));
-            values.Add("vw", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vw)));
-            values.Add("vh", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vh)));
-            values.Add("vmin", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vmin)));
-            values.Add("vmax", data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vmax)));
-            values.Add("ms", data => new CSSPrimitiveValue<Time>(new Time(data, Time.Unit.Ms)));
-            values.Add("s", data => new CSSPrimitiveValue<Time>(new Time(data, Time.Unit.S)));
-            values.Add("dpi", data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dpi)));
-            values.Add("dpcm", data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dpcm)));
-            values.Add("dppx", data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dppx)));
-            values.Add("deg", data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Deg)));
-            values.Add("grad", data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Grad)));
-            values.Add("rad", data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Rad)));
-            values.Add("turn", data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Turn)));
-            values.Add("hz", data => new CSSPrimitiveValue<Frequency>(new Frequency(data, Frequency.Unit.Hz)));
-            values.Add("khz", data => new CSSPrimitiveValue<Frequency>(new Frequency(data, Frequency.Unit.Khz)));
+            values.Add(Units.Em, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Em)));
+            values.Add(Units.Cm, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Cm)));
+            values.Add(Units.Ex, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Ex)));
+            values.Add(Units.In, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.In)));
+            values.Add(Units.Mm, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Mm)));
+            values.Add(Units.Pc, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Pc)));
+            values.Add(Units.Pt, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Pt)));
+            values.Add(Units.Px, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Px)));
+            values.Add(Units.Rem, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Rem)));
+            values.Add(Units.Ch, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Ch)));
+            values.Add(Units.Vw, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vw)));
+            values.Add(Units.Vh, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vh)));
+            values.Add(Units.Vmin, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vmin)));
+            values.Add(Units.Vmax, data => new CSSPrimitiveValue<Length>(new Length(data, Length.Unit.Vmax)));
+            values.Add(Units.Ms, data => new CSSPrimitiveValue<Time>(new Time(data, Time.Unit.Ms)));
+            values.Add(Units.S, data => new CSSPrimitiveValue<Time>(new Time(data, Time.Unit.S)));
+            values.Add(Units.Dpi, data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dpi)));
+            values.Add(Units.Dpcm, data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dpcm)));
+            values.Add(Units.Dppx, data => new CSSPrimitiveValue<Resolution>(new Resolution(data, Resolution.Unit.Dppx)));
+            values.Add(Units.Deg, data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Deg)));
+            values.Add(Units.Grad, data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Grad)));
+            values.Add(Units.Rad, data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Rad)));
+            values.Add(Units.Turn, data => new CSSPrimitiveValue<Angle>(new Angle(data, Angle.Unit.Turn)));
+            values.Add(Units.Hz, data => new CSSPrimitiveValue<Frequency>(new Frequency(data, Frequency.Unit.Hz)));
+            values.Add(Units.Khz, data => new CSSPrimitiveValue<Frequency>(new Frequency(data, Frequency.Unit.Khz)));
         }
 
         /// <summary>
