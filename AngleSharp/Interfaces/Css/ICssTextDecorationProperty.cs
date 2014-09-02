5ceff5fe81b51c132b3269ad89de373ac2da2ea9
Florian Rappl <rappl.florian@gmail.com>
2014-09-02 11:22:42 +0200

    Merged text-decoration longhand properties into shorthand

--- AngleSharp/Interfaces/Css/ICssTextDecorationProperty.cs
+++ AngleSharp/Interfaces/Css/ICssTextDecorationProperty.cs
@@ -1,5 +1,41 @@
 ﻿namespace AngleSharp.DOM.Css
 {
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents the CSS text-decoration-line property.
+    /// </summary>
+    public interface ICssTextDecorationLineProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the enumeration over all selected styles
+        /// for text decoration lines.
+        /// </summary>
+        IEnumerable<TextDecorationLine> Line { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS text-decoration-color property.
+    /// </summary>
+    public interface ICssTextDecorationColorProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the selected text-decoration color.
+        /// </summary>
+        Color Color { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS text-decoration-style property.
+    /// </summary>
+    public interface ICssTextDecorationStyleProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the selected decoration style.
+        /// </summary>
+        TextDecorationStyle DecorationStyle { get; }
+    }
+
     /// <summary>
     /// Represents the CSS text-decoration shorthand property.
     /// </summary>
