473c9b82105459153db6c97dabb1d497ded178ce
Florian Rappl <rappl.florian@gmail.com>
2014-09-02 11:09:32 +0200

    Merged longhand to shorthand

--- AngleSharp/Interfaces/Css/ICssAnimationProperty.cs
+++ AngleSharp/Interfaces/Css/ICssAnimationProperty.cs
@@ -1,5 +1,81 @@
 ﻿namespace AngleSharp.DOM.Css
 {
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents the CSS animation-name property.
+    /// </summary>
+    public interface ICssAnimationNameProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the names of the animations to trigger.
+        /// </summary>
+        IEnumerable<String> Names { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS animation-delay property.
+    /// </summary>
+    public interface ICssAnimationDelayProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the delays for the animations.
+        /// </summary>
+        IEnumerable<Time> Delays { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS animation-duration property.
+    /// </summary>
+    public interface ICssAnimationDurationProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the durations for the animations.
+        /// </summary>
+        IEnumerable<Time> Durations { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS animation-direction property.
+    /// </summary>
+    public interface ICssAnimationDirectionProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets an iteration over all defined directions.
+        /// </summary>
+        IEnumerable<AnimationDirection> Directions { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS animation-fill-mode property.
+    /// </summary>
+    public interface ICssAnimationFillModeProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets an iteration over all defined fill modes.
+        /// </summary>
+        IEnumerable<AnimationFillStyle> FillModes { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS animation-iteration-count property.
+    /// </summary>
+    public interface ICssAnimationIterationCountProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the iteration count of the covered animations.
+        /// </summary>
+        IEnumerable<Single> Iterations { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS anmation-timing-function property.
+    /// </summary>
+    public interface ICssAnimationTimingFunctionProperty : ICssProperty
+    {
+    }
+
     /// <summary>
     /// Represents the CSS animation shorthand property.
     /// </summary>
--- AngleSharp/Interfaces/Css/ICssAnimationTimingFunctionProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AngleSharp.DOM.Css
-{
-    /// <summary>
-    /// Represents the CSS anmation-timing-function property.
-    /// </summary>
-    public interface ICssAnimationTimingFunctionProperty : ICssProperty
-    {
-    }
-}
