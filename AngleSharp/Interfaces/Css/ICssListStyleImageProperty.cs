beaab5f9c4954c92830cf36d082a40ade8a0ca5a
Florian Rappl <rappl.florian@gmail.com>
2014-09-02 11:24:30 +0200

    Merged list-style longhand properties into shorthand

--- AngleSharp/Interfaces/Css/ICssListStyleImageProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AngleSharp.DOM.Css
-{
-    /// <summary>
-    /// Represents the CSS list-style-image property.
-    /// </summary>
-    public interface ICssListStyleImageProperty : ICssProperty
-    {
-    }
-}
--- AngleSharp/Interfaces/Css/ICssListStyleProperty.cs
+++ AngleSharp/Interfaces/Css/ICssListStyleProperty.cs
@@ -1,6 +1,35 @@
 ﻿namespace AngleSharp.DOM.Css
 {
     /// <summary>
+    /// Represents the CSS list-style-type property.
+    /// </summary>
+    public interface ICssListStyleTypeProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the selected style for the list.
+        /// </summary>
+        ListStyle Style { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS list-style-position property.
+    /// </summary>
+    public interface ICssListStylePositionProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the selected position.
+        /// </summary>
+        ListPosition Position { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS list-style-image property.
+    /// </summary>
+    public interface ICssListStyleImageProperty : ICssProperty
+    {
+    }
+
+    /// <summary>
     /// Represents the CSS list-style shorthand property.
     /// </summary>
     public interface ICssListStyleProperty : ICssProperty, ICssListStyleImageProperty, ICssListStylePositionProperty, ICssListStyleTypeProperty
