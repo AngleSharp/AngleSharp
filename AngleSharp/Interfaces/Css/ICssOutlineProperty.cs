16fd491f8aa89ef450449f0d5049944235bc2edd
Florian Rappl <rappl.florian@gmail.com>
2014-09-02 11:20:13 +0200

    Merged outline longhand properties into shorthand

--- AngleSharp/Interfaces/Css/ICssOutlineProperty.cs
+++ AngleSharp/Interfaces/Css/ICssOutlineProperty.cs
@@ -1,6 +1,39 @@
 ﻿namespace AngleSharp.DOM.Css
 {
     /// <summary>
+    /// Represents the CSS outline-width property.
+    /// </summary>
+    public interface ICssOutlineWidthProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the width of the outline of an element.
+        /// </summary>
+        Length Width { get; }
+    }
+
+    /// <summary>
+    /// Repesents the CSS outline-style property.
+    /// </summary>
+    public interface ICssOutlineStyleProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the selected outline style.
+        /// </summary>
+        LineStyle Style { get; }
+    }
+
+    /// <summary>
+    /// Represents the CSS outline-color property.
+    /// </summary>
+    public interface ICssOutlineColorProperty : ICssProperty
+    {
+        /// <summary>
+        /// Gets the color of the outline.
+        /// </summary>
+        Color Color { get; }
+    }
+
+    /// <summary>
     /// Represents the CSS outline shorthand property.
     /// </summary>
     public interface ICssOutlineProperty : ICssProperty, ICssOutlineColorProperty, ICssOutlineStyleProperty, ICssOutlineWidthProperty
