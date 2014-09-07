d9a013cf477bfee4f23b674f474633b3bd20b4d3
Florian Rappl <rappl.florian@gmail.com>
2014-09-07 16:40:14 +0200

    Added the hash changed event

---
+++ AngleSharp/DOM/Events/HashChangedEvent.cs
@@ -0,0 +1,37 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents a custom event that provides an additional details property.
+    /// </summary>
+    class HashChangedEvent : Event, IHashChangeEvent
+    {
+        #region Properties
+
+        public String PreviousUrl
+        {
+            get;
+            private set;
+        }
+
+        public String CurrentUrl
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Init(String type, Boolean bubbles, Boolean cancelable, String previousUrl, String currentUrl)
+        {
+            Init(type, bubbles, cancelable);
+            PreviousUrl = previousUrl;
+            CurrentUrl = currentUrl;
+        }
+
+        #endregion
+    }
+}
---
+++ AngleSharp/Interfaces/Events/IHashChangeEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents the event arguments for a hash changed event.
+    /// </summary>
+    [DomName("HashChangeEvent")]
+    public interface IHashChangeEvent : IEvent
+    {
+        /// <summary>
+        /// Gets the URL before the hash changed.
+        /// </summary>
+        [DomName("oldURL")]
+        String PreviousUrl { get; }
+
+        /// <summary>
+        /// Gets the URL after the hash changed.
+        /// </summary>
+        [DomName("newURL")]
+        String CurrentUrl { get; }
+    }
+}
