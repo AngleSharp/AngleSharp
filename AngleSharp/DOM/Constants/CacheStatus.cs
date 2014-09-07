f765ec19a453e8ff9b4d150971bb05509ec5583f
Florian Rappl <rappl.florian@gmail.com>
2014-09-07 17:47:53 +0200

    Added the cache status enumeration

---
+++ AngleSharp/DOM/Constants/CacheStatus.cs
@@ -0,0 +1,39 @@
+﻿namespace AngleSharp.DOM
+{
+    /// <summary>
+    /// An enumeration over all possible cache states.
+    /// </summary>
+    public enum CacheStatus : short
+    {
+        /// <summary>
+        /// The resource is uncached.
+        /// </summary>
+        [DomName("UNCACHED")]
+        Uncached = 0,
+        /// <summary>
+        /// The cache status checker is idle.
+        /// </summary>
+        [DomName("IDLE")]
+        Idle = 1,
+        /// <summary>
+        /// The cache status is being checked.
+        /// </summary>
+        [DomName("CHECKING")]
+        Checking = 2,
+        /// <summary>
+        /// The resource is being downloaded.
+        /// </summary>
+        [DomName("DOWNLOADING")]
+        Downloading = 3,
+        /// <summary>
+        /// An update for the resource is available.
+        /// </summary>
+        [DomName("UPDATEREADY")]
+        UpdateReady = 4,
+        /// <summary>
+        /// The resource is practically obsolete.
+        /// </summary>
+        [DomName("OBSOLETE")]
+        Obsolete = 5
+    }
+}
