7b5021d6d7ad81927d858d4951cbeb9f421868b0
Florian Rappl <rappl.florian@gmail.com>
2014-09-08 21:26:11 +0200

    Added TouchList

---
+++ AngleSharp/Interfaces/Events/ITouchList.cs
@@ -0,0 +1,26 @@
+﻿namespace AngleSharp.DOM.Events
+{
+    using System;
+
+    /// <summary>
+    /// Represents a list with touch points.
+    /// </summary>
+    [DomName("TouchList")]
+    public interface ITouchList
+    {
+        /// <summary>
+        /// Gets the number of contained touch points.
+        /// </summary>
+        [DomName("length")]
+        Int32 Length { get; }
+
+        /// <summary>
+        /// Gets the data of the touch point at the given index.
+        /// </summary>
+        /// <param name="index">The index of the touch point.</param>
+        /// <returns>The touch point at the index.</returns>
+        [DomAccessor(Accessors.Getter)]
+        [DomName("item")]
+        ITouch this[Int32 index] { get; }
+    }
+}
