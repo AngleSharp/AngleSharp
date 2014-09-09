c7b274064c251444f3556548a833da227dd8a19e
Florian Rappl <rappl.florian@gmail.com>
2014-09-09 23:18:19 +0200

    Added the element map

---
+++ AngleSharp/DOM/Collections/ElementMap.cs
@@ -0,0 +1,37 @@
+﻿namespace AngleSharp.DOM.Collections
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    sealed class ElementMap : IElementMap
+    {
+        readonly Dictionary<String, IElement> _elements;
+
+        public ElementMap()
+        {
+            _elements = new Dictionary<String, IElement>();
+        }
+
+        public IElement this[String name]
+        {
+            get { return _elements[name]; }
+            set { _elements[name] = value; }
+        }
+
+        public void Remove(String name)
+        {
+            _elements.Remove(name);
+        }
+
+        public IEnumerator<KeyValuePair<String, IElement>> GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
