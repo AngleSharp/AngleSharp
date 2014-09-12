1831c12dc64ed159985e1f1437cde3d0a8f3535a
Florian Rappl <rappl.florian@gmail.com>
2014-09-12 23:43:04 +0200

    Defined the cookie service

--- AngleSharp/Infrastructure/ICookieService.cs
+++ AngleSharp/Infrastructure/ICookieService.cs
@@ -1,9 +1,25 @@
 ﻿namespace AngleSharp.Infrastructure
 {
+    using AngleSharp.Network;
+    using System;
+
     /// <summary>
     /// Defines methods to evaluate and store cookies.
     /// </summary>
     public interface ICookieService : IService
     {
+        /// <summary>
+        /// Gets the cookie from the given response.
+        /// </summary>
+        /// <param name="response">The response to investigate.</param>
+        /// <returns>The string representing the content of the cookie.</returns>
+        String GetCookie(IResponse response);
+
+        /// <summary>
+        /// Sets the cookie in the given request.
+        /// </summary>
+        /// <param name="request">The request to modify.</param>
+        /// <param name="value">The cookie to set.</param>
+        void SetCookie(IRequest request, String value);
     }
 }
