2f8db19959aa7e776df03941467e0972ef0485af
Florian Rappl <rappl.florian@gmail.com>
2014-09-16 18:02:05 +0200

    Added extension service

---
+++ AngleSharp/Infrastructure/IExtensionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace AngleSharp.Infrastructure
+{
+    /// <summary>
+    /// Represents an extension offering a special kind of (DOM) interface.
+    /// </summary>
+    /// <typeparam name="TInterface">The interface to offer.</typeparam>
+    public interface IExtensionService<TInterface> : IService
+    {
+        /// <summary>
+        /// Gets the interface specialized for the given address.
+        /// </summary>
+        /// <param name="origin">The origin of the requesting page.</param>
+        /// <returns>The instance.</returns>
+        TInterface this[String origin] { get; }
+    }
+}
