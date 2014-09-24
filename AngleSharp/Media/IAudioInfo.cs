51be25e671a3da3e1d75f83a396806308cf1a5ef
Florian Rappl <rappl.florian@gmail.com>
2014-09-25 00:28:07 +0200

    Started defining the media information

--- AngleSharp/Media/IAudioInfo.cs
+++ AngleSharp/Media/IAudioInfo.cs
@@ -1,9 +1,15 @@
 ﻿namespace AngleSharp.Media
 {
+    using AngleSharp.DOM.Media;
+
     /// <summary>
     /// Contains information about a sound file.
     /// </summary>
     public interface IAudioInfo
     {
+        /// <summary>
+        /// Gets the controller responsible for the media.
+        /// </summary>
+        IMediaController Controller { get; }
     }
 }
--- AngleSharp/Media/IVideoInfo.cs
+++ AngleSharp/Media/IVideoInfo.cs
@@ -1,9 +1,26 @@
 ﻿namespace AngleSharp.Media
 {
+    using AngleSharp.DOM.Media;
+    using System;
+
     /// <summary>
     /// Contains information about a video file.
     /// </summary>
     public interface IVideoInfo
     {
+        /// <summary>
+        /// Gets the controller responsible for the media.
+        /// </summary>
+        IMediaController Controller { get; }
+
+        /// <summary>
+        /// Gets the width of the video.
+        /// </summary>
+        Int32 Width { get; }
+
+        /// <summary>
+        /// Gets the height of the video.
+        /// </summary>
+        Int32 Height { get; }
     }
 }
