7695cf5496667993bd755a9b3e9efbb90c47dbae
Florian Rappl <rappl.florian@gmail.com>
2014-09-25 19:19:50 +0200

    Improved tests for async parsing and added DelayStream mock

--- UnitTests/Library/AsyncParsing.cs
+++ UnitTests/Library/AsyncParsing.cs
@@ -4,6 +4,7 @@ using AngleSharp.Parser.Html;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Text;
+using UnitTests.Mocks;
 
 namespace UnitTests
 {
@@ -28,15 +29,16 @@ namespace UnitTests
         [TestMethod]
         public void TestAsyncHtmlParsingFromStream()
         {
-            var source = "<html><head><title>My test</title></head><body><p>Some text</p></body></html>";
-            var content = Encoding.UTF8.GetBytes(source);
-            var parser = new HtmlParser(new MemoryStream(content), Configuration.Default);
+            var text = "<html><head><title>My test</title></head><body><p>Some text</p></body></html>";
+            var source = new DelayedStream(Encoding.UTF8.GetBytes(text));
+            var parser = new HtmlParser(source, Configuration.Default);
             var task = parser.ParseAsync();
             Assert.IsFalse(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
             task.Wait();
             Assert.IsTrue(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
+
             Assert.AreEqual("My test", parser.Result.Title);
             Assert.AreEqual(1, parser.Result.Body.ChildElementCount);
             Assert.AreEqual("Some text", parser.Result.Body.Children[0].TextContent);
@@ -53,6 +55,7 @@ namespace UnitTests
             task.Wait();
             Assert.IsTrue(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
+
             Assert.AreEqual(4, parser.Result.Rules.Length);
         }
 
@@ -62,11 +65,9 @@ namespace UnitTests
             var source = "<html><head><title>My test</title></head><body><p>Some text</p></body></html>";
             var parser = new HtmlParser(source, Configuration.Default);
             var task = parser.ParseAsync();
-            Assert.IsFalse(task.IsCompleted);
-            Assert.IsNotNull(parser.Result);
-            task.Wait();
             Assert.IsTrue(task.IsCompleted);
             Assert.IsNotNull(parser.Result);
+
             Assert.AreEqual("My test", parser.Result.Title);
             Assert.AreEqual(1, parser.Result.Body.ChildElementCount);
             Assert.AreEqual("Some text", parser.Result.Body.Children[0].TextContent);
---
+++ UnitTests/Mocks/DelayedStream.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTests.Mocks
+{
+    sealed class DelayedStream : Stream
+    {
+        Stream _stream;
+        const Int32 delay = 10;
+
+        public DelayedStream(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public DelayedStream(Byte[] content)
+            : this(new MemoryStream(content))
+        {
+        }
+
+        public override bool CanRead
+        {
+            get { return _stream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return _stream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return _stream.CanWrite; }
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override long Length
+        {
+            get { return _stream.Length; }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _stream.Position;
+            }
+            set
+            {
+                _stream.Position = value;
+            }
+        }
+
+        public override async Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            await Task.Delay(delay).ConfigureAwait(false);
+            await _stream.CopyToAsync(destination, bufferSize, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await Task.Delay(delay).ConfigureAwait(false);
+            return await _stream.ReadAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await Task.Delay(delay).ConfigureAwait(false);
+            await _stream.WriteAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _stream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer, offset, count);
+        }
+    }
+}
