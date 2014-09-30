dc97e7df2967fc0d3a10ab00f71053975e925d0b
Florian Rappl <rappl.florian@gmail.com>
2014-09-30 12:45:15 +0200

    Included parent and creator

--- AngleSharp/Interfaces/IBrowsingContext.cs
+++ AngleSharp/Interfaces/IBrowsingContext.cs
@@ -19,5 +19,19 @@
         /// Gets the configuration for the browsing context.
         /// </summary>
         IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Gets the parent of the current context, if any.
+        /// If a parent is available, then the current context
+        /// contains only embedded documents.
+        /// </summary>
+        IBrowsingContext Parent { get; }
+
+        /// <summary>
+        /// Gets the document that created the current context, if any.
+        /// The creator is the active document of the parent at the time
+        /// of creation.
+        /// </summary>
+        IDocument Creator { get; }
     }
 }
