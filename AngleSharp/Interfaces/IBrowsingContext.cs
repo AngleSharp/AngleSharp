f0560032196e7bfc9a2c1fc5eeec7db1fe561657
Florian Rappl <rappl.florian@gmail.com>
2014-09-30 09:46:15 +0200

    Specified the browsing context.

--- AngleSharp/Interfaces/IBrowsingContext.cs
+++ AngleSharp/Interfaces/IBrowsingContext.cs
@@ -5,5 +5,19 @@
     /// </summary>
     public interface IBrowsingContext
     {
+        /// <summary>
+        /// Gets the current window proxy.
+        /// </summary>
+        IWindowProxy Current { get; }
+
+        /// <summary>
+        /// Gets the session history of the given browsing context.
+        /// </summary>
+        IHistory SessionHistory { get; }
+
+        /// <summary>
+        /// Gets the configuration for the browsing context.
+        /// </summary>
+        IConfiguration Configuration { get; }
     }
 }
