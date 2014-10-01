0cefea18fc556e58d3f408713f96253e50c60109
Florian Rappl <rappl.florian@gmail.com>
2014-10-01 16:55:09 +0200

    Removed fancy features that are not used by the core

--- AngleSharp/Infrastructure/IEventService.cs
+++ AngleSharp/Infrastructure/IEventService.cs
@@ -12,20 +12,5 @@
         /// </summary>
         /// <param name="action">The action to enqueue.</param>
         void Enqueue(Action action);
-
-        /// <summary>
-        /// Gets the running time of the currently running action.
-        /// </summary>
-        TimeSpan ActionTime { get; }
-
-        /// <summary>
-        /// Gets if there are no actions running.
-        /// </summary>
-        Boolean IsIdle { get; }
-
-        /// <summary>
-        /// Cancels the currently running action.
-        /// </summary>
-        void Cancel();
     }
 }
