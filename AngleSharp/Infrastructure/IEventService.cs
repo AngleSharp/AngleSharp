49f5d9e180337d82d7ea8d2bed299e58459a15d8
Florian Rappl <rappl.florian@gmail.com>
2014-10-01 15:48:39 +0200

    Defined the event service

--- AngleSharp/Infrastructure/IEventService.cs
+++ AngleSharp/Infrastructure/IEventService.cs
@@ -1,9 +1,31 @@
 ﻿namespace AngleSharp.Infrastructure
 {
+    using System;
+
     /// <summary>
     /// Represents an event loop.
     /// </summary>
     public interface IEventService : IService
     {
+        /// <summary>
+        /// Enqueues a given action to the event loop.
+        /// </summary>
+        /// <param name="action">The action to enqueue.</param>
+        void Enqueue(Action action);
+
+        /// <summary>
+        /// Gets the running time of the currently running action.
+        /// </summary>
+        TimeSpan ActionTime { get; }
+
+        /// <summary>
+        /// Gets if there are no actions running.
+        /// </summary>
+        Boolean IsIdle { get; }
+
+        /// <summary>
+        /// Cancels the currently running action.
+        /// </summary>
+        void Cancel();
     }
 }
