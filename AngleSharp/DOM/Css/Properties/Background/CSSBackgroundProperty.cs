b448ce635737e104e9b1845ca7668b6d4b9946d4
Florian Rappl <rappl.florian@gmail.com>
2014-10-25 17:43:08 +0200

    Reimplemented validation

--- AngleSharp/DOM/Css/Properties/Background/CSSBackgroundProperty.cs
+++ AngleSharp/DOM/Css/Properties/Background/CSSBackgroundProperty.cs
@@ -116,133 +116,110 @@
         /// <returns>True if the state is valid, otherwise false.</returns>
         protected override Boolean IsValid(CSSValue value)
         {
-            var values = value as CSSValueList ?? new CSSValueList(value);
-            var image = new CSSValueList();
-            var position = new CSSValueList();
-            var size = new CSSValueList();
-            var repeat = new CSSValueList();
-            var attachment = new CSSValueList();
-            var origin = new CSSValueList();
-            var clip = new CSSValueList();
+            var items = (value as CSSValueList ?? new CSSValueList(value)).ToList();
+            var images = new CSSValueList();
+            var positions = new CSSValueList();
+            var sizes = new CSSValueList();
+            var repeats = new CSSValueList();
+            var attachments = new CSSValueList();
+            var origins = new CSSValueList();
+            var clips = new CSSValueList();
             CSSValue color = null;
-            var list = values.ToList();
 
-            for (int i = 0; i < list.Count; i++)
+            foreach (var list in items)
             {
-                var entry = list[i];
-                var hasImage = false;
-                var hasPosition = false;
-                var hasRepeat = false;
-                var hasAttachment = false;
-                var hasBox = false;
-                var hasColor = i + 1 != list.Count;
+                CSSValue image = null, position = null, size = null, repeat = null,
+                         attachment = null, origin = null, clip = null;
 
-                for (int j = 0; j < entry.Length; j++)
+                if (color != null)
+                    return false;
+
+                if (images.Length > 0)
                 {
-                    if (!hasPosition && (entry[j].IsOneOf(Keywords.Top, Keywords.Left, Keywords.Center, Keywords.Bottom, Keywords.Right) || entry[j].ToDistance() != null))
+                    images.Add(CSSValue.Separator);
+                    positions.Add(CSSValue.Separator);
+                    sizes.Add(CSSValue.Separator);
+                    repeats.Add(CSSValue.Separator);
+                    attachments.Add(CSSValue.Separator);
+                    origins.Add(CSSValue.Separator);
+                    clips.Add(CSSValue.Separator);
+                }
+
+                for (int j = 0; j < list.Length; j++)
+                {
+                    var item = list[j];
+
+                    if (_position.CanStore(item, ref position))
                     {
-                        hasPosition = true;
-                        position.Add(entry[j]);
+                        if (j + 1 == list.Length)
+                            continue;
 
-                        while (j + 1 < entry.Length && (entry[j + 1].IsOneOf(Keywords.Top, Keywords.Left, Keywords.Center, Keywords.Bottom, Keywords.Right) || entry[j + 1].ToDistance() != null))
-                            position.Add(entry[++j]);
+                        var pack = new CSSValueList();
+                        pack.Add(position);
+                        pack.Add(list[j + 1]);
 
-                        if (j + 1 < entry.Length && entry[j + 1] == CSSValue.Delimiter)
+                        if (_position.CanTake(pack))
+                        {
+                            positions.Add(position);
+                            position = list[++j];
+                        }
+
+                        if (j + 1 < list.Length && list[j + 1] == CSSValue.Delimiter)
                         {
                             j += 2;
 
-                            if (j < entry.Length && (entry[j].IsOneOf(Keywords.Auto, Keywords.Contain, Keywords.Cover) || entry[j].ToDistance() != null))
+                            if (j < list.Length && _size.CanStore(list[j], ref size))
                             {
-                                size.Add(entry[j]);
+                                if (j + 1 == list.Length)
+                                    continue;
 
-                                if (j + 1 < entry.Length && (entry[j + 1].Is(Keywords.Auto) || entry[j + 1].ToDistance() != null))
-                                    size.Add(entry[++j]);
+                                pack = new CSSValueList();
+                                pack.Add(size);
+                                pack.Add(list[j + 1]);
+
+                                if (_size.CanTake(pack))
+                                {
+                                    sizes.Add(size);
+                                    size = list[++j];
+                                }
                             }
                             else
                                 return false;
                         }
-                        else
-                            size.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.Auto)));
-
-                        continue;
                     }
-
-                    if (!hasImage && entry[j].ToImage() != null)
+                    else if (_repeat.CanStore(item, ref repeat))
                     {
-                        hasImage = true;
-                        image.Add(entry[j]);
-                    }
-                    else if (!hasRepeat && entry[j].IsOneOf(Keywords.RepeatX, Keywords.RepeatY, Keywords.Repeat, Keywords.Space, Keywords.Round, Keywords.NoRepeat))
-                    {
-                        hasRepeat = true;
-                        repeat.Add(entry[j]);
+                        if (j + 1 == list.Length)
+                            continue;
 
-                        if (j + 1 < entry.Length && entry[j + 1].IsOneOf(Keywords.Repeat, Keywords.Space, Keywords.Round, Keywords.NoRepeat))
-                            repeat.Add(entry[++j]);
-                    }
-                    else if (!hasAttachment && entry[j].IsOneOf(Keywords.Local, Keywords.Fixed, Keywords.Scroll))
-                    {
-                        hasAttachment = true;
-                        attachment.Add(entry[j]);
-                    }
-                    else if (!hasBox && entry[j].ToBoxModel().HasValue)
-                    {
-                        hasBox = true;
-                        origin.Add(entry[j]);
+                        var pack = new CSSValueList();
+                        pack.Add(repeat);
+                        pack.Add(list[j + 1]);
 
-                        if (j + 1 < entry.Length && entry[j + 1].ToBoxModel().HasValue)
-                            clip.Add(entry[++j]);
-                        else
-                            clip.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.BorderBox)));
+                        if (_repeat.CanTake(pack))
+                        {
+                            repeats.Add(repeat);
+                            repeat = list[++j];
+                        }
                     }
-                    else if (!hasColor && _color.CanTake(entry[j]))
-                        color = entry[j];
-                    else
+                    else if (!_image.CanStore(item, ref image) && !_attachment.CanStore(item, ref attachment) && 
+                             !_origin.CanStore(item, ref origin) && !_clip.CanStore(item, ref clip) && !_color.CanStore(item, ref color))
                         return false;
                 }
 
-                if (!hasImage)
-                    image.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.None)));
-
-                if (!hasPosition)
-                {
-                    position.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.Center)));
-                    size.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.Auto)));
-                }
-
-                if (!hasRepeat)
-                    repeat.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.Repeat)));
-
-                if (!hasAttachment)
-                    attachment.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.Scroll)));
-
-                if (!hasBox)
-                {
-                    origin.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.BorderBox)));
-                    clip.Add(new CSSPrimitiveValue(new CssIdentifier(Keywords.BorderBox)));
-                }
-
-                if (i + 1 < list.Count)
-                {
-                    image.Add(CSSValue.Separator);
-                    position.Add(CSSValue.Separator);
-                    size.Add(CSSValue.Separator);
-                    repeat.Add(CSSValue.Separator);
-                    attachment.Add(CSSValue.Separator);
-                    origin.Add(CSSValue.Separator);
-                    clip.Add(CSSValue.Separator);
-                }
+                images.Add(image ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.None)));
+                positions.Add(position ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.Center)));
+                sizes.Add(size ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.Auto)));
+                repeats.Add(repeat ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.Repeat)));
+                attachments.Add(attachment ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.Scroll)));
+                origins.Add(origin ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.BorderBox)));
+                clips.Add(clip ?? new CSSPrimitiveValue(new CssIdentifier(Keywords.BorderBox)));
             }
 
-            _image.TrySetValue(image);
-            _position.TrySetValue(position);
-            _repeat.TrySetValue(repeat);
-            _attachment.TrySetValue(attachment);
-            _origin.TrySetValue(origin);
-            _size.TrySetValue(size);
-            _clip.TrySetValue(clip);
-            _color.TrySetValue(color);
-            return true;
+            return _image.TrySetValue(images.Reduce()) && _position.TrySetValue(positions.Reduce()) &&
+                   _repeat.TrySetValue(repeats.Reduce()) && _attachment.TrySetValue(attachments.Reduce()) &&
+                   _origin.TrySetValue(origins.Reduce()) && _size.TrySetValue(sizes.Reduce()) &&
+                   _clip.TrySetValue(clips) && _color.TrySetValue(color);
         }
 
         //internal static String Stringify(CSSStyleDeclaration style)
