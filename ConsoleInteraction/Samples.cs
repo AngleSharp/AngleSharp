a12021c3d379c369720efd8506a4f88197e13cb5
Florian Rappl <rappl.florian@gmail.com>
2014-11-03 00:58:02 +0100

    Added sample to illustrate error handling

--- ConsoleInteraction/Samples.cs
+++ ConsoleInteraction/Samples.cs
@@ -10,6 +10,7 @@
     {
         public static void RunAll()
         {
+            ErrorHandling();
             FirstExample();
             UsingLinq();
             SingleElements();
@@ -18,6 +19,30 @@
             ExtendedScriptingSample();
             CustomEventScriptingExample();
             LegacyEventScriptingExample();
+        }
+
+        static void ErrorHandling()
+        {
+            //The original source, see
+            //http://www.google.com/error
+            var source = @"
+<!DOCTYPE html>
+<html lang=en>
+  <meta charset=utf-8>
+  <meta name=viewport content=""initial-scale=1, minimum-scale=1, width=device-width"">
+  <title>Error 404 (Not Found)!!1</title>
+  <style>
+    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/errors/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/errors/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}
+  </style>
+  <a href=//www.google.com/><span id=logo aria-label=Google></span></a>
+  <p><b>404.</b> <ins>That’s an error.</ins>
+  <p>The requested URL <code>/error</code> was not found on this server.  <ins>That’s all we know.</ins>";
+
+            //Just get the DOM representation
+            var doc = DocumentBuilder.Html(source);
+
+            //Serialize it back to the console
+            Console.WriteLine(doc.DocumentElement.OuterHtml);
         }
 
         static void FirstExample() 
@@ -239,7 +264,7 @@ console.log('After setting the handler!');
 
             var e = document.CreateEvent("event");
             e.Init("hello", false, false);
-            document.Dispatch(e);
+            //document.Dispatch(e);
         }
 
         static void Html5Test()
