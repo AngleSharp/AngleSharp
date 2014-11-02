068c5760526403b14a1cdeea1fec63d92f4e18ba
Florian Rappl <rappl.florian@gmail.com>
2014-11-02 19:36:54 +0100

    Testing the @font-face rule

---
+++ UnitTests/Css/FontFace.cs
@@ -0,0 +1,34 @@
+﻿using AngleSharp.DOM.Css;
+using AngleSharp.Parser.Css;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Css
+{
+    [TestClass]
+    public class FontFaceTests
+    {
+        [TestMethod]
+        public void FontFaceOpenSansWithSource()
+        {
+            var src = "@font-face{font-family:'Open Sans';src:url(fonts/OpenSans-Light.eot);src:local('Open Sans Light'),local('OpenSans-Light'),url(fonts/OpenSans-Light.ttf) format('truetype'),url(fonts/OpenSans-Light.woff) format('woff');font-style:normal}";
+            var css = CssParser.ParseStyleSheet(src);
+            Assert.IsNotNull(css);
+            Assert.AreEqual(1, css.Rules.Length);
+            Assert.IsInstanceOfType(css.Rules[0], typeof(CSSFontFaceRule));
+            var fontface = (CSSFontFaceRule)css.Rules[0];
+            Assert.AreEqual(3, fontface.Style.Length);
+        }
+
+        [TestMethod]
+        public void FontFaceOpenSansNoSource()
+        {
+            var src = "@font-face{font-family:'Open Sans';font-style:normal}";
+            var css = CssParser.ParseStyleSheet(src);
+            Assert.IsNotNull(css);
+            Assert.AreEqual(1, css.Rules.Length);
+            Assert.IsInstanceOfType(css.Rules[0], typeof(CSSFontFaceRule));
+            var fontface = (CSSFontFaceRule)css.Rules[0];
+            Assert.AreEqual(2, fontface.Style.Length);
+        }
+    }
+}
