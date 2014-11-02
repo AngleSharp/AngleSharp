a889f3ce5ed8bcc2d060bf5ff1879875af9a57c9
Florian Rappl <rappl.florian@gmail.com>
2014-11-02 23:21:00 +0100

    Included extension for helping to integrate a JS engine

---
+++ AngleSharp.Scripting/ConfigurationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace AngleSharp.Scripting
+{
+    using System.Linq;
+
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Sets scripting to true, registers the JavaScript engine and returns the same instance.
+        /// </summary>
+        /// <typeparam name="TConfiguration">Instance of Configuration.</typeparam>
+        /// <param name="configuration">The configuration to modify.</param>
+        /// <returns>The same object, for chaining.</returns>
+        public static TConfiguration WithJavaScript<TConfiguration>(this TConfiguration configuration)
+            where TConfiguration : Configuration
+        {
+            configuration.IsScripting = true;
+
+            if (!configuration.ScriptEngines.OfType<JavaScriptEngine>().Any())
+                configuration.Register(new JavaScriptEngine());
+
+            return configuration;
+        }
+    }
+}
