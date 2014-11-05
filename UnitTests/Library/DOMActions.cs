b75b6041428c9dc533ce12ccb8fd848e58dbd36d
Florian Rappl <rappl.florian@gmail.com>
2014-11-05 19:22:25 +0100

    Testing text nodes

--- UnitTests/Library/DOMActions.cs
+++ UnitTests/Library/DOMActions.cs
@@ -150,5 +150,52 @@ namespace UnitTests.Library
             Assert.IsTrue(parent.Contains(child));
             Assert.IsTrue(parent.Contains(grandchild));
         }
+
+        [TestMethod]
+        public void ReturnTextFromBody()
+        {
+            var test = "Some text";
+            var html = string.Format(@"
+<html>
+<body>{0}</body></html>", test);
+            var doc = DocumentBuilder.Html(html);
+            Assert.AreEqual(test, doc.Body.TextContent);
+            Assert.AreEqual(test, doc.Body.Text());
+            Assert.AreEqual(test, doc.Body.ChildNodes[0].TextContent);
+
+            var text = doc.Body.ChildNodes[0] as TextNode;
+            Assert.IsNotNull(text);
+            Assert.AreEqual(test, text.Data);
+            Assert.AreEqual(test, text.Text);
+        }
+
+        [TestMethod]
+        public void ReturnConcatTextFromBody()
+        {
+            var test1 = "Some text";
+            var test2 = "Other text";
+            var test3 = "Another test";
+            var test = string.Concat(test1, test2, test3);
+            var html = @"
+<html>
+<body></body></html>";
+            var doc = DocumentBuilder.Html(html);
+            var text1 = doc.CreateTextNode(test1);
+            var text2 = doc.CreateTextNode(test2);
+            var text3 = doc.CreateTextNode(test3);
+            doc.Body.Append(text1);
+            doc.Body.Append(text2);
+            doc.Body.Append(text3);
+            Assert.AreEqual(test, doc.Body.TextContent);
+            Assert.AreEqual(test, doc.Body.Text());
+            Assert.AreEqual(test1, doc.Body.ChildNodes[0].TextContent);
+
+            Assert.AreEqual(test1, text1.Data);
+            Assert.AreEqual(test, text1.Text);
+            Assert.AreEqual(test2, text2.Data);
+            Assert.AreEqual(test, text2.Text);
+            Assert.AreEqual(test3, text3.Data);
+            Assert.AreEqual(test, text3.Text);
+        }
     }
 }
