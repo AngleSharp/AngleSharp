149706732c3be68a97c43ac5d31d7604ee269412
Florian Rappl <rappl.florian@gmail.com>
2014-11-09 13:46:42 +0100

    Fallback for no durations

--- Performance/TestResult.cs
+++ Performance/TestResult.cs
@@ -21,23 +21,26 @@
 
         public TimeSpan Shortest
         {
-            get { return Durations.Min(); }
+            get { return Durations.Count > 0 ? Durations.Min() : TimeSpan.Zero; }
         }
 
         public TimeSpan Longest
         {
-            get { return Durations.Max(); }
+            get { return Durations.Count > 0 ? Durations.Max() : TimeSpan.Zero; }
         }
 
         public TimeSpan Average
         {
-            get { return TimeSpan.FromMilliseconds(Durations.Average(m => m.TotalMilliseconds)); }
+            get { return TimeSpan.FromMilliseconds(Durations.Count > 0 ? Durations.Average(m => m.TotalMilliseconds) : 0.0); }
         }
 
         public TimeSpan Deviation
         {
             get
             {
+                if (Durations.Count == 0)
+                    return TimeSpan.Zero;
+
                 var avg = Durations.Average(m => m.TotalMilliseconds);
                 var sqr = Durations.Average(m => m.TotalMilliseconds * m.TotalMilliseconds);
                 var dev = Math.Sqrt(sqr - avg * avg);
