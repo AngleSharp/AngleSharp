fad5d57a71f98bd443411175bb7367dde065b704
Florian Rappl <rappl.florian@gmail.com>
2014-11-13 01:07:04 +0100

    Added some elementary tests for default sheet #36

---
+++ UnitTests/Library/BasicConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using AngleSharp;
+using AngleSharp.DOM.Css;
+using AngleSharp.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTests.Library
+{
+    [TestClass]
+    public class BasicConfigurationTests
+    {
+        [TestMethod]
+        public void HasStyleEngine()
+        {
+            var config = new Configuration();
+            var engine = config.StyleEngines.FirstOrDefault();
+            Assert.IsNotNull(engine);
+            Assert.IsInstanceOfType(engine, typeof(CssStyleEngine));
+        }
+
+        [TestMethod]
+        public void ObtainDefaultSheet()
+        {
+            var engine = new CssStyleEngine();
+            Assert.IsNotNull(engine.Default);
+            Assert.AreEqual("text/css", engine.Type);
+            var sheet = engine.Default as CSSStyleSheet;
+            Assert.IsNotNull(sheet);
+            Assert.AreEqual(49, sheet.Rules.Length);
+        }
+    }
+}
