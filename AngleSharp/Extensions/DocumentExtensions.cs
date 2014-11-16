07902f4db80794ba9499d2c869c2a1f7ee97a71e
Florian Rappl <rappl.florian@gmail.com>
2014-11-16 13:15:14 +0100

    Added useful document extensions, such as foreach range

---
+++ AngleSharp/Extensions/DocumentExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace AngleSharp.Extensions
+{
+    using AngleSharp.DOM;
+    using AngleSharp.DOM.Collections;
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Useful methods for document objects.
+    /// </summary>
+    [DebuggerStepThrough]
+    static class DocumentExtensions
+    {
+        /// <summary>
+        /// Iterates over all ranges in the document, applying the provided action
+        /// when the given condition is fulfilled.
+        /// </summary>
+        /// <param name="document">The document that hosts the ranges.</param>
+        /// <param name="condition">The condition that needs to be fulfilled.</param>
+        /// <param name="action">The action to apply to the range.</param>
+        public static void ForEachRange(this Document document, Predicate<Range> condition, Action<Range> action)
+        {
+            if (document == null)
+                return;
+
+            foreach (var range in document.Ranges)
+            {
+                if (condition(range))
+                    action(range);
+            }
+        }
+    }
+}
