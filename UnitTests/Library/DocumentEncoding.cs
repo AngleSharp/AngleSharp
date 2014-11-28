c0ad3fa0b408d1920f3e9bf2d5e36c33648b40d5
Florian Rappl <rappl.florian@gmail.com>
2014-11-28 08:11:03 +0100

    Added test for encoding change #42

---
+++ UnitTests/Library/DocumentEncoding.cs
@@ -0,0 +1,25 @@
+﻿using AngleSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+
+namespace UnitTests.Library
+{
+    [TestClass]
+    public class DocumentEncodingTests
+    {
+        [TestMethod]
+        public void BaiduEncodingTransformationEnToUtf8VsZhToUtf8()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var url = new Uri("http://www.baidu.com/", UriKind.Absolute);
+
+                var titleWithEnUs = DocumentBuilder.Html(url, new Configuration { Culture = new CultureInfo("en-US") }).Title;
+                var titleWithZhCn = DocumentBuilder.Html(url, new Configuration { Culture = new CultureInfo("zh-CN") }).Title;
+
+                Assert.AreEqual(titleWithEnUs, titleWithZhCn);
+            }
+        }
+    }
+}
