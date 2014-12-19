118dc1c9ffd3166b466bd0350ae7a7e5c534d459
Florian Rappl <rappl.florian@gmail.com>
2014-12-20 00:04:27 +0100

    Added unknown rule and property (not connected yet), see #37

---
+++ AngleSharp/DOM/Css/Properties/CSSUnknownProperty.cs
@@ -0,0 +1,38 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents an unknown property that takes every value.
+    /// </summary>
+    sealed class CSSUnknownProperty : CSSProperty
+    {
+        #region ctor
+
+        internal CSSUnknownProperty(String name, CSSStyleDeclaration rule)
+            : base(name, rule)
+        {
+            Reset();
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal override void Reset()
+        {
+        }
+
+        /// <summary>
+        /// Determines if the given value represents a valid state of this property.
+        /// </summary>
+        /// <param name="value">The state that should be used.</param>
+        /// <returns>True if the state is valid, otherwise false.</returns>
+        protected override Boolean IsValid(ICssValue value)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ AngleSharp/DOM/Css/Rules/CSSUnknownRule.cs
@@ -0,0 +1,63 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using AngleSharp.Extensions;
+    using System;
+
+    /// <summary>
+    /// Represents an unknown CSS rule.
+    /// </summary>
+    sealed class CSSUnknownRule : CSSGroupingRule
+    {
+        #region Fields
+
+        String _keyText;
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Creates a new unknown rule.
+        /// </summary>
+        public CSSUnknownRule()
+            : base(CssRuleType.Unknown)
+        {
+            _keyText = String.Empty;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the key text of the unknown rule.
+        /// </summary>
+        public String KeyText
+        {
+            get { return _keyText; }
+            set { _keyText = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void ReplaceWith(ICssRule rule)
+        {
+            var newRule = rule as CSSUnknownRule;
+            _keyText = newRule._keyText;
+            base.ReplaceWith(rule);
+        }
+
+        /// <summary>
+        /// Returns a CSS code representation of the rule.
+        /// </summary>
+        /// <returns>A string that contains the code.</returns>
+        protected override String ToCss()
+        {
+            return String.Concat(_keyText, " ", Rules.ToCssBlock());
+        }
+
+        #endregion
+    }
+}
