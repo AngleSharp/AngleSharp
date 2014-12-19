a192da43b6171f1f34c17237d710ec4ac137ef93
Florian Rappl <rappl.florian@gmail.com>
2014-12-20 00:10:55 +0100

    Added the various scripting state

---
+++ AngleSharp/DOM/Css/Constants/ScriptingState.cs
@@ -0,0 +1,25 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    /// <summary>
+    /// Enumeration to describe the current scripting state.
+    /// </summary>
+    public enum ScriptingState
+    {
+        /// <summary>
+        /// Scripts will not run for this document; either it doesn't
+        /// support a scripting language, or the support isn't active
+        /// for the current document.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Scripting is enabled during the initial page load, but is
+        /// not supported afterwards, e.g., printed pages.
+        /// </summary>
+        InitialOnly,
+        /// <summary>
+        /// Scripting of the page is supported in general and is active
+        /// for the current document.
+        /// </summary>
+        Enabled
+    }
+}
