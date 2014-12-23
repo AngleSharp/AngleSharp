31d3e54fd55fb2488f8694c6de27710fa1cff527
Florian Rappl <rappl.florian@gmail.com>
2014-12-23 15:55:04 +0100

    Added tests for media feature validation

--- UnitTests/Css/CssMediaFeatures.cs
+++ UnitTests/Css/CssMediaFeatures.cs
@@ -2,6 +2,7 @@
 using AngleSharp.Css.MediaFeatures;
 using AngleSharp.DOM.Css;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace UnitTests.Css
 {
@@ -37,6 +38,58 @@ namespace UnitTests.Css
 
             var illegal = MediaFeatureFactory.Create("illegal");
             Assert.IsNull(illegal);
+        }
+
+        [Test]
+        public void CssMediaWidthValidation()
+        {
+            var width = new WidthMediaFeature(FeatureNames.Width);
+            var check = width.TrySetValue(new Length(100, Length.Unit.Px));
+            var valid = width.Validate(new RenderDevice(100, 0));
+            var invalid = width.Validate(new RenderDevice(0, 0));
+            Assert.IsTrue(check);
+            Assert.IsTrue(valid);
+            Assert.IsFalse(invalid);
+        }
+
+        [Test]
+        public void CssMediaMaxHeightValidation()
+        {
+            var height = new HeightMediaFeature(FeatureNames.MaxHeight);
+            var check = height.TrySetValue(new Length(100, Length.Unit.Px));
+            var valid = height.Validate(new RenderDevice(0, 99));
+            var invalid = height.Validate(new RenderDevice(0, 101));
+            Assert.IsTrue(check);
+            Assert.IsTrue(valid);
+            Assert.IsFalse(invalid);
+        }
+
+        [Test]
+        public void CssMediaMinDeviceWidthValidation()
+        {
+            var devwidth = new DeviceWidthMediaFeature(FeatureNames.MinDeviceWidth);
+            var check = devwidth.TrySetValue(new Length(100, Length.Unit.Px));
+            var valid = devwidth.Validate(new RenderDevice(101, 0));
+            var invalid = devwidth.Validate(new RenderDevice(99, 0));
+            Assert.IsTrue(check);
+            Assert.IsTrue(valid);
+            Assert.IsFalse(invalid);
+        }
+
+        [Test]
+        public void CssMediaAspectRatio()
+        {
+            var ratio = new AspectRatioMediaFeature(FeatureNames.AspectRatio);
+            var check = ratio.TrySetValue(new CssValueList(new List<ICssValue>(new ICssValue[] {
+                new Number(1f, Number.Unit.Integer),
+                CssValue.Delimiter,
+                new Number(1f, Number.Unit.Integer)
+            })));
+            var valid = ratio.Validate(new RenderDevice(100, 100));
+            var invalid = ratio.Validate(new RenderDevice(16, 9));
+            Assert.IsTrue(check);
+            Assert.IsTrue(valid);
+            Assert.IsFalse(invalid);
         }
     }
 }
