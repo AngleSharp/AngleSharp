d2f70ad5973f06c2e16c8638e5a3048a67c64b17
Florian Rappl <rappl.florian@gmail.com>
2014-12-28 00:12:33 +0100

    Prepared rules for conditional checking

--- AngleSharp/DOM/Css/Rules/CSSSupportsRule.cs
+++ AngleSharp/DOM/Css/Rules/CSSSupportsRule.cs
@@ -3,6 +3,8 @@
     using AngleSharp.Css;
     using AngleSharp.Extensions;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// Represents an @supports rule.
@@ -11,8 +13,9 @@
     {
         #region Fields
 
-        String _condition;
-        Boolean _used;
+        ICondition _condition;
+
+        static readonly ICondition empty = new EmptyCondition();
 
         #endregion
 
@@ -21,8 +24,7 @@
         internal CSSSupportsRule()
             : base(CssRuleType.Supports)
         {
-            _condition = String.Empty;
-            _used = true;
+            _condition = empty;
         }
 
         #endregion
@@ -34,8 +36,8 @@
         /// </summary>
         public String ConditionText
         {
-            get { return _condition; }
-            set { _condition = value; }
+            get { return _condition.Text; }
+            set { /* Parse */ }//TODO
         }
 
         /// <summary>
@@ -43,8 +45,7 @@
         /// </summary>
         public Boolean IsSupported
         {
-            get { return _used; }
-            set { _used = value; }
+            get { return _condition.Check(); }
         }
 
         #endregion
@@ -55,13 +56,11 @@
         {
             base.ReplaceWith(rule);
             var newRule = rule as CSSSupportsRule;
-            _condition = newRule._condition;
-            _used = newRule._used;
+            ConditionText = newRule.ConditionText;
         }
 
         internal override Boolean IsValid(RenderDevice device)
         {
-            //TODO
             return true;
         }
 
@@ -75,7 +74,145 @@
         /// <returns>A string that contains the code.</returns>
         protected override String ToCss()
         {
-            return String.Concat("@supports ", _condition, " ", Rules.ToCssBlock());
+            return String.Concat("@supports ", ConditionText, " ", Rules.ToCssBlock());
+        }
+
+        #endregion
+
+        #region Rules
+
+        interface ICondition
+        {
+            Boolean Check();
+
+            String Text { get; }
+        }
+
+        sealed class AndCondition : ICondition
+        {
+            readonly ICondition[] _conditions;
+
+            public AndCondition(IEnumerable<ICondition> conditions)
+            {
+                _conditions = conditions.ToArray();
+            }
+
+            public String Text
+            {
+                get { return String.Join(" and ", _conditions.Select(m => m.Text)); }
+            }
+
+            public Boolean Check()
+            {
+                foreach (var condition in _conditions)
+                {
+                    if (condition.Check() == false)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+
+        sealed class OrCondition : ICondition
+        {
+            readonly ICondition[] _conditions;
+
+            public OrCondition(IEnumerable<ICondition> conditions)
+            {
+                _conditions = conditions.ToArray();
+            }
+
+            public String Text
+            {
+                get { return String.Join(" or ", _conditions.Select(m => m.Text)); }
+            }
+
+            public Boolean Check()
+            {
+                foreach (var condition in _conditions)
+                {
+                    if (condition.Check() == true)
+                        return true;
+                }
+
+                return false;
+            }
+        }
+
+        sealed class NotCondition : ICondition
+        {
+            readonly ICondition _content;
+
+            public NotCondition(ICondition content)
+            {
+                _content = content;
+            }
+
+            public String Text
+            {
+                get { return String.Concat("not ", _content.Text); }
+            }
+
+            public Boolean Check()
+            {
+                return !_content.Check();
+            }
+        }
+
+        sealed class EmptyCondition : ICondition
+        {
+            public String Text
+            {
+                get { return String.Empty; }
+            }
+
+            public Boolean Check()
+            {
+                return true;
+            }
+        }
+
+        sealed class DeclarationCondition : ICondition
+        {
+            readonly CSSProperty _property;
+            readonly ICssValue _value;
+
+            public DeclarationCondition(CSSProperty property, ICssValue value)
+            {
+                _property = property;
+                _value = value;
+            }
+
+            public String Text
+            {
+                get { return CSSProperty.Serialize(_property.Name, _value.CssText, _property.IsImportant); }
+            }
+
+            public Boolean Check()
+            {
+                return (_property is CSSUnknownProperty == false) && _property.TrySetValue(_value);
+            }
+        }
+
+        sealed class GroupCondition : ICondition
+        {
+            readonly ICondition _content;
+
+            public GroupCondition(ICondition content)
+            {
+                _content = content;
+            }
+
+            public String Text
+            {
+                get { return String.Concat("(", _content.Text, ")"); }
+            }
+
+            public Boolean Check()
+            {
+                return _content.Check();
+            }
         }
 
         #endregion
