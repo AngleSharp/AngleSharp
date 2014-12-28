430d21526e9d456f979dd6856c1246d436a18301
Florian Rappl <rappl.florian@gmail.com>
2014-12-28 22:32:53 +0100

    More tests for CSS conditionals

--- UnitTests/Css/CssSupports.cs
+++ UnitTests/Css/CssSupports.cs
@@ -72,5 +72,70 @@ namespace UnitTests.Css
             Assert.AreEqual("(background-transparency: zero)", supports.ConditionText);
             Assert.IsFalse(supports.IsSupported);
         }
+
+        [Test]
+        public void SupportsBackgroundRedWithImportantRule()
+        {
+            var source = @"@supports (background: red !important) { }";
+            var parser = new CssParser(source);
+            parser.Parse();
+            Assert.AreEqual(1, parser.Result.Rules.Length);
+            Assert.IsInstanceOf<CSSSupportsRule>(parser.Result.Rules[0]);
+            var supports = parser.Result.Rules[0] as CSSSupportsRule;
+            Assert.AreEqual("(background: red !important)", supports.ConditionText);
+            Assert.IsTrue(supports.IsSupported);
+        }
+
+        [Test]
+        public void SupportsPaddingTopOrPaddingLeftRule()
+        {
+            var source = @"@supports ((padding-TOP :  0) or (padding-left : 0)) { }";
+            var parser = new CssParser(source);
+            parser.Parse();
+            Assert.AreEqual(1, parser.Result.Rules.Length);
+            Assert.IsInstanceOf<CSSSupportsRule>(parser.Result.Rules[0]);
+            var supports = parser.Result.Rules[0] as CSSSupportsRule;
+            Assert.AreEqual("((padding-top: 0) or (padding-left: 0))", supports.ConditionText);
+            Assert.IsTrue(supports.IsSupported);
+        }
+
+        [Test]
+        public void SupportsPaddingTopOrPaddingLeftAndPaddingBottomOrPaddingRightRule()
+        {
+            var source = @"@supports (((padding-top: 0)  or  (padding-left: 0))  and  ((padding-bottom:  0)  or  (padding-right: 0))) { }";
+            var parser = new CssParser(source);
+            parser.Parse();
+            Assert.AreEqual(1, parser.Result.Rules.Length);
+            Assert.IsInstanceOf<CSSSupportsRule>(parser.Result.Rules[0]);
+            var supports = parser.Result.Rules[0] as CSSSupportsRule;
+            Assert.AreEqual("(((padding-top: 0) or (padding-left: 0)) and ((padding-bottom: 0) or (padding-right: 0)))", supports.ConditionText);
+            Assert.IsTrue(supports.IsSupported);
+        }
+
+        [Test]
+        public void SupportsDisplayFlexWithImportantRule()
+        {
+            var source = @"@supports (display: flex !important) { }";
+            var parser = new CssParser(source);
+            parser.Parse();
+            Assert.AreEqual(1, parser.Result.Rules.Length);
+            Assert.IsInstanceOf<CSSSupportsRule>(parser.Result.Rules[0]);
+            var supports = parser.Result.Rules[0] as CSSSupportsRule;
+            Assert.AreEqual("(display: flex !important)", supports.ConditionText);
+            Assert.IsTrue(supports.IsSupported);
+        }
+
+        [Test]
+        public void SupportsBareDisplayFlexRule()
+        {
+            var source = @"@supports display: flex { }";
+            var parser = new CssParser(source);
+            parser.Parse();
+            Assert.AreEqual(1, parser.Result.Rules.Length);
+            Assert.IsInstanceOf<CSSSupportsRule>(parser.Result.Rules[0]);
+            var supports = parser.Result.Rules[0] as CSSSupportsRule;
+            Assert.AreEqual(String.Empty, supports.ConditionText);
+            Assert.IsTrue(supports.IsSupported);
+        }
     }
 }
