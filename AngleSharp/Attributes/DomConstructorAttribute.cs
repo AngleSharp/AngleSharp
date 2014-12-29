c55984b7ba384365d8b2a38257d2704fa0fa896b
Florian Rappl <rappl.florian@gmail.com>
2014-12-29 21:03:22 +0100

    Added the DomConstructor attribute

---
+++ AngleSharp/Attributes/DomConstructorAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace AngleSharp.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// This attribute is used to mark a constructor as being
+    /// accessible from scripts.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public sealed class DomConstructorAttribute : Attribute
+    {
+        internal DomConstructorAttribute()
+        {
+        }
+    }
+}
