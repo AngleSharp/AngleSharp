8150a200de7db42df4bab00bc5f9d9db8991505c
Florian Rappl <rappl.florian@gmail.com>
2015-01-02 11:13:52 +0100

    More tests for type mismatch

--- UnitTests/Html/Validation.cs
+++ UnitTests/Html/Validation.cs
@@ -1,16 +1,16 @@
 ﻿using AngleSharp;
 using AngleSharp.DOM.Html;
 using NUnit.Framework;
-using System;
 
 namespace UnitTests.Html
 {
     [TestFixture]
     public class ValidationTests
     {
-        #region Forms
-
-        static readonly String Form1 = @"<form id=""fm1"">
+        [Test]
+        public void BasicInvalidFormValidation()
+        {
+            var doc = DocumentBuilder.Html(@"<form id=""fm1"">
   <fieldset id=""test0"">
     <input type=""text"" required value="""" id=""test1"">
   </fieldset>
@@ -19,32 +19,7 @@ namespace UnitTests.Html
   <select id=""test4""></select>
   <textarea id=""test5""></textarea>
   <output id=""test6""></output>
-</form>";
-
-        static readonly String Form2 = @"<form id=""fm2"">
-  <fieldset>
-    <input type=""text"" required value=""abc"">
-  </fieldset>
-  <input type=""email"" value=""test@example.com"">
-  <button>TEST</button>
-  <select></select>
-  <textarea></textarea>
-  <output></output>
-</form>";
-
-        static readonly String Form3 = @"<form id=""fm3"">
-  <fieldset id=""fs"">
-    <legend><input type=""text"" id=""inp1""></legend>
-    <input type=""text"" required value="""" id=""inp2"">
-  </fieldset>
-</form>";
-
-        #endregion
-
-        [Test]
-        public void BasicInvalidFormValidation()
-        {
-            var doc = DocumentBuilder.Html(Form1);
+</form>");
             Assert.AreEqual(1, doc.Forms.Length);
             var form = doc.Forms[0] as IHtmlFormElement;
             Assert.IsNotNull(form);
@@ -54,7 +29,16 @@ namespace UnitTests.Html
         [Test]
         public void BasicValidFormValidation()
         {
-            var doc = DocumentBuilder.Html(Form2);
+            var doc = DocumentBuilder.Html(@"<form id=""fm2"">
+  <fieldset>
+    <input type=""text"" required value=""abc"">
+  </fieldset>
+  <input type=""email"" value=""test@example.com"">
+  <button>TEST</button>
+  <select></select>
+  <textarea></textarea>
+  <output></output>
+</form>");
             Assert.AreEqual(1, doc.Forms.Length);
             var form = doc.Forms[0] as IHtmlFormElement;
             Assert.IsNotNull(form);
@@ -64,7 +48,12 @@ namespace UnitTests.Html
         [Test]
         public void InvalidFormValidationWithChanges()
         {
-            var doc = DocumentBuilder.Html(Form3);
+            var doc = DocumentBuilder.Html(@"<form id=""fm3"">
+  <fieldset id=""fs"">
+    <legend><input type=""text"" id=""inp1""></legend>
+    <input type=""text"" required value="""" id=""inp2"">
+  </fieldset>
+</form>");
             Assert.AreEqual(1, doc.Forms.Length);
             var form = doc.Forms[0] as IHtmlFormElement;
             Assert.IsNotNull(form);
@@ -182,6 +171,254 @@ namespace UnitTests.Html
             element.SetCustomValidity("");
             Assert.AreEqual(false, element.Validity.IsCustomError);
             Assert.AreEqual("", element.ValidationMessage);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail1()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail2()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "test@example.com");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail3()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", @"
+     test@example.com
+    ");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail4()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "abc");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(true, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail5()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "test1@example.com,test2@example.com");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(true, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail6()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("multiple", "multiple");
+            element.SetAttribute("value", "test1@example.com,test2@example.com");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputEmail7()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "email";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("multiple", "multiple");
+            element.SetAttribute("value", "test1@example.com;test2@example.com");
+            Assert.AreEqual("email", element.Type);
+            Assert.AreEqual(true, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputUrl1()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "url";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "");
+            Assert.AreEqual("url", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputUrl2()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "url";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "http://www.example.com");
+            Assert.AreEqual("url", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputUrl3()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "url";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", @"
+     http://www.example.com 
+     ");
+            Assert.AreEqual("url", element.Type);
+            Assert.AreEqual(false, element.Validity.IsTypeMismatch);
+        }
+
+        [Test]
+        public void TestTypemismatchInputUrl4()
+        {
+            var document = DocumentBuilder.Html("");
+            var element = document.CreateElement("input") as HTMLInputElement;
+            Assert.IsNotNull(element);
+            element.Type = "url";
+            element.RemoveAttribute("required");
+            element.RemoveAttribute("pattern");
+            element.RemoveAttribute("step");
+            element.RemoveAttribute("max");
+            element.RemoveAttribute("min");
+            element.RemoveAttribute("maxlength");
+            element.RemoveAttribute("value");
+            element.RemoveAttribute("multiple");
+            element.RemoveAttribute("checked");
+            element.RemoveAttribute("selected");
+            element.SetAttribute("value", "abc");
+            Assert.AreEqual("url", element.Type);
+            Assert.AreEqual(true, element.Validity.IsTypeMismatch);
         }
     }
 }
