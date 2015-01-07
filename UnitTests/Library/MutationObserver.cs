5e2629fde4ec270ade015e93e863680adc2e60b2
Florian Rappl <rappl.florian@gmail.com>
2015-01-07 13:59:01 +0100

    Added test class for mutation observer

---
+++ UnitTests/Library/MutationObserver.cs
@@ -0,0 +1,33 @@
+﻿using AngleSharp;
+using AngleSharp.DOM;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests.Library
+{
+    [TestFixture]
+    public class MutationObserverTests
+    {
+        [Test]
+        public void ConnectMutationObserverTriggerManually()
+        {
+            var called = false;
+
+            var observer = new MutationObserver((mut, obs) =>
+            {
+                called = mut.Length == 1 && mut[0].Added != null && mut[0].Added.Length == 1;
+            });
+
+            var document = DocumentBuilder.Html("");
+
+            observer.Connect(document.Body, new MutationObserverInit
+            {
+                ObserveTargetChildNodes = true
+            });
+
+            document.Body.AppendChild(document.CreateElement("span"));
+            observer.TriggerWith(observer.Flush().ToArray());
+            Assert.IsTrue(called);
+        }
+    }
+}
