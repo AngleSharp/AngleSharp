0777c0072c9a0bb958add3cc56ca44ee0bb8727c
Florian Rappl <rappl.florian@gmail.com>
2015-01-08 22:29:27 +0100

    Implemented target override

--- AngleSharp/Extensions/EventTargetExtensions.cs
+++ AngleSharp/Extensions/EventTargetExtensions.cs
@@ -10,10 +10,9 @@
     static class EventTargetExtensions
     {
         /// <summary>
-        /// Firing a simple event named e means that a trusted event with the name e,
-        /// which does not bubble (except where otherwise stated) and is not cancelable
-        /// (except where otherwise stated), and which uses the Event interface, must
-        /// be created and dispatched at the given target.
+        /// Firing a simple event named e means that a trusted event with a name, which
+        /// does not bubble, is not cancelable and which uses the Event interface. It is
+        /// created and dispatched at the given target.
         /// </summary>
         /// <param name="target">The target of the simple event.</param>
         /// <param name="eventName">The name of the event to be fired.</param>
@@ -28,20 +27,18 @@
         }
 
         /// <summary>
-        /// Firing an event means dispatching the initialized (and trusted) event
-        /// at the specified event target.
+        /// Firing an event means dispatching the initialized (and trusted) event at the specified event target.
         /// </summary>
-        /// <param name="target">The target of the event.</param>
+        /// <param name="target">The target, where the event has been invoked.</param>
         /// <param name="initializer">The used initializer.</param>
-        /// <param name="currentTarget">The current event target, if different to the target.</param>
+        /// <param name="targetOverride">The current event target, if different to the invoked target.</param>
         /// <returns>True if the element was cancelled, otherwise false.</returns>
-        public static Boolean Fire<T>(this EventTarget target, Action<T> initializer, IEventTarget currentTarget = null)
+        public static Boolean Fire<T>(this EventTarget target, Action<T> initializer, EventTarget targetOverride = null)
             where T : Event, new()
         {
             var ev = new T { IsTrusted = true };
             initializer(ev);
-            //TODO dispatch at currentTarget for target (!)
-            return ev.Dispatch(target);
+            return ev.Dispatch(targetOverride ?? target);
         }
     }
 }
