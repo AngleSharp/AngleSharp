17130d05e4023c5900a40c38cc6124d9292472a2
Florian Rappl <rappl.florian@gmail.com>
2015-01-09 09:56:35 +0100

    Pseudo-Implemented setting of system font

--- AngleSharp/DOM/Css/Properties/Font/CSSFontProperty.cs
+++ AngleSharp/DOM/Css/Properties/Font/CSSFontProperty.cs
@@ -157,20 +157,29 @@
         /// keyword can be used to represent a specific system font.
         /// </summary>
         /// <param name="font">The font to select.</param>
-        static void SetSystemFont(SystemFont font)
+        void SetSystemFont(SystemFont font)
         {
-            //TODO set properties to the setting given by the enumeration value
-
             switch (font)
             {
                 case SystemFont.Caption:
                 case SystemFont.Icon:
-                case SystemFont.Menu:
                 case SystemFont.MessageBox:
-                case SystemFont.SmallCaption:
+                    SetFont("Arial", "16px");
+                    break;
                 case SystemFont.StatusBar:
+                case SystemFont.Menu:
+                    SetFont("Segoe UI", "12px");
+                    break;
+                case SystemFont.SmallCaption:
+                    SetFont("Segoe UI", "15px");
                     break;
             }
+        }
+
+        void SetFont(String family, String size)
+        {
+            var value = String.Concat(size, " ", family.CssString());
+            Rule.SetPropertyValue(Name, value);
         }
 
         internal override String SerializeValue(IEnumerable<CssProperty> properties)
