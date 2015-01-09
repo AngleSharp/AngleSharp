8a34ec0acabc97ca2d27ecca61caccb787564f89
Florian Rappl <rappl.florian@gmail.com>
2015-01-09 16:50:26 +0100

    Added IPseudoElement and IGetStyleUtils interfaces

---
+++ AngleSharp/Interfaces/Css/IGetStyleUtils.cs
@@ -0,0 +1,37 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using AngleSharp.Attributes;
+
+    /// <summary>
+    /// Utilities for getting style information. See:
+    /// http://dev.w3.org/csswg/cssom/#getstyleutils
+    /// </summary>
+    [DomNoInterfaceObject]
+    [DomName("GetStyleUtil")]
+    public interface IGetStyleUtils
+    {
+        /// <summary>
+        /// Gets the cascaded style of the element.
+        /// </summary>
+        [DomName("cascadedStyle")]
+        ICssStyleDeclaration CascadedStyle { get; }
+
+        /// <summary>
+        /// Gets the default style of the element.
+        /// </summary>
+        [DomName("defaultStyle")]
+        ICssStyleDeclaration DefaultStyle { get; }
+
+        /// <summary>
+        /// Gets the raw computed style.
+        /// </summary>
+        [DomName("rawComputedStyle")]
+        ICssStyleDeclaration RawComputedStyle { get; }
+
+        /// <summary>
+        /// Gets the used style of the element.
+        /// </summary>
+        [DomName("usedStyle")]
+        ICssStyleDeclaration UsedStyle { get; }
+    }
+}
---
+++ AngleSharp/Interfaces/Css/IPseudoElement.cs
@@ -0,0 +1,12 @@
+﻿namespace AngleSharp.DOM.Css
+{
+    using AngleSharp.Attributes;
+
+    /// <summary>
+    /// The PseudoElement interface is used for representing CSS pseudo-elements.
+    /// </summary>
+    [DomName("PseudoElement")]
+    public interface IPseudoElement : IGetStyleUtils
+    {
+    }
+}
