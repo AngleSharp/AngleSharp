04fc82cd9fb5fd5fb78eae3c4a3d1e8988b7249f
Florian Rappl <rappl.florian@gmail.com>
2015-01-14 14:43:04 +0100

    Added documentation

--- AngleSharp/Css/PseudoElementNames.cs
+++ AngleSharp/Css/PseudoElementNames.cs
@@ -7,10 +7,29 @@
     /// </summary>
     static class PseudoElementNames
     {
+        /// <summary>
+        /// The before pseudo element.
+        /// </summary>
         public static readonly String Before = "before";
+
+        /// <summary>
+        /// The after pseudo element.
+        /// </summary>
         public static readonly String After = "after";
+
+        /// <summary>
+        /// The selection pseudo element.
+        /// </summary>
         public static readonly String Selection = "selection";
+
+        /// <summary>
+        /// The first-line pseudo element.
+        /// </summary>
         public static readonly String FirstLine = "first-line";
+
+        /// <summary>
+        /// The first-letter pseudo element.
+        /// </summary>
         public static readonly String FirstLetter = "first-letter";
     }
 }
