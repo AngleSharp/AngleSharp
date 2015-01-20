fd838347b8d33ff0fff149956a7a2231b2134289
Florian Rappl <rappl.florian@gmail.com>
2015-01-20 23:48:07 +0100

    Added a very simple dummy context

---
+++ AngleSharp/Foundation/SimpleBrowsingContext.cs
@@ -0,0 +1,61 @@
+﻿namespace AngleSharp
+{
+    using AngleSharp.DOM;
+
+    /// <summary>
+    /// A simple and lightweight browsing context.
+    /// </summary>
+    sealed class SimpleBrowsingContext : IBrowsingContext
+    {
+        #region Fields
+
+        readonly IConfiguration _configuration;
+        IDocument _active;
+
+        #endregion
+
+        #region ctor
+
+        public SimpleBrowsingContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IDocument Active
+        {
+            get { return _active; }
+            set { _active = value; }
+        }
+
+        public IConfiguration Configuration
+        {
+            get { return _configuration; }
+        }
+
+        public IDocument Creator
+        {
+            get { return null; }
+        }
+
+        public IWindow Current
+        {
+            get { return _active != null ? _active.DefaultView : null; }
+        }
+
+        public IBrowsingContext Parent
+        {
+            get { return null; }
+        }
+
+        public IHistory SessionHistory
+        {
+            get { return null; }
+        }
+
+        #endregion
+    }
+}
