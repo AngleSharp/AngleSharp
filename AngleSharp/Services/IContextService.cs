dbf49cea1e737a175f0cff24e188671cd6076df8
Florian Rappl <rappl.florian@gmail.com>
2015-01-23 15:57:58 +0100

    Added the context service.

---
+++ AngleSharp/Services/IContextService.cs
@@ -0,0 +1,33 @@
+﻿namespace AngleSharp.Services
+{
+    using AngleSharp.DOM;
+    using System;
+
+    /// <summary>
+    /// Defines methods to create or find browsing contexts.
+    /// </summary>
+    public interface IContextService : IService
+    {
+        /// <summary>
+        /// Creates a new browsing context without any particular name.
+        /// </summary>
+        /// <returns>The created browsing context.</returns>
+        IBrowsingContext Create();
+
+        /// <summary>
+        /// Creates a new browsing context with the given name,
+        /// instructed by the specified document.
+        /// </summary>
+        /// <param name="name">The name of the new context.</param>
+        /// <param name="creator">The creator of the context.</param>
+        /// <returns>The created browsing context.</returns>
+        IBrowsingContext Create(String name, IDocument creator);
+
+        /// <summary>
+        /// Tries to find a browsing context with the given name.
+        /// </summary>
+        /// <param name="name">The name of the context.</param>
+        /// <returns>A context with the name, otherwise null.</returns>
+        IBrowsingContext Find(String name);
+    }
+}
