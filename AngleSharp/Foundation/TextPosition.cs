14cf6be7d6e314ac887a89cbdd1e801275ffb666
Florian Rappl <rappl.florian@gmail.com>
2015-01-26 09:19:10 +0100

    Implemented useful interfaces

--- AngleSharp/Foundation/TextPosition.cs
+++ AngleSharp/Foundation/TextPosition.cs
@@ -7,7 +7,7 @@
     /// The position in the source code.
     /// </summary>
     [DebuggerStepThrough]
-    public struct TextPosition
+    public struct TextPosition : IEquatable<TextPosition>, IComparable<TextPosition>
     {
         #region Fields
 
@@ -58,6 +58,65 @@
         public Int32 Position
         {
             get { return _position; }
+        }
+
+        #endregion
+
+        #region Comparison
+
+        /// <summary>
+        /// Indicates whether this instance and a specified object are equal.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current instance.</param>
+        /// <returns>True if the given object is a text position with the same values, otherwise false.</returns>
+        public override Boolean Equals(Object obj)
+        {
+            if (obj is TextPosition)
+                return Equals((TextPosition)obj);
+
+            return false;
+        }
+
+        /// <summary>
+        /// Indicates whether the current position is equal to the given position.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>True if the given position has the same values, otherwise false.</returns>
+        public Boolean Equals(TextPosition other)
+        {
+            return this._position == other._position && this._column == other._column && this._line == other._line;
+        }
+
+        /// <summary>
+        /// Compares the two positions by their absolute positions in the text source.
+        /// </summary>
+        /// <param name="a">The first operand.</param>
+        /// <param name="b">The second operand.</param>
+        /// <returns>True if the position of the first operand is greater than the second operand.</returns>
+        public static Boolean operator >(TextPosition a, TextPosition b)
+        {
+            return a._position > b._position;
+        }
+
+        /// <summary>
+        /// Compares the two positions by their absolute positions in the text source.
+        /// </summary>
+        /// <param name="a">The first operand.</param>
+        /// <param name="b">The second operand.</param>
+        /// <returns>True if the position of the first operand is less than the second operand.</returns>
+        public static Boolean operator <(TextPosition a, TextPosition b)
+        {
+            return a._position < b._position;
+        }
+
+        /// <summary>
+        /// Compares the current position with another position.
+        /// </summary>
+        /// <param name="other">The position to compare to.</param>
+        /// <returns>A mathematical representation of the relation (1 = greater, -1 = less, 0 = equal).</returns>
+        public Int32 CompareTo(TextPosition other)
+        {
+            return this.Equals(other) ? 0 : (this > other ? 1 : -1);
         }
 
         #endregion
