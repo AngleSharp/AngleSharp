dda3931b10952f91ee6fbbad7f1c1379da029ce5
Florian Rappl <rappl.florian@gmail.com>
2015-01-27 20:08:35 +0100

    Added the navigator service

---
+++ AngleSharp/Services/INavigatorService.cs
@@ -0,0 +1,18 @@
+﻿namespace AngleSharp.Services
+{
+    using AngleSharp.DOM;
+    using AngleSharp.DOM.Navigator;
+
+    /// <summary>
+    /// Defines methods to create INavigator instances.
+    /// </summary>
+    public interface INavigatorService : IService
+    {
+        /// <summary>
+        /// Creates an INavigator object for the provided window.
+        /// </summary>
+        /// <param name="window">The window that needs an INavigator.</param>
+        /// <returns>The INavigator instance for the window.</returns>
+        INavigator Create(IWindow window);
+    }
+}
