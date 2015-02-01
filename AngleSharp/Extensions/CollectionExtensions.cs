3e1b3cc716f2a3c546820bc3a0756615f52a4e9c
Florian Rappl <rappl.florian@gmail.com>
2015-02-01 22:23:23 +0100

    Refactored and supplied documentation

--- AngleSharp/Extensions/CollectionExtensions.cs
+++ AngleSharp/Extensions/CollectionExtensions.cs
@@ -12,15 +12,25 @@
     [DebuggerStepThrough]
     static class CollectionExtensions
     {
+        #region Methods
+
+        /// <summary>
+        /// Gets the descendents from the provided parent that fulfill the
+        /// specified predicate, if any.
+        /// </summary>
+        /// <typeparam name="T">The type of elements to obtain.</typeparam>
+        /// <param name="parent">The parent of the descendents.</param>
+        /// <param name="deep">
+        /// True if all descendents, false if only direct descendents should be
+        /// considered.
+        /// </param>
+        /// <param name="predicate">The filter function, if any.</param>
+        /// <param name="result">The result collection.</param>
         public static IEnumerable<T> GetElements<T>(this INode parent, Boolean deep = true, Predicate<T> predicate = null)
             where T : class, INode
         {
             predicate = predicate ?? (m => true);
-
-            if (deep)
-                return GetAllElementsOf<T>(parent, predicate);
-            else
-                return GetOnlyElementsOf<T>(parent, predicate);
+            return deep ? parent.GetAllElements(predicate) : parent.GetDescendendElements(predicate);
         }
 
         /// <summary>
@@ -28,7 +38,7 @@
         /// </summary>
         /// <param name="children">The nodelist to investigate.</param>
         /// <param name="id">The id to find.</param>
-        /// <returns>The element or NULL.</returns>
+        /// <returns>The element or null.</returns>
         public static IElement GetElementById(this INodeList children, String id)
         {
             for (int i = 0; i < children.Length; i++)
@@ -72,45 +82,57 @@
             }
         }
 
+        /// <summary>
+        /// Determines if the current filter settings includes the provided
+        /// node.
+        /// </summary>
+        /// <param name="filter">The filter settings to use.</param>
+        /// <param name="node">The node to check against.</param>
+        /// <returns>True if the node is accepted, otherwise false.</returns>
         public static Boolean Accepts(this FilterSettings filter, INode node)
         {
             switch (node.NodeType)
             {
-                case NodeType.Attribute:
-                    return filter.HasFlag(FilterSettings.Attribute);
-                case NodeType.CharacterData:
-                    return filter.HasFlag(FilterSettings.CharacterData);
-                case NodeType.Comment:
-                    return filter.HasFlag(FilterSettings.Comment);
-                case NodeType.Document:
-                    return filter.HasFlag(FilterSettings.Document);
-                case NodeType.DocumentFragment:
-                    return filter.HasFlag(FilterSettings.DocumentFragment);
-                case NodeType.DocumentType:
-                    return filter.HasFlag(FilterSettings.DocumentType);
-                case NodeType.Element:
-                    return filter.HasFlag(FilterSettings.Element);
-                case NodeType.Entity:
-                    return filter.HasFlag(FilterSettings.Entity);
-                case NodeType.EntityReference:
-                    return filter.HasFlag(FilterSettings.EntityReference);
-                case NodeType.ProcessingInstruction:
-                    return filter.HasFlag(FilterSettings.ProcessingInstruction);
-                case NodeType.Notation:
-                    return filter.HasFlag(FilterSettings.Notation);
-                case NodeType.Text:
-                    return filter.HasFlag(FilterSettings.Text);
+                case NodeType.Attribute:             return filter.HasFlag(FilterSettings.Attribute);
+                case NodeType.CharacterData:         return filter.HasFlag(FilterSettings.CharacterData);
+                case NodeType.Comment:               return filter.HasFlag(FilterSettings.Comment);
+                case NodeType.Document:              return filter.HasFlag(FilterSettings.Document);
+                case NodeType.DocumentFragment:      return filter.HasFlag(FilterSettings.DocumentFragment);
+                case NodeType.DocumentType:          return filter.HasFlag(FilterSettings.DocumentType);
+                case NodeType.Element:               return filter.HasFlag(FilterSettings.Element);
+                case NodeType.Entity:                return filter.HasFlag(FilterSettings.Entity);
+                case NodeType.EntityReference:       return filter.HasFlag(FilterSettings.EntityReference);
+                case NodeType.ProcessingInstruction: return filter.HasFlag(FilterSettings.ProcessingInstruction);
+                case NodeType.Notation:              return filter.HasFlag(FilterSettings.Notation);
+                case NodeType.Text:                  return filter.HasFlag(FilterSettings.Text);
             }
 
             return filter == FilterSettings.All;
         }
 
+        /// <summary>
+        /// Gets the elements that satisfy the provided filter settings.
+        /// </summary>
+        /// <typeparam name="T">The type of nodes to obtain.</typeparam>
+        /// <param name="parent">The parent of the nodes to find.</param>
+        /// <param name="filter">The filter settings to apply.</param>
+        /// <returns>
+        /// The filtered list of all descendents from the provided node.
+        /// </returns>
         public static IEnumerable<T> GetElements<T>(this INode parent, FilterSettings filter)
             where T : class, INode
         {
             return parent.GetElements<T>(predicate: (node => filter.Accepts(node)));
         }
 
+        /// <summary>
+        /// Gets the element with the provided id, if any. Otherwise the
+        /// element with the same name is searched.
+        /// </summary>
+        /// <typeparam name="T">The type of node to obtain.</typeparam>
+        /// <param name="elements">The list of elements to filter.</param>
+        /// <param name="id">The id of the element to find.</param>
+        /// <returns>The element with the given id, or null.</returns>
         public static T GetElementById<T>(this IEnumerable<T> elements, String id)
             where T : class, IElement
         {
@@ -129,7 +151,11 @@
             return null;
         }
 
-        static IEnumerable<T> GetAllElementsOf<T>(INode parent, Predicate<T> predicate)
+        #endregion
+
+        #region Helpers
+
+        static IEnumerable<T> GetAllElements<T>(this INode parent, Predicate<T> predicate)
             where T : class, INode
         {
             for (int i = 0; i < parent.ChildNodes.Length; i++)
@@ -139,12 +165,12 @@
                 if (child != null && predicate(child))
                     yield return child;
 
-                foreach (var element in GetAllElementsOf<T>(parent.ChildNodes[i], predicate))
+                foreach (var element in parent.ChildNodes[i].GetAllElements<T>(predicate))
                     yield return element;
             }
         }
 
-        static IEnumerable<T> GetOnlyElementsOf<T>(INode parent, Predicate<T> predicate)
+        static IEnumerable<T> GetDescendendElements<T>(this INode parent, Predicate<T> predicate)
             where T : class, INode
         {
             for (int i = 0; i < parent.ChildNodes.Length; i++)
@@ -155,5 +181,7 @@
                     yield return child;
             }
         }
+
+        #endregion
     }
 }
