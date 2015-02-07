892c08cf30cf9f6d386574b84dd1647b5ab6eb44
Florian Rappl <rappl.florian@gmail.com>
2015-02-07 11:10:13 +0100

    Guarded and removed obsolete methods

--- AngleSharp/ConfigurationExtensions.cs
+++ AngleSharp/ConfigurationExtensions.cs
@@ -1,12 +1,9 @@
 ﻿namespace AngleSharp
 {
-    using AngleSharp.Dom;
     using AngleSharp.Dom.Css;
     using AngleSharp.Network;
     using System;
-    using System.IO;
     using System.Linq;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// A set of useful extensions for IConfiguration and Configuration objects.
@@ -24,6 +21,9 @@
         public static TConfiguration WithCss<TConfiguration>(this TConfiguration configuration)
             where TConfiguration : Configuration
         {
+            if (configuration == null)
+                throw new ArgumentNullException("configuration");
+
             configuration.IsStyling = true;
 
             if (configuration.StyleEngines.OfType<CssStyleEngine>().Any() == false)
@@ -41,6 +41,9 @@
         public static TConfiguration WithStyling<TConfiguration>(this TConfiguration configuration)
             where TConfiguration : IConfiguration
         {
+            if (configuration == null)
+                throw new ArgumentNullException("configuration");
+
             configuration.IsStyling = true;
             return configuration;
         }
@@ -58,6 +61,9 @@
         public static TConfiguration WithScripting<TConfiguration>(this TConfiguration configuration)
             where TConfiguration : IConfiguration
         {
+            if (configuration == null)
+                throw new ArgumentNullException("configuration");
+
             configuration.IsScripting = true;
             return configuration;
         }
@@ -76,86 +82,11 @@
         public static TConfiguration WithDefaultRequester<TConfiguration>(this TConfiguration configuration, String agent = null)
             where TConfiguration : Configuration
         {
+            if (configuration == null)
+                throw new ArgumentNullException("configuration");
+
             configuration.Register(new DefaultRequester(agent));
             return configuration;
-        }
-
-        #endregion
-
-        #region HTML creation
-
-        /// <summary>
-        /// Builds a new HTML Document with the given source code string.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="sourceCode">The string to use as source code.</param>
-        /// <param name="url">[Optional] The base URL of the document.</param>
-        /// <returns>The constructed HTML document.</returns>
-        public static IDocument ParseHtml(this IConfiguration configuration, String sourceCode, String url = null)
-        {
-            return DocumentBuilder.Html(sourceCode, configuration, url);
-        }
-
-        /// <summary>
-        /// Builds a new HTML Document asynchronously with the given (network) stream.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="content">The stream of chars to use as source code.</param>
-        /// <param name="url">[Optional] The base URL of the document.</param>
-        /// <returns>The task to construct the HTML document.</returns>
-        public static Task<IDocument> ParseHtmlAsync(this IConfiguration configuration, Stream content, String url = null)
-        {
-            return DocumentBuilder.HtmlAsync(content, configuration, url);
-        }
-
-        /// <summary>
-        /// Builds a new HTML Document by asynchronously requesting the given URL.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="url">The URL which points to the address containing the source code.</param>
-        /// <returns>The task that constructs the HTML document.</returns>
-        public static Task<IDocument> ParseHtmlAsync(this IConfiguration configuration, Uri url)
-        {
-            return DocumentBuilder.HtmlAsync(url, configuration);
-        }
-
-        #endregion
-
-        #region CSS creation
-
-        /// <summary>
-        /// Builds a new CSSStyleSheet with the given source code string.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="sourceCode">The string to use as source code.</param>
-        /// <param name="url">[Optional] The base URL of the document.</param>
-        /// <returns>The constructed CSS stylesheet.</returns>
-        public static ICssStyleSheet ParseCss(this IConfiguration configuration, String sourceCode, String url = null)
-        {
-            return DocumentBuilder.Css(sourceCode, configuration, url);
-        }
-
-        /// <summary>
-        /// Builds a new CSSStyleSheet asynchronously by requesting the given (network) stream.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="stream">The stream of chars to use as source code.</param>
-        /// <param name="url">[Optional] The base URL of the document.</param>
-        /// <returns>The task which constructs the CSS stylesheet.</returns>
-        public static Task<ICssStyleSheet> ParseCssAsync(this IConfiguration configuration, Stream stream, String url = null)
-        {
-            return DocumentBuilder.CssAsync(stream, configuration, url);
-        }
-
-        /// <summary>
-        /// Builds a new CSSStyleSheet asynchronously by requesting the given URL.
-        /// </summary>
-        /// <param name="configuration">Options to use for the document generation.</param>
-        /// <param name="url">The URL which points to the address containing the source code.</param>
-        /// <returns>The task which constructs the CSS stylesheet.</returns>
-        public static Task<ICssStyleSheet> ParseCssAsync(this IConfiguration configuration, Uri url)
-        {
-            return DocumentBuilder.CssAsync(url, configuration);
         }
 
         #endregion
