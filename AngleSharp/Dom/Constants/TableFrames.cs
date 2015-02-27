56a2c045fd85efb2bb44a50994ef2bfede845291
Florian Rappl <rappl.florian@gmail.com>
2015-02-27 22:43:39 +0100

    Extracted constants

---
+++ AngleSharp/Dom/Constants/TableFrames.cs
@@ -0,0 +1,45 @@
+﻿namespace AngleSharp.Dom.Html
+{
+    /// <summary>
+    /// The list of possible table frame directives.
+    /// </summary>
+    public enum TableFrames
+    {
+        /// <summary>
+        /// No sides. This is the default value.
+        /// </summary>
+        Void,
+        /// <summary>
+        /// All four sides.
+        /// </summary>
+        Box,
+        /// <summary>
+        /// The top side only.
+        /// </summary>
+        Above,
+        /// <summary>
+        /// The bottom side only.
+        /// </summary>
+        Below,
+        /// <summary>
+        /// The top and bottom sides only.
+        /// </summary>
+        HSides,
+        /// <summary>
+        /// The right and left sides only.
+        /// </summary>
+        VSides,
+        /// <summary>
+        /// The left-hand side only.
+        /// </summary>
+        LHS,
+        /// <summary>
+        /// The right-hand side only.
+        /// </summary>
+        RHS,
+        /// <summary>
+        /// All four sides.
+        /// </summary>
+        Border
+    }
+}
---
+++ AngleSharp/Dom/Constants/TableRules.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp.Dom.Html
+{
+    /// <summary>
+    /// The list of possible table rules.
+    /// </summary>
+    public enum TableRules
+    {
+        /// <summary>
+        /// No rules. This is the default value.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Rules will appear between rows only.
+        /// </summary>
+        Rows,
+        /// <summary>
+        /// Rules will appear between columns only.
+        /// </summary>
+        Cols,
+        /// <summary>
+        /// Rules will appear between row groups and column groups only.
+        /// </summary>
+        Groups,
+        /// <summary>
+        /// Rules will appear between all rows and columns.
+        /// </summary>
+        All
+    }
+}
