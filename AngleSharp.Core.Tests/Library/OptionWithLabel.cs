00e1184b1dfc2ecfd00d41b70b01f5b6243bd431
Florian Rappl <rappl.florian@gmail.com>
2015-02-28 23:42:47 +0100

    Tests for the value attribute of the option element

--- AngleSharp.Core.Tests/Library/OptionWithLabel.cs
+++ AngleSharp.Core.Tests/Library/OptionWithLabel.cs
@@ -114,5 +114,114 @@ namespace AngleSharp.Core.Tests.Library
             option.SetAttribute("http://www.example.com/", "label", "label");
             Assert.AreEqual("child node", option.Label);
         }
+
+        [Test]
+        public void OptionNoChildrenEmptyValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.SetAttribute("value", "");
+            Assert.AreEqual("", option.Value);
+        }
+
+        [Test]
+        public void OptionNoChildrenValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.SetAttribute("value", "value");
+            Assert.AreEqual("value", option.Value);
+        }
+
+        [Test]
+        public void OptionNoChildrenNamespacedValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.SetAttribute("http://www.example.com/", "value", "");
+            Assert.AreEqual("", option.Value);
+        }
+
+        [Test]
+        public void OptionSingleChildNoValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            Assert.AreEqual("child", option.Value);
+        }
+
+        [Test]
+        public void OptionSingleChildEmptyValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.SetAttribute("value", "");
+            Assert.AreEqual("", option.Value);
+        }
+
+        [Test]
+        public void OptionSingleChildValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.SetAttribute("value", "value");
+            Assert.AreEqual("value", option.Value);
+        }
+
+        [Test]
+        public void OptionSingleChildNamespacedValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.SetAttribute("http://www.example.com/", "value", "value");
+            Assert.AreEqual("child", option.Value);
+        }
+
+        [Test]
+        public void OptionTwoChildrenNoValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.AppendChild(document.CreateTextNode(" node "));
+            Assert.AreEqual("child node", option.Value);
+        }
+
+        [Test]
+        public void OptionTwoChildrenEmptyValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.AppendChild(document.CreateTextNode(" node "));
+            option.SetAttribute("value", "");
+            Assert.AreEqual("", option.Value);
+        }
+
+        [Test]
+        public void OptionTwoChildrenValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.AppendChild(document.CreateTextNode(" node "));
+            option.SetAttribute("value", "value");
+            Assert.AreEqual("value", option.Value);
+        }
+
+        [Test]
+        public void OptionTwoChildrenNamespacedValue()
+        {
+            var document = DocumentBuilder.Html("");
+            var option = document.CreateElement<IHtmlOptionElement>();
+            option.AppendChild(document.CreateTextNode(" child "));
+            option.AppendChild(document.CreateTextNode(" node "));
+            option.SetAttribute("http://www.example.com/", "value", "value");
+            Assert.AreEqual("child node", option.Value);
+        }
     }
 }
