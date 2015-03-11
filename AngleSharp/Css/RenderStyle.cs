f3c87358566db976ade986d2da20841a89199f2f
Florian Rappl <rappl.florian@gmail.com>
2015-03-11 23:14:41 +0100

    Added renderstyle

---
+++ AngleSharp/Css/RenderStyle.cs
@@ -0,0 +1,47 @@
+﻿namespace AngleSharp.Css
+{
+    using AngleSharp.Dom.Css;
+    using System;
+
+    /// <summary>
+    /// A computed style related to a render device.
+    /// </summary>
+    public class RenderStyle
+    {
+        /// <summary>
+        /// Gets or sets the height of the style.
+        /// </summary>
+        public Int32 Height
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the style.
+        /// </summary>
+        public Int32 Width
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the text color.
+        /// </summary>
+        public Color ForeColor
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the back color.
+        /// </summary>
+        public Color BackColor
+        {
+            get;
+            set;
+        }
+    }
+}
