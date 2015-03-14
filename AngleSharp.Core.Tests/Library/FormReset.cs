37a1e59b350f8c8722527fb8f02f91e7bf6f986b
Florian Rappl <rappl.florian@gmail.com>
2015-03-14 14:34:13 +0100

    Check form reset even with simulated buttons

--- AngleSharp.Core.Tests/Library/FormReset.cs
+++ AngleSharp.Core.Tests/Library/FormReset.cs
@@ -107,5 +107,83 @@ namespace AngleSharp.Core.Tests.Library
             Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[1].IsSelected);
             Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[2].IsSelected);
         }
+
+        [Test]
+        public void ResetFormByClickingButtonInResetStatus()
+        {
+            var document = DocumentBuilder.Html(documentSource);
+            document.QuerySelector<IHtmlInputElement>("#ipt1").Value = "123";
+            document.QuerySelector<IHtmlInputElement>("#ipt2").Value = "123";
+            document.QuerySelector<IHtmlInputElement>("#rd1").IsChecked = false;
+            document.QuerySelector<IHtmlInputElement>("#rd2").IsChecked = true;
+            document.QuerySelector<IHtmlInputElement>("#cb1").IsChecked = false;
+            document.QuerySelector<IHtmlInputElement>("#cb2").IsChecked = true;
+            document.QuerySelector<IHtmlTextAreaElement>("#ta").Value = "123";
+            document.QuerySelector<IHtmlOutputElement>("#opt").TextContent = "abc";
+            document.QuerySelector<IHtmlSelectElement>("#slt1").Value = "2";
+            document.QuerySelector<IHtmlSelectElement>("#slt2").Value = "1";
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[0].IsSelected = true;
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[1].IsSelected = false;
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[2].IsSelected = false;
+
+            document.QuerySelector<IHtmlButtonElement>("#rst1").DoClick();
+
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlInputElement>("#ipt1").Value);
+            Assert.AreEqual("", document.QuerySelector<IHtmlInputElement>("#ipt2").Value);
+            Assert.IsTrue(document.QuerySelector<IHtmlInputElement>("#rd1").IsChecked);
+            Assert.IsFalse(document.QuerySelector<IHtmlInputElement>("#rd2").IsChecked);
+            Assert.IsTrue(document.QuerySelector<IHtmlInputElement>("#cb1").IsChecked);
+            Assert.IsFalse(document.QuerySelector<IHtmlInputElement>("#cb2").IsChecked);
+            Assert.AreEqual(document.QuerySelector<IHtmlTextAreaElement>("#ta").TextContent, document.QuerySelector<IHtmlTextAreaElement>("#ta").Value);
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlTextAreaElement>("#ta").Value);
+            Assert.AreEqual(document.QuerySelector<IHtmlOutputElement>("#opt").DefaultValue, document.QuerySelector<IHtmlOutputElement>("#opt").TextContent);
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlOutputElement>("#opt").TextContent);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt1").Options[0].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt1").Options[1].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt2").Options[0].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt2").Options[1].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[0].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[1].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[2].IsSelected);
+        }
+
+        [Test]
+        public void ResetFormByClickingResetButton()
+        {
+            var document = DocumentBuilder.Html(documentSource);
+            document.QuerySelector<IHtmlInputElement>("#ipt1").Value = "123";
+            document.QuerySelector<IHtmlInputElement>("#ipt2").Value = "123";
+            document.QuerySelector<IHtmlInputElement>("#rd1").IsChecked = false;
+            document.QuerySelector<IHtmlInputElement>("#rd2").IsChecked = true;
+            document.QuerySelector<IHtmlInputElement>("#cb1").IsChecked = false;
+            document.QuerySelector<IHtmlInputElement>("#cb2").IsChecked = true;
+            document.QuerySelector<IHtmlTextAreaElement>("#ta").Value = "123";
+            document.QuerySelector<IHtmlOutputElement>("#opt").TextContent = "abc";
+            document.QuerySelector<IHtmlSelectElement>("#slt1").Value = "2";
+            document.QuerySelector<IHtmlSelectElement>("#slt2").Value = "1";
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[0].IsSelected = true;
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[1].IsSelected = false;
+            document.QuerySelector<IHtmlSelectElement>("#slt3").Options[2].IsSelected = false;
+
+            document.QuerySelector<IHtmlInputElement>("#rst2").DoClick();
+
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlInputElement>("#ipt1").Value);
+            Assert.AreEqual("", document.QuerySelector<IHtmlInputElement>("#ipt2").Value);
+            Assert.IsTrue(document.QuerySelector<IHtmlInputElement>("#rd1").IsChecked);
+            Assert.IsFalse(document.QuerySelector<IHtmlInputElement>("#rd2").IsChecked);
+            Assert.IsTrue(document.QuerySelector<IHtmlInputElement>("#cb1").IsChecked);
+            Assert.IsFalse(document.QuerySelector<IHtmlInputElement>("#cb2").IsChecked);
+            Assert.AreEqual(document.QuerySelector<IHtmlTextAreaElement>("#ta").TextContent, document.QuerySelector<IHtmlTextAreaElement>("#ta").Value);
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlTextAreaElement>("#ta").Value);
+            Assert.AreEqual(document.QuerySelector<IHtmlOutputElement>("#opt").DefaultValue, document.QuerySelector<IHtmlOutputElement>("#opt").TextContent);
+            Assert.AreEqual("abc", document.QuerySelector<IHtmlOutputElement>("#opt").TextContent);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt1").Options[0].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt1").Options[1].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt2").Options[0].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt2").Options[1].IsSelected);
+            Assert.IsFalse(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[0].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[1].IsSelected);
+            Assert.IsTrue(document.QuerySelector<IHtmlSelectElement>("#slt3").Options[2].IsSelected);
+        }
     }
 }
