8a1aad16837b859196e2f0923203e9a3dbd80b01
Florian Rappl <rappl.florian@gmail.com>
2015-03-14 16:30:18 +0100

    Added clipboard

---
+++ AngleSharp/Services/IClipboardService.cs
@@ -0,0 +1,15 @@
+﻿namespace AngleSharp.Services
+{
+    using System;
+
+    /// <summary>
+    /// Defines methods to manipulate the clipboard.
+    /// </summary>
+    public interface IClipboardService : IService
+    {
+        /// <summary>
+        /// Gets or sets the text that is stored on the clipboard.
+        /// </summary>
+        String Text { get; set; }
+    }
+}
