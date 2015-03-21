da52973dc6cdcdef00f0179c35fadeff41e54d35
Florian Rappl <rappl.florian@gmail.com>
2015-03-21 13:32:28 +0100

    Added the ISvgDocument interface

--- AngleSharp/Interfaces/Xml/IXmlDocument.cs
+++ AngleSharp/Interfaces/Xml/IXmlDocument.cs
@@ -4,10 +4,7 @@
     using System;
 
     /// <summary>
-    /// The XMLDocument interface represent an XML document. It inherits from
-    /// the generic Document and do not add any specific methods or properties
-    /// to it: nevertheless, several algorithms behave differently with the two
-    /// types of documents.
+    /// The interface represent an XML document.
     /// </summary>
     [DomName("XMLDocument")]
     public interface IXmlDocument : IDocument
