08229b2443694f27401982508522bbb0838427ab
Florian Rappl <rappl.florian@gmail.com>
2015-03-28 13:15:29 +0100

    Include document and resource loader

---
+++ AngleSharp/Network/IDocumentLoader.cs
@@ -0,0 +1,10 @@
+﻿namespace AngleSharp.Network
+{
+    /// <summary>
+    /// Interface used to handle top-level document requests. These requests
+    /// include navigation tasks.
+    /// </summary>
+    interface IDocumentLoader
+    {
+    }
+}
---
+++ AngleSharp/Network/IResourceLoader.cs
@@ -0,0 +1,11 @@
+﻿namespace AngleSharp.Network
+{
+    /// <summary>
+    /// Interface used to handle resource requests for a document. These
+    /// requests include, but are not limited to, media, script and styling
+    /// resources.
+    /// </summary>
+    interface IResourceLoader
+    {
+    }
+}
