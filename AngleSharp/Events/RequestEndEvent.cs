aae388db5ecbcf5259050f3f0aa50442a469b7f8
Florian Rappl <rappl.florian@gmail.com>
2015-03-29 11:38:41 +0200

    Added events for starting and ending requests

---
+++ AngleSharp/Events/RequestEndEvent.cs
@@ -0,0 +1,39 @@
+﻿namespace AngleSharp.Events
+{
+    using AngleSharp.Network;
+
+    /// <summary>
+    /// The event that is published in case of a finished request.
+    /// </summary>
+    public class RequestEndEvent
+    {
+        /// <summary>
+        /// Creates a new event for finishing a request.
+        /// </summary>
+        /// <param name="requester">The associated requester.</param>
+        /// <param name="request">The data of the request.</param>
+        public RequestEndEvent(IRequester requester, IRequest request)
+        {
+            Requester = requester;
+            Request = request;
+        }
+
+        /// <summary>
+        /// Gets the associated requester.
+        /// </summary>
+        public IRequester Requester
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the request data that has been transmitted.
+        /// </summary>
+        public IRequest Request
+        {
+            get;
+            private set;
+        }
+    }
+}
