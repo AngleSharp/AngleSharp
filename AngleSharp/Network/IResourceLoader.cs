ba3edf43e84b5e347473b4c5d8f3254793a965c6
Florian Rappl <rappl.florian@gmail.com>
2015-04-02 19:59:55 +0200

    Specified the resource loader interface

--- AngleSharp/Network/IResourceLoader.cs
+++ AngleSharp/Network/IResourceLoader.cs
@@ -1,5 +1,8 @@
 ﻿namespace AngleSharp.Network
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Interface used to handle resource requests for a document. These
     /// requests include, but are not limited to, media, script and styling
@@ -7,5 +10,14 @@
     /// </summary>
     public interface IResourceLoader
     {
+        /// <summary>
+        /// Performs an asynchronous request that can be cancelled.
+        /// </summary>
+        /// <param name="request">The options to consider.</param>
+        /// <param name="cancel">The token for cancelling the task.</param>
+        /// <returns>
+        /// The task that will eventually give the resource's response data.
+        /// </returns>
+        Task<IResponse> LoadAsync(ResourceRequest request, CancellationToken cancel);
     }
 }
---
+++ AngleSharp/Network/ResourceRequest.cs
@@ -0,0 +1,58 @@
+﻿namespace AngleSharp.Network
+{
+    using System;
+
+    /// <summary>
+    /// Represents the arguments to load a resource.
+    /// </summary>
+    public class ResourceRequest
+    {
+        /// <summary>
+        /// Creates a new resource request for the given url.
+        /// </summary>
+        /// <param name="target">The resource's url.</param>
+        public ResourceRequest(Url target)
+        {
+            Target = target;
+            IsManualRedirectDesired = false;
+            IsSameOriginForced = false;
+            IsCookieBlocked = false;
+        }
+
+        /// <summary>
+        /// Gets the target of the request.
+        /// </summary>
+        public Url Target
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets or sets if the manual redirect flag is active.
+        /// </summary>
+        public Boolean IsManualRedirectDesired
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if the force same origin flag is active.
+        /// </summary>
+        public Boolean IsSameOriginForced
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if the block cookies flag is active.
+        /// </summary>
+        public Boolean IsCookieBlocked
+        {
+            get;
+            set;
+        }
+    }
+}
