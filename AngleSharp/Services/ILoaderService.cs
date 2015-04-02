ab810a52f7f541f2f19f28dda8a2a24bc2d020f3
Florian Rappl <rappl.florian@gmail.com>
2015-04-02 19:38:12 +0200

    Specify originators on loading

--- AngleSharp/Services/ILoaderService.cs
+++ AngleSharp/Services/ILoaderService.cs
@@ -1,5 +1,6 @@
 ﻿namespace AngleSharp.Services
 {
+    using AngleSharp.Dom;
     using AngleSharp.Network;
 
     /// <summary>
@@ -11,14 +12,20 @@
         /// Creates a loader for documents. Returning null will disable loading
         /// documents from external sources.
         /// </summary>
+        /// <param name="context">
+        /// The originator of the document requests.
+        /// </param>
         /// <returns>The new document loader.</returns>
-        IDocumentLoader CreateDocumentLoader();
+        IDocumentLoader CreateDocumentLoader(IBrowsingContext context);
 
         /// <summary>
         /// Creates a loader for resources, such as images. Returning null will
         /// disable loading resources.
         /// </summary>
+        /// <param name="document">
+        /// The originator of the resource requests.
+        /// </param>
         /// <returns>The new resource loader.</returns>
-        IResourceLoader CreateResourceLoader();
+        IResourceLoader CreateResourceLoader(IDocument document);
     }
 }
