a4830dc60301089a5566147e503b34acacf0b060
Florian Rappl <rappl.florian@gmail.com>
2015-04-05 20:08:15 +0200

    Added new events, which are invoked when the parsing starts

---
+++ AngleSharp/Events/CssParseStartEvent.cs
@@ -0,0 +1,45 @@
+﻿namespace AngleSharp.Events
+{
+    using AngleSharp.Dom.Css;
+    using AngleSharp.Parser.Css;
+    using System;
+
+    /// <summary>
+    /// The event that is published in case of starting CSS parsing.
+    /// </summary>
+    public class CssParseStartEvent
+    {
+        /// <summary>
+        /// Action called once the parsing ended.
+        /// </summary>
+        public event Action<ICssStyleSheet> Ended;
+
+        /// <summary>
+        /// Creates a new event for parsing a new CSS stylesheet.
+        /// </summary>
+        /// <param name="parser">The associated parser.</param>
+        public CssParseStartEvent(CssParser parser)
+        {
+            Parser = parser;
+        }
+
+        /// <summary>
+        /// Gets the associated parser.
+        /// </summary>
+        public CssParser Parser
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Sets the sheet by invoking the ended event.
+        /// </summary>
+        /// <param name="sheet">The sheet to propagate.</param>
+        public void SetResponse(ICssStyleSheet sheet)
+        {
+            if (Ended != null)
+                Ended(sheet);
+        }
+    }
+}
---
+++ AngleSharp/Events/HtmlParseStartEvent.cs
@@ -0,0 +1,45 @@
+﻿namespace AngleSharp.Events
+{
+    using AngleSharp.Dom;
+    using AngleSharp.Parser.Html;
+    using System;
+
+    /// <summary>
+    /// The event that is published in case of starting HTML parsing.
+    /// </summary>
+    public class HtmlParseStartEvent
+    {
+        /// <summary>
+        /// Action called once the parsing ended.
+        /// </summary>
+        public event Action<IDocument> Ended;
+
+        /// <summary>
+        /// Creates a new event for parsing a new HTML document.
+        /// </summary>
+        /// <param name="parser">The associated parser.</param>
+        public HtmlParseStartEvent(HtmlParser parser)
+        {
+            Parser = parser;
+        }
+
+        /// <summary>
+        /// Gets the associated parser.
+        /// </summary>
+        public HtmlParser Parser
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Sets the document by invoking the ended event.
+        /// </summary>
+        /// <param name="document">The document to propagate.</param>
+        public void SetResponse(IDocument document)
+        {
+            if (Ended != null)
+                Ended(document);
+        }
+    }
+}
