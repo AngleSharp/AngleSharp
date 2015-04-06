702fdcb49533228eb2c1be7a71aae4c20173cecb
Florian Rappl <rappl.florian@gmail.com>
2015-04-06 23:34:30 +0200

    Added mock for receiving selected events

---
+++ AngleSharp.Core.Tests/Mocks/EventReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using AngleSharp.Events;
+
+namespace AngleSharp.Core.Tests.Mocks
+{
+    class EventReceiver<TReceivingEvent> : IEventAggregator
+    {
+        readonly List<TReceivingEvent> _received = new List<TReceivingEvent>();
+
+        public List<TReceivingEvent> Received
+        {
+            get { return _received; }
+        }
+
+        public void Publish<TEvent>(TEvent data)
+        {
+            if (typeof(TEvent) == typeof(TReceivingEvent))
+                _received.Add((TReceivingEvent)(data as Object));
+        }
+
+        public void Subscribe<TEvent>(ISubscriber<TEvent> listener)
+        {
+            //Empty on purpose
+        }
+
+        public void Unsubscribe<TEvent>(ISubscriber<TEvent> listener)
+        {
+            //Empty on purpose
+        }
+    }
+}
