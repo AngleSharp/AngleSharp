dcf3752a90175c483002e55eb3fe095581e354f7
Florian Rappl <rappl.florian@gmail.com>
2015-04-22 00:03:38 +0200

    Updated version number

--- AngleSharp/Properties/AssemblyInfo.cs
+++ AngleSharp/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AngleSharp")]
-[assembly: AssemblyDescription("AngleSharp is the ultimate angle brackets parser library. It parses HTML5, MathML, SVG and CSS to construct a DOM based on the official W3C specification.")]
+[assembly: AssemblyDescription("AngleSharp is the ultimate angle brackets parser library. It parses HTML5, MathML, SVG, XML and CSS to construct a DOM based on the official W3C specification.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AngleVisions")]
 [assembly: AssemblyProduct("AngleSharp")]
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleToAttribute("AngleSharp.Core.Tests")]
-[assembly: AssemblyVersion("0.8.2.*")]
-[assembly: AssemblyFileVersion("0.8.2")]
+[assembly: AssemblyVersion("0.8.3.*")]
+[assembly: AssemblyFileVersion("0.8.3")]
