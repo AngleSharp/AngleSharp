d6d25da57c4634fdb5dc4409c5fca070ed5bd102
Florian Rappl <rappl.florian@gmail.com>
2015-05-23 22:30:00 +0200

    Improved type deduction

--- AngleSharp/DOM/Css/CssValue.cs
+++ AngleSharp/DOM/Css/CssValue.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
     using AngleSharp.Extensions;
     using AngleSharp.Parser.Css;
 
@@ -17,7 +16,10 @@
         readonly CssValueType _type;
         readonly List<CssToken> _tokens;
 
-        public static CssValue Initial = new CssValue(new []{ new CssKeywordToken(CssTokenType.Ident, "initial", TextPosition.Empty) });
+        public static CssValue Initial = new CssValue(new []
+        { 
+            new CssKeywordToken(CssTokenType.Ident, "initial", TextPosition.Empty) 
+        });
 
         #endregion
 
@@ -30,17 +32,42 @@
         public CssValue(IEnumerable<CssToken> tokens)
         {
             _tokens = new List<CssToken>(tokens);
+            _type = FindType(_tokens);
+        }
 
-            if (_tokens.Count == 1 && _tokens[0].Data.Equals(Keywords.Initial, StringComparison.OrdinalIgnoreCase))
-                _type = CssValueType.Initial;
-            else if (_tokens.Count == 1 && _tokens[0].Data.Equals(Keywords.Inherit, StringComparison.OrdinalIgnoreCase))
-                _type = CssValueType.Inherit;
-            else if (_tokens.Any(m => m.Type == CssTokenType.Whitespace))
-                _type = CssValueType.List;
-            else if (_tokens.Count != 0)
-                _type = CssValueType.Primitive;
-            else
-                _type = CssValueType.Custom;
+        static CssValueType FindType(List<CssToken> tokens)
+        {
+            var type = CssValueType.Custom;
+            var open = 0;
+
+            if (tokens.Count == 1)
+            {
+                if (tokens[0].Data.Equals(Keywords.Initial, StringComparison.OrdinalIgnoreCase))
+                    return CssValueType.Initial;
+                else if (tokens[0].Data.Equals(Keywords.Inherit, StringComparison.OrdinalIgnoreCase))
+                    return CssValueType.Inherit;
+            }
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (type == CssValueType.Custom)
+                    type = CssValueType.Primitive;
+
+                if (open > 0 && tokens[i].Type == CssTokenType.RoundBracketClose)
+                    open--;
+
+                if (tokens[i].Type == CssTokenType.Function || tokens[i].Type == CssTokenType.RoundBracketOpen)
+                    open++;
+
+                if (open > 0)
+                    continue;
+
+                if (tokens[i].Type == CssTokenType.Whitespace || tokens[i].Type == CssTokenType.Comma ||
+                    (tokens[i].Type == CssTokenType.Delim && tokens[i].Data == "/"))
+                    type = CssValueType.List;
+            }
+
+            return type;
         }
 
         #endregion
