4bdc3b9ed1a7094b952b1663e6ac7f58171a9983
Florian Rappl <rappl.florian@gmail.com>
2015-06-01 19:28:11 +0200

    Added documentation

--- AngleSharp/Parser/Css/CssParserExtensions.cs
+++ AngleSharp/Parser/Css/CssParserExtensions.cs
@@ -25,18 +25,43 @@
             return (Int32)code;
         }
 
+        /// <summary>
+        /// Checks if the provided token is either of the first or the second 
+        /// type of token.
+        /// </summary>
+        /// <param name="token">The token to examine.</param>
+        /// <param name="a">The first type to match.</param>
+        /// <param name="b">The alternative match for the token.</param>
+        /// <returns>Result of the examination.</returns>
         public static Boolean Is(this CssToken token, CssTokenType a, CssTokenType b)
         {
             var type = token.Type;
             return type == a || type == b;
         }
 
+        /// <summary>
+        /// Checks if the provided token is neither of the first nor the second 
+        /// type of token.
+        /// </summary>
+        /// <param name="token">The token to examine.</param>
+        /// <param name="a">The first type to unmatch.</param>
+        /// <param name="b">The alternative unmatch for the token.</param>
+        /// <returns>Result of the examination.</returns>
         public static Boolean IsNot(this CssToken token, CssTokenType a, CssTokenType b)
         {
             var type = token.Type;
             return type != a && type != b;
         }
 
+        /// <summary>
+        /// Checks if the provided token is neither of the first, nor the
+        /// second nor the third type of token.
+        /// </summary>
+        /// <param name="token">The token to examine.</param>
+        /// <param name="a">The first type to unmatch.</param>
+        /// <param name="b">The alternative unmatch for the token.</param>
+        /// <param name="c">The final unmatch for the token.</param>
+        /// <returns>Result of the examination.</returns>
         public static Boolean IsNot(this CssToken token, CssTokenType a, CssTokenType b, CssTokenType c)
         {
             var type = token.Type;
@@ -60,8 +85,10 @@
         }
 
         /// <summary>
-        /// Called before a document function has been found.
+        /// Tries to create an IDocumentFunction from the provided token.
         /// </summary>
+        /// <param name="token">The token to examine.</param>
+        /// <returns>The created IDocumentFunction or null.</returns>
         public static IDocumentFunction ToDocumentFunction(this CssToken token)
         {
             switch (token.Type)
