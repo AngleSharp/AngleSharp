e5b20645d223b6ae2392d5d10a9853e1eb299f91
Florian Rappl <rappl.florian@gmail.com>
2015-06-02 22:57:51 +0200

    Added mock to delay reponse

---
+++ AngleSharp.Core.Tests/Mocks/DelayRequester.cs
@@ -0,0 +1,36 @@
+﻿namespace AngleSharp.Core.Tests.Mocks
+{
+    using AngleSharp.Network;
+    using AngleSharp.Network.Default;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    sealed class DelayRequester : IRequester
+    {
+        readonly Int32 _timeout;
+
+        public DelayRequester(Int32 timeout)
+        {
+            _timeout = timeout;
+        }
+
+        public Boolean SupportsProtocol(String protocol)
+        {
+            return true;
+        }
+
+        public async Task<IResponse> RequestAsync(IRequest request, CancellationToken cancel)
+        {
+            await Task.Delay(_timeout, cancel);
+            return new Response
+            {
+                Address  = request.Address,
+                Content = MemoryStream.Null,
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
