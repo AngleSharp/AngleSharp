4983cd3de1a5af68f50b5a6bf908b7d3b15bb77b
Florian Rappl <rappl.florian@gmail.com>
2015-06-13 00:01:12 +0200

    Added the any converter

---
+++ AngleSharp/Css/ValueConverters/AnyValueConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace AngleSharp.Css.ValueConverters
+{
+    using System;
+    using System.Collections.Generic;
+    using AngleSharp.Extensions;
+    using AngleSharp.Parser.Css;
+
+    sealed class AnyValueConverter : IValueConverter
+    {
+        public IPropertyValue Convert(IEnumerable<CssToken> value)
+        {
+            return new AnyValue(value);
+        }
+
+        sealed class AnyValue : IPropertyValue
+        {
+            readonly IEnumerable<CssToken> _value;
+
+            public AnyValue(IEnumerable<CssToken> value)
+            {
+                _value = value;
+            }
+
+            public String CssText
+            {
+                get { return _value.ToText(); }
+            }
+        }
+    }
+}
