796e4618e62c209da92a32dabb9fcabf8ba4756e
Florian Rappl <rappl.florian@gmail.com>
2015-06-14 17:10:15 +0200

    Do not create longhand properties

--- AngleSharp/Dom/Css/Properties/CssShorthandProperty.cs
+++ AngleSharp/Dom/Css/Properties/CssShorthandProperty.cs
@@ -1,35 +1,18 @@
 ﻿namespace AngleSharp.Dom.Css
 {
-    using System;
-    using System.Linq;
     using AngleSharp.Css;
+    using System;
 
     /// <summary>
     /// Base class for all shorthand properties
     /// </summary>
     abstract class CssShorthandProperty : CssProperty
     {
-        #region Fields
-
-        readonly CssProperty[] _properties;
-
-        #endregion
-
         #region ctor
 
         public CssShorthandProperty(String name, PropertyFlags flags = PropertyFlags.None)
             : base(name, flags | PropertyFlags.Shorthand)
         {
-            _properties = Factory.Properties.CreateLonghandsFor(name).ToArray();
-        }
-
-        #endregion
-
-        #region Properties
-
-        public CssProperty[] Properties
-        {
-            get { return _properties; }
         }
 
         #endregion
