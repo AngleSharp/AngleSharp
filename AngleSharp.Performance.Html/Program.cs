7eed6c1e5871fa5b55a1bf336f4e8c654d77717d
Florian Rappl <rappl.florian@gmail.com>
2015-06-18 00:07:47 +0200

    Use outsourced UrlTests collection

--- AngleSharp.Performance.Html/Program.cs
+++ AngleSharp.Performance.Html/Program.cs
@@ -7,61 +7,61 @@
     {
         static void Main(String[] args)
         {
-            UrlTest.UseBuffer = true;
+            var websites = new UrlTests(
+                extension: ".html", 
+                withBuffer: true);
 
-            var tests = new List<ITest>
-            {
-                UrlTest.For("http://www.amazon.com").Result,
-                UrlTest.For("http://www.blogspot.com").Result,
-                UrlTest.For("http://www.smashing.com").Result,
-                UrlTest.For("http://www.youtube.com").Result,
-                UrlTest.For("http://www.weibo.com").Result,
-                UrlTest.For("http://en.wikipedia.org").Result,
-                UrlTest.For("http://www.w3.org").Result,
-                UrlTest.For("http://www.yahoo.com").Result,
-                UrlTest.For("http://www.google.com").Result,
-                UrlTest.For("http://www.linkedin.com").Result,
-                UrlTest.For("http://www.pinterest.com").Result,
-                UrlTest.For("http://news.google.com").Result,
-                UrlTest.For("http://www.baidu.com").Result,
-                UrlTest.For("http://www.codeproject.com").Result,
-                UrlTest.For("http://www.ebay.com").Result,
-                UrlTest.For("http://www.msn.com").Result,
-                UrlTest.For("http://www.nbc.com").Result,
-                UrlTest.For("http://www.qq.com").Result,
-                UrlTest.For("http://www.florian-rappl.de").Result,
-                UrlTest.For("http://www.stackoverflow.com").Result,
-                UrlTest.For("http://www.html5rocks.com/en").Result,
-                UrlTest.For("http://www.live.com").Result,
-                UrlTest.For("http://www.taobao.com").Result,
-                UrlTest.For("http://www.huffingtonpost.com").Result,
-                UrlTest.For("http://www.wordpress.org").Result,
-                UrlTest.For("http://www.myspace.com").Result,
-                UrlTest.For("http://www.flickr.com").Result,
-                UrlTest.For("http://www.godaddy.com").Result,
-                UrlTest.For("http://www.reddit.com").Result,
-                UrlTest.For("http://www.nytimes.com").Result,
-                UrlTest.For("http://peacekeeper.futuremark.com").Result,
-                UrlTest.For("http://www.pcmag.com").Result,
-                UrlTest.For("http://www.sitepoint.com").Result,
-                UrlTest.For("http://html5test.com").Result,
-                UrlTest.For("http://www.spiegel.de").Result,
-                UrlTest.For("http://www.tmall.com").Result,
-                UrlTest.For("http://www.sohu.com").Result,
-                UrlTest.For("http://www.vk.com").Result,
-                UrlTest.For("http://www.wordpress.com").Result,
-                UrlTest.For("http://www.bing.com").Result,
-                UrlTest.For("http://www.tumblr.com").Result,
-                UrlTest.For("http://www.ask.com").Result,
-                UrlTest.For("http://www.mail.ru").Result,
-                UrlTest.For("http://www.imdb.com").Result,
-                UrlTest.For("http://www.kickass.to").Result,
-                UrlTest.For("http://www.360.cn").Result,
-                UrlTest.For("http://www.163.com").Result,
-                UrlTest.For("http://www.neobux.com").Result,
-                UrlTest.For("http://www.aliexpress.com").Result,
-                UrlTest.For("http://www.netflix.com").Result
-            };
+            websites.Include(
+                "http://www.amazon.com",
+                "http://www.blogspot.com",
+                "http://www.smashing.com",
+                "http://www.youtube.com",
+                "http://www.weibo.com",
+                "http://en.wikipedia.org",
+                "http://www.w3.org",
+                "http://www.yahoo.com",
+                "http://www.google.com",
+                "http://www.linkedin.com",
+                "http://www.pinterest.com",
+                "http://news.google.com",
+                "http://www.baidu.com",
+                "http://www.codeproject.com",
+                "http://www.ebay.com",
+                "http://www.msn.com",
+                "http://www.nbc.com",
+                "http://www.qq.com",
+                "http://www.florian-rappl.de",
+                "http://www.stackoverflow.com",
+                "http://www.html5rocks.com/en",
+                "http://www.live.com",
+                "http://www.taobao.com",
+                "http://www.huffingtonpost.com",
+                "http://www.wordpress.org",
+                "http://www.myspace.com",
+                "http://www.flickr.com",
+                "http://www.godaddy.com",
+                "http://www.reddit.com",
+                "http://www.nytimes.com",
+                "http://peacekeeper.futuremark.com",
+                "http://www.pcmag.com",
+                "http://www.sitepoint.com",
+                "http://html5test.com",
+                "http://www.spiegel.de",
+                "http://www.tmall.com",
+                "http://www.sohu.com",
+                "http://www.vk.com",
+                "http://www.wordpress.com",
+                "http://www.bing.com",
+                "http://www.tumblr.com",
+                "http://www.ask.com",
+                "http://www.mail.ru",
+                "http://www.imdb.com",
+                "http://www.kickass.to",
+                "http://www.360.cn",
+                "http://www.163.com",
+                "http://www.neobux.com",
+                "http://www.aliexpress.com",
+                "http://www.netflix.com").Wait();
 
             var statistics = new StatisticParser();
 
@@ -73,12 +73,7 @@
                 new AgilityPackParser()
             };
 
-            //Majestic is neither HTML5 conform, nor building a realistic DOM structure.
-            //Therefore Majestic has been excluded. You could, however, just re-enable
-            //it by uncommenting the following line.
-            //parsers.Add(new MajesticParser());
-
-            var testsuite = new TestSuite(parsers, tests, new Output(), new Warmup())
+            var testsuite = new TestSuite(parsers, websites.Tests, new Output(), new Warmup())
             {
                 NumberOfRepeats = 5,
                 NumberOfReRuns = 1
