e091b359ece1dc2092048c8ef3301789863f2040
Florian Rappl <rappl.florian@gmail.com>
2015-06-18 00:15:12 +0200

    Added standardized tests

---
+++ AngleSharp.Performance.Utilities/StandardTest.cs
@@ -0,0 +1,30 @@
+﻿namespace AngleSharp.Performance
+{
+    using System;
+
+    sealed class StandardTest : ITest
+    {
+        public StandardTest(String value)
+            : this(value, value)
+        {
+        }
+
+        public StandardTest(String name, String source)
+        {
+            Name = name;
+            Source = source;
+        }
+
+        public String Name
+        {
+            get;
+            private set;
+        }
+
+        public String Source
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ AngleSharp.Performance.Utilities/StandardTests.cs
@@ -0,0 +1,30 @@
+﻿namespace AngleSharp.Performance
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class StandardTests
+    {
+        readonly List<ITest> _tests;
+
+        public StandardTests()
+        {
+            _tests = new List<ITest>();
+        }
+
+        public List<ITest> Tests
+        {
+            get { return _tests; }
+        }
+
+        public StandardTests Include(params String[] values)
+        {
+            foreach (var value in values)
+            {
+                _tests.Add(new StandardTest(value));
+            }
+
+            return this;
+        }
+    }
+}
