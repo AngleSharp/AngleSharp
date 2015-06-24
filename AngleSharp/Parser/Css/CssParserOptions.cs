8cc2c19dfb588d96363e2506d2ccc38e2f595448
Florian Rappl <rappl.florian@gmail.com>
2015-06-24 07:31:25 -0400

    Added options structures for the parsers

---
+++ AngleSharp/Parser/Css/CssParserOptions.cs
@@ -0,0 +1,37 @@
+﻿namespace AngleSharp.Parser.Css
+{
+    using System;
+
+    /// <summary>
+    /// Contains a number of options for the CSS parser.
+    /// </summary>
+    public struct CssParserOptions
+    {
+        /// <summary>
+        /// Gets or sets if unknown (@-) rules are dropped.
+        /// </summary>
+        public Boolean IsDroppingUnknownRules
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if unknown declarations are dropped.
+        /// </summary>
+        public Boolean IsDroppingUnknownDeclarations
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if invalid values for declarations should be ignored.
+        /// </summary>
+        public Boolean IsIgnoringInvalidValues
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ AngleSharp/Parser/Html/HtmlParserOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace AngleSharp.Parser.Html
+{
+    using System;
+
+    /// <summary>
+    /// Contains a number of options for the HTML parser.
+    /// </summary>
+    public struct HtmlParserOptions
+    {
+        /// <summary>
+        /// Gets or sets if the document is embedded.
+        /// </summary>
+        public Boolean IsEmbedded
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets if scripting is allowed.
+        /// </summary>
+        public Boolean IsScripting
+        {
+            get;
+            set;
+        }
+    }
+}
