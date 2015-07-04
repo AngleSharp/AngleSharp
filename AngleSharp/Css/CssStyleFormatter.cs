2c1ef73a610b6ba1bd18f38274009567bd8e4f25
Florian Rappl <rappl.florian@gmail.com>
2015-07-04 17:43:07 +0200

    Defined basic interface and provide standard implementation

---
+++ AngleSharp/Css/CssStyleFormatter.cs
@@ -0,0 +1,68 @@
+﻿namespace AngleSharp.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents the standard CSS3 style formatter.
+    /// </summary>
+    public sealed class CssStyleFormatter : IStyleFormatter
+    {
+        #region Instance
+
+        /// <summary>
+        /// An instance of the CssStyleFormatter.
+        /// </summary>
+        public static readonly IStyleFormatter Instance = new CssStyleFormatter();
+
+        #endregion
+
+        #region Methods
+
+        String IStyleFormatter.Sheet(String[] rules)
+        {
+            var sb = Pool.NewStringBuilder();
+
+            if (rules.Length > 0)
+            {
+                sb.Append(rules[0]);
+
+                for (int i = 1; i < rules.Length; i++)
+                {
+                    sb.AppendLine().Append(rules[i]);
+                }
+            }
+
+            return sb.ToPool();
+        }
+        String IStyleFormatter.Declaration(String name, String value, Boolean important)
+        {
+            var rest = String.Concat(value, important ? " !important" : String.Empty);
+            return String.Concat(name, ": ", rest, ";");
+        }
+
+        String IStyleFormatter.Medium(Boolean exclusive, Boolean inverse, String type, String[] constraints)
+        {
+            var prefix = exclusive ? "only " : (inverse ? "not " : String.Empty);
+
+            if (constraints.Length != 0)
+            {
+                var constraint = String.Join(" and ", constraints);
+
+                if (String.IsNullOrEmpty(type))
+                    return String.Concat(prefix, constraint);
+
+                return String.Concat(prefix, type, " and ", constraint);
+            }
+
+            return String.Concat(prefix, type ?? String.Empty);
+        }
+
+        string IStyleFormatter.Constraint(String name, String value)
+        {
+            var ending = value != null ? ": " + value : String.Empty;
+            return String.Concat("(", name, ending, ")");
+        }
+
+        #endregion
+    }
+}
--- AngleSharp/IStyleFormatter.cs
+++ AngleSharp/IStyleFormatter.cs
@@ -1,9 +1,46 @@
 ﻿namespace AngleSharp
 {
+    using System;
+
     /// <summary>
     /// Basic interface for CSS node serialization.
     /// </summary>
     public interface IStyleFormatter
     {
+        /// <summary>
+        /// Concats the given rules to create the stylesheet serialization.
+        /// </summary>
+        /// <param name="rules">The rules to aggregate.</param>
+        /// <returns>The serialization of the sheet.</returns>
+        String Sheet(String[] rules);
+
+        /// <summary>
+        /// Creates the serialization of a declaration with the given name,
+        /// value and important flag.
+        /// </summary>
+        /// <param name="name">The name of the declaration.</param>
+        /// <param name="value">The value of the declaration.</param>
+        /// <param name="important">The value of the important flag.</param>
+        /// <returns>The serialization of the declaration.</returns>
+        String Declaration(String name, String value, Boolean important);
+
+        /// <summary>
+        /// Serializes a CSS medium with the provided properties.
+        /// </summary>
+        /// <param name="exclusive">Is the medium exclusive (only)?</param>
+        /// <param name="inverse">Is the medium inverse (not)?</param>
+        /// <param name="type">The type of the medium.</param>
+        /// <param name="constraints">The constraints to use.</param>
+        /// <returns>The serialization of the medium.</returns>
+        String Medium(Boolean exclusive, Boolean inverse, String type, String[] constraints);
+
+        /// <summary>
+        /// Creates the serialization of the constraint with the provided name
+        /// and value, if any.
+        /// </summary>
+        /// <param name="name">The name of the constraint.</param>
+        /// <param name="value">The optional value of the constraint.</param>
+        /// <returns>The serialization of the constraint.</returns>
+        String Constraint(String name, String value);
     }
 }
