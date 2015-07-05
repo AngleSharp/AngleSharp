b39f701ee629f79c729563c85c8e1327257a453d
Florian Rappl <rappl.florian@gmail.com>
2015-07-05 15:37:53 +0200

    Added a few more test sheets

--- AngleSharp.Performance.Css/Program.cs
+++ AngleSharp.Performance.Css/Program.cs
@@ -20,7 +20,10 @@
                 "http://assets-cdn.github.com/assets/github2/index-0d569068eabc0305b93b246950eb913fc9d2948b952824895172164835b351dd.css",
                 "http://codeproject.cachefly.net/App_Themes/CodeProject/Css/Main.min.css?dt=2.8.150616.1",
                 "http://cdn.sstatic.net/stackoverflow/all.css?v=5a386bc7d85d",
-                "http://fbstatic-a.akamaihd.net/rsrc.php/v2/yX/r/fr81HMP3WaY.css").Wait();
+                "http://fbstatic-a.akamaihd.net/rsrc.php/v2/yX/r/fr81HMP3WaY.css",
+                "https://s.yimg.com/zz/combo?/os/stencil/3.1.0/styles-ltr.css&/os/fp/atomic-css.2c9008c2.css",
+                "http://static.licdn.com/sc/h/59ugqor0mlj9qjt7ohmi1kpko,cy00fqioblh011agl8l3n36sc",
+                "http://style.aliunicorn.com/css/6v/??apollo/core/core-ws-responsive.css,run/wholesale/font-v1.css,run/site/en/common/header/header-v150213.css,run/site/en/common/bottombar/bottom-bar.css?t=145f12b3e_104c763f9a").Wait();
 
             var parsers = new List<ITestee>
             {
