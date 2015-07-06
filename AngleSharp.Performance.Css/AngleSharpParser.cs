dd233c2df927e6f6285b989700372255e763c055
Florian Rappl <rappl.florian@gmail.com>
2015-07-06 21:04:57 +0200

    Compare equal functionality by disabling validation

--- AngleSharp.Performance.Css/AngleSharpParser.cs
+++ AngleSharp.Performance.Css/AngleSharpParser.cs
@@ -1,8 +1,8 @@
 ﻿namespace AngleSharp.Performance.Css
 {
-    using System;
     using AngleSharp;
     using AngleSharp.Parser.Css;
+    using System;
 
     class AngleSharpParser : ITestee
     {
@@ -21,7 +21,13 @@
         public void Run(String source)
         {
             var parser = new CssParser(source, configuration);
-            parser.Parse();
+            parser.Parse(new CssParserOptions
+            {
+                IsIncludingUnknownDeclarations = true,
+                IsIncludingUnknownRules = true,
+                IsToleratingInvalidConstraints = true,
+                IsToleratingInvalidValues = true
+            });
         }
     }
 }
