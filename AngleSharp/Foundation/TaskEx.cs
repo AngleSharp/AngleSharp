92e7620097ea336a1cdd80a3b9573afe1229b6e2
Florian Rappl <rappl.florian@gmail.com>
2015-07-15 20:59:08 +0200

    Introduce TaskEx wrapper to simplify .NET 4 / SL compat

---
+++ AngleSharp/Foundation/TaskEx.cs
@@ -0,0 +1,29 @@
+﻿namespace AngleSharp
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Simple wrapper for static methods of Task, which are missing in older
+    /// versions of the .NET-Framework.
+    /// </summary>
+    static class TaskEx
+    {
+        /// <summary>
+        /// Wrapper for Task.WhenAll, but also works with .NET 4 and SL due to
+        /// same naming as TaskEx in BCL.Async.
+        /// </summary>
+        public static Task WhenAll(params Task[] tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Wrapper for Task.FromResult, but also works with .NET 4 and SL due
+        /// to same naming as TaskEx in BCL.Async.
+        /// </summary>
+        public static Task<TResult> FromResult<TResult>(TResult result)
+        {
+            return Task.FromResult(result);
+        }
+    }
+}
