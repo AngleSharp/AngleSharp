846d243993d37182338fe55dd08ea8085f6e53ea
Florian Rappl <rappl.florian@gmail.com>
2015-07-15 14:50:47 +0200

    Use

--- AngleSharp.Core.Tests/TestExtensions.cs
+++ AngleSharp.Core.Tests/TestExtensions.cs
@@ -2,8 +2,6 @@
 {
     using AngleSharp.Dom;
     using AngleSharp.Dom.Css;
-    using AngleSharp.Dom.Html;
-    using AngleSharp.Extensions;
     using AngleSharp.Parser.Css;
     using AngleSharp.Parser.Html;
     using AngleSharp.Parser.Xml;
@@ -44,25 +42,8 @@
 
         public static INodeList ToHtmlFragment(this String sourceCode, IElement context = null, IConfiguration configuration = null)
         {
-            var ctx = BrowsingContext.New(configuration);
-            var source = new TextSource(sourceCode);
-            var document = new HtmlDocument(ctx, source);
-            var parser = new HtmlDomBuilder(sourceCode, configuration);
-            var element = context as Element;
-
-            if (element != null)
-            {
-                var options = new HtmlParserOptions
-                {
-                    IsEmbedded = false,
-                    IsScripting = configuration.IsScripting()
-                };
-                return parser.ParseFragment(options, element).DocumentElement.ChildNodes;
-            }
-            else
-            {
-                return parser.Parse(default(HtmlParserOptions)).ChildNodes;
-            }
+            var parser = new HtmlParser(configuration);
+            return parser.ParseFragment(sourceCode, context);
         }
 
         public static IDocument ToHtmlDocument(this Stream content, IConfiguration configuration = null)
