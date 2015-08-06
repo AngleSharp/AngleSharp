35c790d2b1a96107350758b5b725dd1986a82738
Florian Rappl <rappl.florian@gmail.com>
2015-08-07 00:04:40 +0200

    Added basic conversion tests

---
+++ AngleSharp.Core.Tests/Library/UnitConversion.cs
@@ -0,0 +1,41 @@
+﻿namespace AngleSharp.Core.Tests.Library
+{
+    using AngleSharp.Css.Values;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnitConversionTests
+    {
+        [Test]
+        public void LengthParseCorrectPxValue()
+        {
+            var s = "12px";
+            var v = default(Length);
+            var r = Length.TryParse(s, out v);
+            Assert.IsTrue(r);
+            Assert.AreEqual(12f, v.Value);
+            Assert.AreEqual(Length.Unit.Px, v.Type);
+        }
+
+        [Test]
+        public void LengthParseIncorrectValue()
+        {
+            var s = "123.5";
+            var v = default(Length);
+            var r = Length.TryParse(s, out v);
+            Assert.IsFalse(r);
+        }
+
+        [Test]
+        public void LengthParseCorrectVwValue()
+        {
+            var s = "12.2vw";
+            var v = default(Length);
+            var r = Length.TryParse(s, out v);
+            Assert.IsTrue(r);
+            Assert.AreEqual(12.2f, v.Value);
+            Assert.AreEqual(Length.Unit.Vw, v.Type);
+        }
+
+    }
+}
