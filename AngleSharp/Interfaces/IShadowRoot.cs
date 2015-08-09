980061579f0408fc6e43ad0c5f80d61fd6c6fd09
Florian Rappl <rappl.florian@gmail.com>
2015-08-09 16:47:45 +0200

    Added the shadow root interface #98

---
+++ AngleSharp/Interfaces/IShadowRoot.cs
@@ -0,0 +1,36 @@
+﻿namespace AngleSharp.Dom
+{
+    using AngleSharp.Attributes;
+    using System;
+
+    /// <summary>
+    /// The ShadowRoot interface represents the shadow root.
+    /// </summary>
+    [DomName("ShadowRoot")]
+    public interface IShadowRoot : IDocumentFragment
+    {
+        /// <summary>
+        /// Gets the currently focused element in the shadow tree, if any.
+        /// </summary>
+        [DomName("activeElement")]
+        IElement ActiveElement { get; }
+
+        /// <summary>
+        /// Gets the host element, which contains this shadow root.
+        /// </summary>
+        [DomName("host")]
+        IElement Host { get; }
+
+        /// <summary>
+        /// Gets the markup of the current shadow root's contents.
+        /// </summary>
+        [DomName("innerHTML")]
+        String InnerHtml { get; set; }
+
+        /// <summary>
+        /// Gets the shadow root style sheets.
+        /// </summary>
+        [DomName("styleSheets")]
+        IStyleSheetList StyleSheets { get; }
+    }
+}
