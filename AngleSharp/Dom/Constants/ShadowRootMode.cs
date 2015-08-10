a8a2171010b62993cb6e985b3602c004080cb05f
Florian Rappl <rappl.florian@gmail.com>
2015-08-10 17:27:11 +0200

    Added shadow root enumeration

---
+++ AngleSharp/Dom/Constants/ShadowRootMode.cs
@@ -0,0 +1,23 @@
+﻿namespace AngleSharp.Dom
+{
+    using AngleSharp.Attributes;
+
+    /// <summary>
+    /// List of possible shadow root mode states.
+    /// </summary>
+    [DomName("ShadowRootMode")]
+    [DomLiterals]
+    public enum ShadowRootMode
+    {
+        /// <summary>
+        /// Specifies open encapsulation mode.
+        /// </summary>
+        [DomName("open")]
+        Open = 0,
+        /// <summary>
+        /// Specifies closed encapsulation mode.
+        /// </summary>
+        [DomName("closed")]
+        Closed = 1
+    }
+}
