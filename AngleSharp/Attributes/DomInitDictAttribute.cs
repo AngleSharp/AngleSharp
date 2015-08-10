49e58b6c44aa34d7f831cdebdd36cfd23dbb62f5
Florian Rappl <rappl.florian@gmail.com>
2015-08-10 17:26:52 +0200

    Added additional DOM attributes

---
+++ AngleSharp/Attributes/DomInitDictAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace AngleSharp.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// This attribute decorates official DOM methods as specified by the W3C.
+    /// It tells scripting engines that bags with objects should be provided,
+    /// which have to be expanded to be used as arguments.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class DomInitDictAttribute : Attribute
+    {
+    }
+}
---
+++ AngleSharp/Attributes/DomLiteralsAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace AngleSharp.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// This attribute is used to mark an enum as being just a collection of
+    /// constant string values (with the names being the strings).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum)]
+    public sealed class DomLiteralsAttribute : Attribute
+    {
+    }
+}
