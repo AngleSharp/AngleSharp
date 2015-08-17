c641950ecd933a85745fbd424781904f5f3c6539
Florian Rappl <rappl.florian@gmail.com>
2015-08-18 00:31:45 +0200

    Added comment node #38

---
+++ AngleSharp/Css/CssComment.cs
@@ -0,0 +1,30 @@
+﻿namespace AngleSharp.Css
+{
+    using System;
+
+    /// <summary>
+    /// Represents a comment in the CSS AST.
+    /// </summary>
+    public class CssComment : CssNode
+    {
+        /// <summary>
+        /// Creates a new comment for the CSS AST.
+        /// </summary>
+        /// <param name="content">The contained content.</param>
+        /// <param name="range">The covered range in the source.</param>
+        public CssComment(String content, TextRange range)
+            : base(range)
+        {
+            Content = content;
+        }
+
+        /// <summary>
+        /// Gets the associated trivia content.
+        /// </summary>
+        public String Content
+        {
+            get;
+            private set;
+        }
+    }
+}
