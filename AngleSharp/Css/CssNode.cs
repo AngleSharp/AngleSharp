075a1fb5752ebda3c305b562bf354b9b681013bd
Florian Rappl <rappl.florian@gmail.com>
2015-08-17 23:59:00 +0200

    Added documentation #38

--- AngleSharp/Css/CssNode.cs
+++ AngleSharp/Css/CssNode.cs
@@ -1,12 +1,22 @@
 ﻿namespace AngleSharp.Css
 {
+    /// <summary>
+    /// Represents a single node in the CSS AST.
+    /// </summary>
     public class CssNode
     {
+        /// <summary>
+        /// Creates a new Node for the CSS AST.
+        /// </summary>
+        /// <param name="range">The covered range in the source.</param>
         public CssNode(TextRange range)
         {
             Range = range;
         }
 
+        /// <summary>
+        /// Gets the covered range within the source file.
+        /// </summary>
         public TextRange Range
         {
             get;
--- AngleSharp/Css/CssParseTree.cs
+++ AngleSharp/Css/CssParseTree.cs
@@ -3,16 +3,27 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    /// <summary>
+    /// Represents the ParseTree for a CSS file.
+    /// </summary>
     public class CssParseTree : CssNode
     {
         readonly List<CssNode> _nodes;
 
+        /// <summary>
+        /// Creates a new ParseTree.
+        /// </summary>
+        /// <param name="range">The covered range in the source.</param>
+        /// <param name="nodes">Nodes to be included.</param>
         public CssParseTree(TextRange range, IEnumerable<CssNode> nodes)
             : base(range)
         {
             _nodes = new List<CssNode>(nodes);
         }
 
+        /// <summary>
+        /// Gets the contained nodes.
+        /// </summary>
         public IEnumerable<CssNode> Nodes
         {
             get { return _nodes.AsEnumerable(); }
