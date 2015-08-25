3a31c68a96e511352fad0abd23745632f3abab23
Florian Rappl <rappl.florian@gmail.com>
2015-08-25 10:06:26 +0200

    Convinience methods for CssNode #38

--- AngleSharp/Extensions/StyleSheetExtensions.cs
+++ AngleSharp/Extensions/StyleSheetExtensions.cs
@@ -44,31 +44,60 @@
         }
 
         /// <summary>
-        /// Gets the comments contained in the stylesheet, if any.
+        /// Gets the comments contained in the CSS node, if any.
         /// </summary>
-        /// <param name="sheet">The stylesheet to examine.</param>
+        /// <param name="node">The node to examine.</param>
         /// <returns>An iterator over all comments.</returns>
-        public static IEnumerable<CssComment> GetComments(this ICssStyleSheet sheet)
+        public static IEnumerable<CssComment> GetComments(this CssNode node)
         {
-            var root = sheet as CssNode;
-
-            if (root != null)
-                return root.GetComments();
-
-            return Enumerable.Empty<CssComment>();
+            var tokens = node.Tokens;
+            var comments = tokens.Where(m => m.Type == CssTokenType.Comment).Select(m => new CssComment(m));
+            return comments.Concat(node.Children.SelectMany(m => m.GetComments()));
         }
 
-        static IEnumerable<CssComment> GetComments(this CssNode node)
+        /// <summary>
+        /// Gets the original source code of the CSS node.
+        /// </summary>
+        /// <param name="node">The node to examine.</param>
+        /// <returns>The original text representation.</returns>
+        public static String GetSource(this CssNode node)
         {
-            var tokens = node.Trivia;
-            var comments = Enumerable.Empty<CssComment>();
+            var tokens = node.Tokens;
+            var childs = node.Children;
+            var total = tokens.Count + childs.Count;
+            var str = new String[total];
+            var pos = new TextPosition[total];
 
-            if (tokens != null)
+            for (int i = 0; i < tokens.Count; i++)
             {
-                comments = tokens.Where(m => m.Type == CssTokenType.Comment).Select(m => new CssComment(m.Data, m.Position));
+                str[i] = tokens[i].ToValue();
+                pos[i] = tokens[i].Position;
             }
 
-            return comments.Concat(node.GetChildren().SelectMany(m => m.GetComments()));
+            for (int i = tokens.Count, k = 0; i < total; i++, k++)
+            {
+                str[i] = childs[k].GetSource();
+                pos[i] = childs[k].Start;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (pos[j] < pos[j + 1])
+                        break;
+
+                    Swap(pos, j);
+                    Swap(str, j);
+                }
+            }
+
+            return String.Join(String.Empty, str);
+        }
+
+        static void Swap<T>(T[] array, Int32 i)
+        {
+            var j = i + 1;
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
