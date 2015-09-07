545dd57cdbe1cc6141494416b6dd12f4ee01ea30
Florian Rappl <rappl.florian@gmail.com>
2015-09-07 18:41:15 +0200

    Added Exposed attribute

---
+++ AngleSharp/Attributes/DomExposedAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharp.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// This attribute is used to mark a constructor as being
+    /// accessible from scripts.
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct, 
+        AllowMultiple = true, Inherited = false)]
+    public sealed class DomExposedAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new DomExposedAttribute.
+        /// </summary>
+        /// <param name="target">
+        /// The official name of the target interface.
+        /// </param>
+        public DomExposedAttribute(String target)
+        {
+            Target = target;
+        }
+
+        /// <summary>
+        /// Gets the official name of the target interface.
+        /// </summary>
+        public String Target
+        {
+            get;
+            private set;
+        }
+    }
+}
