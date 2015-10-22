83847904550b7843b4060fc263521e7dbc22030c
Florian Rappl <rappl.florian@gmail.com>
2015-10-22 23:44:46 +0200

    Test for loading import from data url

--- AngleSharp.Core.Tests/Library/PageImport.cs
+++ AngleSharp.Core.Tests/Library/PageImport.cs
@@ -26,5 +26,28 @@
             Assert.IsNotNull(link.Import);
             Assert.AreEqual("http://example.com/test.html", result);
         }
+
+        [Test]
+        public async Task ImportPageFromDataRequest()
+        {
+            var receivedRequest = new TaskCompletionSource<Boolean>();
+            var config = Configuration.Default.WithDefaultLoader(setup =>
+            {
+                setup.IsResourceLoadingEnabled = true;
+                setup.Filter = request =>
+                {
+                    receivedRequest.SetResult(true);
+                    return true;
+                };
+            });
+
+            var document = await BrowsingContext.New(config).OpenAsync(m => m.Content("<!doctype html><link rel=import href='data:text/html,<div>foo</div>'>"));
+            var link = document.QuerySelector<IHtmlLinkElement>("link");
+            var finished = await receivedRequest.Task;
+
+            Assert.AreEqual("import", link.Relation);
+            Assert.IsNotNull(link.Import);
+            Assert.AreEqual("foo", link.Import.QuerySelector("div").TextContent);
+        }
     }
 }
