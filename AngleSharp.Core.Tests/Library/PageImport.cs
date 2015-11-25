1a4ce28734fe04b86f22a99f947c9093138f624b
Florian Rappl <rappl.florian@gmail.com>
2015-11-26 00:47:30 +0100

    Cycle tests for page imports #179

--- AngleSharp.Core.Tests/Library/PageImport.cs
+++ AngleSharp.Core.Tests/Library/PageImport.cs
@@ -5,6 +5,8 @@
     using AngleSharp.Extensions;
     using NUnit.Framework;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     [TestFixture]
@@ -48,6 +50,45 @@
             Assert.AreEqual("import", link.Relation);
             Assert.IsNotNull(link.Import);
             Assert.AreEqual("foo", link.Import.QuerySelector("div").TextContent);
+        }
+
+        [Test]
+        public async Task ImportPageWithDirectCycle()
+        {
+            var content = "<!doctype html><link rel=import href=http://example.com/test.html>";
+            var requester = new MockRequester();
+            var requestCount = 0;
+            requester.OnRequest = request => requestCount++;
+            requester.BuildResponse = request => content;
+            var config = Configuration.Default.WithDefaultLoader(setup => setup.IsResourceLoadingEnabled = true, new[] { requester });
+            var document = await BrowsingContext.New(config).OpenAsync(m => m.Content(content));
+            var link = document.QuerySelector<IHtmlLinkElement>("link");
+            Assert.AreEqual("import", link.Relation);
+            Assert.IsNotNull(link.Import);
+            Assert.AreEqual(1, requestCount);
+        }
+
+        [Test]
+        public async Task ImportPageWithIndirectCycle()
+        {
+            var content = "<!doctype html><link rel=import href=http://example.com/test.html>";
+            var nested = new Queue<String>(new [] 
+            {
+                "<!doctype html><link rel=import href=http://example.com/foo.html>",
+                "<!doctype html><link rel=import href=http://example.com/bar.html>",
+                "<!doctype html><link rel=import href=http://example.com/test.html>",
+                "<!doctype html><link rel=import href=http://example.com/foo.html>"
+            });
+            var requester = new MockRequester();
+            var requestCount = 0;
+            requester.OnRequest = request => requestCount++;
+            requester.BuildResponse = request => nested.Dequeue();
+            var config = Configuration.Default.WithDefaultLoader(setup => setup.IsResourceLoadingEnabled = true, new[] { requester });
+            var document = await BrowsingContext.New(config).OpenAsync(m => m.Content(content));
+            var link = document.QuerySelector<IHtmlLinkElement>("link");
+            Assert.AreEqual("import", link.Relation);
+            Assert.IsNotNull(link.Import);
+            Assert.AreEqual(3, requestCount);
         }
     }
 }
