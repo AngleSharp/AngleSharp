aec7e019074e19ae8797837cb73c09023b0eb57b
Florian Rappl <rappl.florian@gmail.com>
2015-12-02 15:54:03 +0100

    Started changing event loop

--- AngleSharp/Foundation/TaskEventLoop.cs
+++ AngleSharp/Foundation/TaskEventLoop.cs
@@ -1,30 +1,143 @@
 ﻿namespace AngleSharp
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
 
     sealed class TaskEventLoop : IEventLoop
     {
-        Task _current;
+        readonly Dictionary<TaskPriority, Queue<TaskEventLoopEntry>> _queues;
+        TaskEventLoopEntry _current;
 
         public TaskEventLoop()
         {
-            _current = TaskEx.FromResult(false);
+            _queues = new Dictionary<TaskPriority, Queue<TaskEventLoopEntry>>();
+            _current = null;
         }
 
-        public Task Enqueue(Action task)
+        public IEventLoopEntry Enqueue(Action<CancellationToken> task, TaskPriority priority)
         {
-            _current = _current.ContinueWith(_ => task());
-            return _current;
+            var entry = new TaskEventLoopEntry(task);
+
+            lock (this)
+            {
+                var entries = default(Queue<TaskEventLoopEntry>);
+
+                if (!_queues.TryGetValue(priority, out entries))
+                {
+                    entries = new Queue<TaskEventLoopEntry>();
+                    _queues.Add(priority, entries);
+                }
+
+                entries.Enqueue(entry);
+            }
+
+            return entry;
         }
 
-        public Task Execute(Action microtask)
+        public void Spin()
         {
-            return Enqueue(microtask);
+            lock (this)
+            {
+                if (_current != null && _current.IsRunning)
+                {
+                    return;
+                }
+
+                _current = Dequeue(TaskPriority.Critical) ?? 
+                           Dequeue(TaskPriority.Microtask) ?? 
+                           Dequeue(TaskPriority.Normal) ?? 
+                           Dequeue(TaskPriority.None);
+            }
+
+            if (_current != null)
+            {
+                _current.Run();
+            }
+        }
+
+        TaskEventLoopEntry Dequeue(TaskPriority priority)
+        {
+            if (_queues.ContainsKey(priority) && _queues[priority].Count != 0)
+            {
+                return _queues[priority].Dequeue();
+            }
+
+            return null;
         }
 
         public void Shutdown()
         {
+            lock (this)
+            {
+                foreach (var queue in _queues)
+                {
+                    var entries = queue.Value;
+
+                    foreach (var entry in entries)
+                    {
+                        entry.Cancel();
+                    }
+
+                    entries.Clear();
+                }
+
+                _queues.Clear();
+            }
+        }
+
+        sealed class TaskEventLoopEntry : IEventLoopEntry
+        {
+            readonly Task _task;
+            readonly CancellationTokenSource _cts;
+            
+            Boolean _started;
+            DateTime _created;
+
+            public TaskEventLoopEntry(Action<CancellationToken> action)
+            {
+                _cts = new CancellationTokenSource();
+                _task = new Task(() => action(_cts.Token), _cts.Token);
+            }
+
+            public Boolean IsRunning
+            {
+                get 
+                { 
+                    return _task.Status == TaskStatus.Running || 
+                           _task.Status == TaskStatus.WaitingForActivation || 
+                           _task.Status == TaskStatus.WaitingToRun; 
+                }
+            }
+
+            public void Run()
+            {
+                if (!_started)
+                {
+                    _created = DateTime.Now;
+                    _task.Start();
+                    _started = true;
+                }
+            }
+
+            public void Cancel()
+            {
+                _cts.Cancel();
+            }
+
+            public DateTime? Started
+            {
+                get 
+                {
+                    if (!IsRunning)
+                    {
+                        return null;
+                    }
+
+                    return _created;
+                }
+            }
         }
     }
 }
