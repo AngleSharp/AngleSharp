71d979e9ec2a55bfe182d910d94ffa8dfe956e16
Florian Rappl <rappl.florian@gmail.com>
2015-12-04 16:19:59 +0100

    Added common loader scenario

--- AngleSharp/Network/Default/DocumentLoader.cs
+++ AngleSharp/Network/Default/DocumentLoader.cs
@@ -1,42 +1,31 @@
 ﻿namespace AngleSharp.Network.Default
 {
-    using AngleSharp.Extensions;
     using System;
     using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// Represents the default document loader. This class can be inherited.
     /// </summary>
-    public class DocumentLoader : IDocumentLoader
+    public class DocumentLoader : BaseLoader, IDocumentLoader
     {
-        readonly IEnumerable<IRequester> _requesters;
-        readonly IBrowsingContext _context;
-        readonly Predicate<IRequest> _filter;
-
         /// <summary>
         /// Creates a new document loader.
         /// </summary>
         /// <param name="requesters">The requesters to use.</param>
-        /// <param name="context">The context for the document loader.</param>
+        /// <param name="configuration">The configuration to use..</param>
         /// <param name="filter">The optional request filter to use.</param>
-        public DocumentLoader(IEnumerable<IRequester> requesters, IBrowsingContext context, Predicate<IRequest> filter = null)
+        public DocumentLoader(IEnumerable<IRequester> requesters, IConfiguration configuration, Predicate<IRequest> filter = null)
+            : base(requesters, configuration, filter)
         {
-            _requesters = requesters;
-            _context = context;
-            _filter = filter ?? (_ => true);
         }
 
         /// <summary>
         /// Loads the data for the request asynchronously.
         /// </summary>
         /// <param name="request">The issued request.</param>
-        /// <param name="cancel">The cancellation token.</param>
-        /// <returns>The task creating the response.</returns>
-        public virtual Task<IResponse> LoadAsync(DocumentRequest request, CancellationToken cancel)
+        /// <returns>The active download.</returns>
+        public virtual IDownload DownloadAsync(DocumentRequest request)
         {
-            var events = _context.Configuration.Events;
             var data = new Request
             {
                 Address = request.Target,
@@ -45,14 +34,18 @@
             };
 
             foreach (var header in request.Headers)
+            {
                 data.Headers[header.Key] = header.Value;
+            }
 
-            var cookie = _context.Configuration.GetCookie(request.Target.Origin);
+            var cookie = GetCookie(request.Target);
 
             if (cookie != null)
+            {
                 data.Headers[HeaderNames.Cookie] = cookie;
+            }
 
-            return _filter(data) ? _requesters.LoadAsync(data, events, cancel) : TaskEx.FromResult(default(IResponse));
+            return DownloadAsync(data, request.Source);
         }
     }
 }
