55178ea26e30d73c046ce21bd218660e6def882e
Florian Rappl <rappl.florian@gmail.com>
2015-12-21 22:59:09 +0100

    Fixed mime-type detection

--- AngleSharp/Extensions/ResponseExtensions.cs
+++ AngleSharp/Extensions/ResponseExtensions.cs
@@ -19,7 +19,7 @@
             var fileName = response.Address.Path;
             var index = fileName.LastIndexOf('.');
             var extension = index >= 0 ? fileName.Substring(index) : ".a";
-            var defaultType = MimeTypes.FromExtension(MimeTypes.Binary);
+            var defaultType = MimeTypes.FromExtension(extension);
             return response.GetContentType(defaultType);
         }
 
