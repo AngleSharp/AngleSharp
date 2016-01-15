7cbf779c112776aa05a4d72fe32aa192b0aa386d
Florian Rappl <rappl.florian@gmail.com>
2016-01-15 20:29:55 +0100

    Added more useful extensions

--- AngleSharp/Extensions/StyleSheetExtensions.cs
+++ AngleSharp/Extensions/StyleSheetExtensions.cs
@@ -15,6 +15,96 @@
     [DebuggerStepThrough]
     public static class StyleSheetExtensions
     {
+        #region Adding Rules
+
+        static readonly Dictionary<String, CssRuleType> RuleMapping = new Dictionary<String, CssRuleType>
+        {
+            { typeof(ICssCharsetRule).FullName, CssRuleType.Charset },
+            { typeof(ICssCounterStyleRule).FullName, CssRuleType.CounterStyle },
+            { typeof(ICssDocumentRule).FullName, CssRuleType.Document },
+            { typeof(ICssFontFaceRule).FullName, CssRuleType.FontFace },
+            { typeof(ICssFontFeatureValuesRule).FullName, CssRuleType.FontFeatureValues },
+            { typeof(ICssImportRule).FullName, CssRuleType.Import },
+            { typeof(ICssKeyframeRule).FullName, CssRuleType.Keyframe },
+            { typeof(ICssKeyframesRule).FullName, CssRuleType.Keyframes },
+            { typeof(ICssMediaRule).FullName, CssRuleType.Media },
+            { typeof(ICssNamespaceRule).FullName, CssRuleType.Namespace },
+            { typeof(ICssPageRule).FullName, CssRuleType.Page },
+            { typeof(ICssStyleRule).FullName, CssRuleType.Style },
+            { typeof(ICssSupportsRule).FullName, CssRuleType.Supports },
+        };
+
+        /// <summary>
+        /// Creates a new CSS rule and appends it to the current node.
+        /// </summary>
+        /// <typeparam name="TRule">The type of rule to create.</typeparam>
+        /// <param name="creator">The host of the rule.</param>
+        /// <returns>The created rule.</returns>
+        public static TRule AddNewRule<TRule>(this ICssRuleCreator creator)
+            where TRule : ICssRule
+        {
+            var ruleFullName = typeof(TRule).FullName;
+            var ruleType = CssRuleType.Unknown;
+            
+            if (RuleMapping.TryGetValue(ruleFullName, out ruleType))
+            {
+                var rule = creator.AddNewRule(ruleType);
+
+                if (rule is TRule)
+                {
+                    return (TRule)rule;
+                }
+            }
+
+            return default(TRule);
+        }
+
+        /// <summary>
+        /// Creates a new CSS style rule and appends it to the current node. The
+        /// given selector and declarations are set in the beginning.
+        /// </summary>
+        /// <param name="creator">The host of the rule.</param>
+        /// <param name="selector">The selector to use, if any.</param>
+        /// <param name="declarations">The declarations to set, if any.</param>
+        /// <returns>The created style rule.</returns>
+        public static ICssStyleRule AddNewStyle(this ICssRuleCreator creator, String selector = null, IDictionary<String, String> declarations = null)
+        {
+            var rule = creator.AddNewRule<ICssStyleRule>();
+
+            if (!String.IsNullOrEmpty(selector))
+            {
+                rule.SelectorText = selector;
+            }
+
+            if (declarations != null)
+            {
+                foreach (var declaration in declarations)
+                {
+                    rule.Style.SetProperty(declaration.Key, declaration.Value);
+                }
+            }
+
+            return rule;
+        }
+
+        /// <summary>
+        /// Creates a new CSS style rule and appends it to the current node.
+        /// The given selector and declarations from an anonymous object are set
+        /// in the beginning.
+        /// </summary>
+        /// <param name="creator">The host of the rule.</param>
+        /// <param name="selector">The selector to use.</param>
+        /// <param name="declarations">The declarations to set.</param>
+        /// <returns>The created style rule.</returns>
+        public static ICssStyleRule AddNewStyle(this ICssRuleCreator creator, String selector, Object declarations)
+        {
+            return creator.AddNewStyle(selector, declarations.ToDictionary());
+        }
+
+        #endregion
+
+        #region Obtaining Rules
+
         /// <summary>
         /// Gets all rules that are of the provided type.
         /// </summary>
@@ -108,6 +198,10 @@
             }
         }
 
+        #endregion
+
+        #region Linked Stylesheet States
+
         /// <summary>
         /// Gets if the link contains a stylesheet that is regarded persistent.
         /// </summary>
@@ -138,5 +232,7 @@
             var relation = link.RelationList;
             return relation.Contains(LinkRelNames.StyleSheet) && relation.Contains(LinkRelNames.Alternate) && link.Title != null;
         }
+
+        #endregion
     }
 }
