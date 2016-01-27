fb3075bfb09d8f19f0154db289e4ad2ee13a6c7a
Florian Rappl <rappl.florian@gmail.com>
2016-01-27 17:18:44 +0100

    Improved helper

--- AngleSharp.Core.Tests/Helper.cs
+++ AngleSharp.Core.Tests/Helper.cs
@@ -22,7 +22,9 @@
         public static Boolean IsNetworkAvailable()
         {
             if (IsNetworkAvailable(0))
+            {
                 return true;
+            }
 
             Assert.Inconclusive("No network has been detected. Test skipped.");
             return false;
@@ -36,32 +38,32 @@
         /// <returns>True if a network connection is available; otherwise false.</returns>
         public static Boolean IsNetworkAvailable(Int64 minimumSpeed)
         {
-            if (!NetworkInterface.GetIsNetworkAvailable())
-                return false;
-
-            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            if (NetworkInterface.GetIsNetworkAvailable())
             {
-                // discard because of standard reasons
-                if ((ni.OperationalStatus != OperationalStatus.Up) ||
-                    (ni.NetworkInterfaceType == NetworkInterfaceType.Loopback) ||
-                    (ni.NetworkInterfaceType == NetworkInterfaceType.Tunnel))
-                    continue;
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    // discard because of standard reasons
+                    if ((ni.OperationalStatus != OperationalStatus.Up) ||
+                        (ni.NetworkInterfaceType == NetworkInterfaceType.Loopback) ||
+                        (ni.NetworkInterfaceType == NetworkInterfaceType.Tunnel))
+                        continue;
 
-                // this allow to filter modems, serial, etc.
-                // I use 10000000 as a minimum speed for most cases
-                if (ni.Speed < minimumSpeed)
-                    continue;
+                    // this allow to filter modems, serial, etc.
+                    // I use 10000000 as a minimum speed for most cases
+                    if (ni.Speed < minimumSpeed)
+                        continue;
 
-                // discard virtual cards (virtual box, virtual pc, etc.)
-                if ((ni.Description.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0) ||
-                    (ni.Name.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0))
-                    continue;
+                    // discard virtual cards (virtual box, virtual pc, etc.)
+                    if ((ni.Description.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                        (ni.Name.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0))
+                        continue;
 
-                // discard "Microsoft Loopback Adapter", it will not show as NetworkInterfaceType.Loopback but as Ethernet Card.
-                if (ni.Description.Equals("Microsoft Loopback Adapter", StringComparison.OrdinalIgnoreCase))
-                    continue;
+                    // discard "Microsoft Loopback Adapter", it will not show as NetworkInterfaceType.Loopback but as Ethernet Card.
+                    if (ni.Description.Equals("Microsoft Loopback Adapter", StringComparison.OrdinalIgnoreCase))
+                        continue;
 
-                return true;
+                    return true;
+                }
             }
 
             return false;
