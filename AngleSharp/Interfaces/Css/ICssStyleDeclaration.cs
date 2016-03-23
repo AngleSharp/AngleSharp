c6ead4d39e1f8263434ff0b887511677512366a0
Florian Rappl <rappl.florian@gmail.com>
2016-03-23 14:57:46 +0100

    Created the interface for declarations #297

--- AngleSharp/Interfaces/Css/ICssStyleDeclaration.cs
+++ AngleSharp/Interfaces/Css/ICssStyleDeclaration.cs
@@ -9,7 +9,7 @@
     /// this underlying state depends upon the source of the CSSStyleDeclaration instance.
     /// </summary>
     [DomName("CSSStyleDeclaration")]
-    public interface ICssStyleDeclaration : IEnumerable<ICssProperty>, ICssNode
+    public interface ICssStyleDeclaration : ICssProperties, ICssNode
     {
         #region API
 
@@ -23,57 +23,10 @@
         String this[Int32 index] { get; }
 
         /// <summary>
-        /// Gets the value of the property with the given name.
-        /// </summary>
-        /// <param name="propertyName">The name of the property to get.</param>
-        /// <returns>The value of the property.</returns>
-        String this[String propertyName] { get; }
-
-        /// <summary>
         /// Gets or sets the textual representation of the declaration block.
         /// </summary>
         [DomName("cssText")]
         String CssText { get; set; }
-
-        /// <summary>
-        /// Gets the number of properties in the declaration.
-        /// </summary>
-        [DomName("length")]
-        Int32 Length { get; }
-
-        /// <summary>
-        /// Gets the value of a property with the given name.
-        /// </summary>
-        /// <param name="propertyName">The name of the property to get the value of.</param>
-        /// <returns>A string or null if nothing has been set.</returns>
-        [DomName("getPropertyValue")]
-        String GetPropertyValue(String propertyName);
-
-        /// <summary>
-        /// Returns the optional priority, "important" or null, if no priority has been set.
-        /// </summary>
-        /// <param name="propertyName">The name of the property to get the priority of.</param>
-        /// <returns>A priority or null.</returns>
-        [DomName("getPropertyPriority")]
-        String GetPropertyPriority(String propertyName);
-
-        /// <summary>
-        /// Sets a property with the given name and value. Optionally the priority can be
-        /// set to "important" or left empty.
-        /// </summary>
-        /// <param name="propertyName">The property's name.</param>
-        /// <param name="propertyValue">The value of the property.</param>
-        /// <param name="priority">The optional priority.</param>
-        [DomName("setProperty")]
-        void SetProperty(String propertyName, String propertyValue, String priority = null);
-
-        /// <summary>
-        /// Removes the property with the given name and returns its value.
-        /// </summary>
-        /// <param name="propertyName">The name of the property to be removed.</param>
-        /// <returns>The value of the deleted property.</returns>
-        [DomName("removeProperty")]
-        String RemoveProperty(String propertyName);
 
         /// <summary>
         /// Gets the containing rule.
