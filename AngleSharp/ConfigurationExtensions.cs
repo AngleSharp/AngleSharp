2740460d55e0871cce6365fc4e28a6ad57bfc4d6
Florian Rappl <rappl.florian@gmail.com>
2016-03-31 18:22:10 +0200

    Removed the default event aggregator

--- AngleSharp/ConfigurationExtensions.cs
+++ AngleSharp/ConfigurationExtensions.cs
@@ -2,7 +2,6 @@
 {
     using AngleSharp.Dom.Css;
     using AngleSharp.Events;
-    using AngleSharp.Events.Default;
     using AngleSharp.Extensions;
     using AngleSharp.Network;
     using AngleSharp.Network.Default;
@@ -93,30 +92,6 @@
             }
 
             return new Configuration(configuration.Services, events, configuration.Culture);
-        }
-
-        /// <summary>
-        /// Uses the currently active event aggregator or integrates a simple
-        /// default aggregator to add the subscriber.
-        /// </summary>
-        /// <typeparam name="T">The type of event to handle.</typeparam>
-        /// <param name="configuration">The configuration to extend.</param>
-        /// <param name="subscriber">The subscriber to add.</param>
-        /// <returns>The current instance, or a new instance.</returns>
-        public static IConfiguration SetHandler<T>(this IConfiguration configuration, ISubscriber<T> subscriber)
-        {
-            if (configuration == null)
-            {
-                throw new ArgumentNullException("configuration");
-            }
-
-            if (configuration.Events == null)
-            {
-                configuration = configuration.SetEvents(new SimpleEventAggregator());
-            }
-
-            configuration.Events.Subscribe(subscriber);
-            return configuration;
         }
 
         #endregion
