9c54ec5edb1b45c6121891982d7f621dfbcf3c0b
Florian Rappl <rappl.florian@gmail.com>
2016-05-09 09:19:43 +0200

    Removed unused functionality, made public

--- src/AngleSharp/Html/NamespaceNames.cs
+++ src/AngleSharp/Html/NamespaceNames.cs
@@ -1,12 +1,11 @@
 ﻿namespace AngleSharp.Html
 {
     using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Contains a list of common namespaces.
     /// </summary>
-    static class NamespaceNames
+    public static class NamespaceNames
     {
         /// <summary>
         /// Gets the namespace for HTML elements.
@@ -39,11 +38,6 @@
         public static readonly String MathMlUri = "http://www.w3.org/1998/Math/MathML";
 
         /// <summary>
-        /// Gets the prefix for HTML elements.
-        /// </summary>
-        public static readonly String HtmlPrefix = "html";
-
-        /// <summary>
         /// Gets the prefix for XMLNS elements.
         /// </summary>
         public static readonly String XmlNsPrefix = "xmlns";
@@ -57,52 +51,5 @@
         /// Gets the prefix for XML elements.
         /// </summary>
         public static readonly String XmlPrefix = "xml";
-
-        /// <summary>
-        /// Gets the prefix for SVG elements.
-        /// </summary>
-        public static readonly String SvgPrefix = "svg";
-
-        /// <summary>
-        /// Gets the prefix for MathML elements.
-        /// </summary>
-        public static readonly String MathMlPrefix = "mathml";
-
-        static readonly Dictionary<String, String> namespaces = new Dictionary<String, String>
-        {
-            { HtmlPrefix, HtmlUri },
-            { XLinkPrefix, XLinkUri },
-            { XmlPrefix, XmlUri },
-            { XmlNsPrefix, XmlNsUri },
-            { SvgPrefix, SvgUri },
-            { MathMlPrefix, MathMlUri }
-        };
-
-        /// <summary>
-        /// Gets the declaration for the given prefix.
-        /// </summary>
-        /// <param name="prefix">The prefix or null for the default namespace.</param>
-        /// <returns>The name for the declaration attribute.</returns>
-        public static String DeclarationFor(String prefix)
-        {
-            if (String.IsNullOrEmpty(prefix))
-            {
-                return XmlNsPrefix;
-            }
-
-            return String.Concat(XmlNsPrefix, ":", prefix);
-        }
-
-        /// <summary>
-        /// Gets the namespace URI for the given prefix.
-        /// </summary>
-        /// <param name="prefix">The prefix to look for.</param>
-        /// <returns>The namespace URI if any, otherwise NULL.</returns>
-        public static String LookupNamespaceUri(String prefix)
-        {
-            var namespaceUri = default(String);
-            namespaces.TryGetValue(prefix, out namespaceUri);
-            return namespaceUri;
-        }
     }
 }
