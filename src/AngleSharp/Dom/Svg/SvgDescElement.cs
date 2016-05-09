1ce0e80cf196776ea7a6bb0c2c3531a7b736bed1
Florian Rappl <rappl.florian@gmail.com>
2016-05-09 11:15:45 +0200

    Added missing interface implementations

--- src/AngleSharp/Dom/Svg/SvgDescElement.cs
+++ src/AngleSharp/Dom/Svg/SvgDescElement.cs
@@ -6,7 +6,7 @@
     /// <summary>
     /// Represents the desc element of the SVG DOM.
     /// </summary>
-    sealed class SvgDescElement : SvgElement
+    sealed class SvgDescElement : SvgElement, ISvgDescriptionElement
     {
         public SvgDescElement(Document owner, String prefix = null)
             : base(owner, TagNames.Desc, prefix, NodeFlags.HtmlTip | NodeFlags.Special | NodeFlags.Scoped)
