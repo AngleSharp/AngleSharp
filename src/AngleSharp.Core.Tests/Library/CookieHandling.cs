085326181529cfd1b9ddb967f2648ee6bc861fef
Florian Rappl <rappl.florian@gmail.com>
2016-05-13 15:12:54 +0200

    More tests to verify optimum cookie behavior

--- src/AngleSharp.Core.Tests/Library/CookieHandling.cs
+++ src/AngleSharp.Core.Tests/Library/CookieHandling.cs
@@ -18,6 +18,13 @@
         }
 
         [Test]
+        public async Task SettingTwoSimpleCookiesInRequestAppearsInDocument()
+        {
+            var cookie = await LoadDocumentWithCookie("UserID=Foo, Auth=bar");
+            Assert.AreEqual("UserID=Foo; Auth=bar", cookie);
+        }
+
+        [Test]
         public async Task SettingSingleCookieWithMaxAgeInRequestAppearsInDocument()
         {
             var cookie = await LoadDocumentWithCookie("cookie=two; Max-Age=36001");
@@ -25,10 +32,24 @@
         }
 
         [Test]
-        public async Task SettingSingleExpiredCookieInRequestAppearsInDocument()
+        public async Task SettingSingleExpiredCookieInRequestDoesNotAppearInDocument()
         {
             var cookie = await LoadDocumentWithCookie("cookie=expiring; Expires=Tue, 10 Nov 2009 23:00:00 GMT");
             Assert.AreEqual("", cookie);
+        }
+
+        [Test]
+        public async Task SettingMultipleExpiredCookieInRequestDoNotAppearInDocument()
+        {
+            var cookie = await LoadDocumentWithCookie("cookie=expiring; Expires=Tue, 10 Nov 2009 23:00:00 GMT, foo=bar; Expires=Tue, 10 Nov 2009 23:00:00 GMT");
+            Assert.AreEqual("", cookie);
+        }
+
+        [Test]
+        public async Task SettingOneExpiredCookieAndAFutureCookieInRequestDoAppearInDocument()
+        {
+            var cookie = await LoadDocumentWithCookie("cookie=expiring; Expires=Tue, 10 Nov 2009 23:00:00 GMT, foo=bar; Expires=Tue, 28 Jan 2025 13:37:00 GMT");
+            Assert.AreEqual("foo=bar", cookie);
         }
 
         [Test]
@@ -89,19 +110,47 @@
         }
 
         [Test]
+        public async Task SettingOneCookiesInOneRequestAppearsInDocument()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var url = "https://httpbin.org/cookies/set?k1=v1";
+                var config = Configuration.Default.WithCookies().WithDefaultLoader();
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(url);
+
+                Assert.AreEqual("k1=v1", document.Cookie);
+            }
+        }
+
+        [Test]
         public async Task SettingTwoCookiesInOneRequestAppearsInDocument()
         {
-            var config = Configuration.Default.WithCookies();
-            var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(res =>
-                res.Content("<div></div>").
-                    Address("http://localhost/").
-                    Header(HeaderNames.SetCookie, "UserID=Foo"));
+            if (Helper.IsNetworkAvailable())
+            {
+                var url = "https://httpbin.org/cookies/set?k2=v2&k1=v1";
+                var config = Configuration.Default.WithCookies().WithDefaultLoader();
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(url);
 
-            Assert.IsNotNull(document.QuerySelector("div"));
-            Assert.AreEqual("UserID=Foo", document.Cookie);
+                Assert.AreEqual("k2=v2; k1=v1", document.Cookie);
+            }
         }
-        
+
+        [Test]
+        public async Task SettingThreeCookiesInOneRequestAppearsInDocument()
+        {
+            if (Helper.IsNetworkAvailable())
+            {
+                var url = "https://httpbin.org/cookies/set?test=baz&k2=v2&k1=v1&foo=bar";
+                var config = Configuration.Default.WithCookies().WithDefaultLoader();
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(url);
+
+                Assert.AreEqual("test=baz; k2=v2; k1=v1; foo=bar", document.Cookie);
+            }
+        }
+
         static async Task<String> LoadDocumentWithCookie(String cookieValue)
         {
             var config = Configuration.Default.WithCookies();
