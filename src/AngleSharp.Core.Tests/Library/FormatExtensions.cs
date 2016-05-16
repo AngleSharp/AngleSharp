a92209cd353eacbd38e239a3341f6f46931d068c
Adrian Phinney <adrian.phinney@gmail.com>
2016-05-16 10:30:50 -0400

    Add tests (including failing test case for 'ToHtml(TextWriter)').

---
+++ src/AngleSharp.Core.Tests/Library/FormatExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Extensions;
+using AngleSharp.Dom;
+using NUnit.Framework;
+using AngleSharp.Html;
+using System.IO;
+
+namespace AngleSharp.Core.Tests.Library
+{
+    [TestFixture]
+    public class FormatExtensions
+    {
+        static IDocument Html(String code)
+        {
+            var config = new Configuration().WithCss();
+            return code.ToHtmlDocument(config);
+        }
+
+        [Test]
+        public void ExtensionToHtml()
+        {
+            var document = Html("<!DOCTYPE html><html><head></head><body></body></html>");
+
+            var html = document.ToHtml();
+
+            Assert.AreEqual("<!DOCTYPE html><html><head></head><body></body></html>", html);
+        }
+
+        [Test]
+        public void ExtensionToHtmlWithFormatter()
+        {
+            var document = Html("<!DOCTYPE html><html><head></head><body></body></html>");
+
+            var html = document.ToHtml(HtmlMarkupFormatter.Instance);
+
+            Assert.AreEqual("<!DOCTYPE html><html><head></head><body></body></html>", html);
+        }
+
+        [Test]
+        public void ExtensionToHtmlWithTextWriter()
+        {
+            var builder = new StringBuilder();
+            using (var writer = new StringWriter(builder))
+            {
+                var document = Html("<!DOCTYPE html><html><head></head><body></body></html>");
+
+                document.ToHtml(writer);
+
+                Assert.AreEqual("<!DOCTYPE html><html><head></head><body></body></html>", builder.ToString());
+            }
+        }
+    }
+}
