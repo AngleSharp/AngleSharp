753589bcee6cdee1169f330859606e9d41f94af6
Florian Rappl <rappl.florian@gmail.com>
2016-07-21 11:26:08 +0200

    Included integrity provider #308

---
+++ src/AngleSharp/Services/IIntegrityProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace AngleSharp.Services
+{
+    /// <summary>
+    /// Defines the methods to perform an integrity check.
+    /// </summary>
+    public interface IIntegrityProvider
+    {
+    }
+}
