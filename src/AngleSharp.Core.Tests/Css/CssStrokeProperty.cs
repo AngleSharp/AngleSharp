ad23079b773f927f3031d0374429f3a2954e79ed
Jakub Swietek <FTWB36@motorolasolutions.com>
2016-09-23 10:56:41 +0200

    Added unit tests

--- src/AngleSharp.Core.Tests/Css/CssStrokeProperty.cs
+++ src/AngleSharp.Core.Tests/Css/CssStrokeProperty.cs
@@ -108,5 +108,62 @@
 			Assert.IsTrue(concrete.HasValue);
 			Assert.AreEqual("url(\"#linear\")", concrete.Value);
 		}
+
+
+		[Test]
+		public void CssStrokeDasharrayNumberNumberLegal()
+		{
+			var snippet = "stroke-dasharray: 5 5";
+			var property = ParseDeclaration(snippet);
+			Assert.AreEqual("stroke-dasharray", property.Name);
+			Assert.IsFalse(property.IsImportant);
+			Assert.IsInstanceOf<CssStrokeDasharrayProperty>(property);
+			var concrete = (CssStrokeDasharrayProperty)property;
+			Assert.IsFalse(concrete.IsInherited);
+			Assert.IsTrue(concrete.HasValue);
+			Assert.AreEqual("5 5", concrete.Value);
+		}
+
+		[Test]
+		public void CssStrokeDasharrayLengthLengthLegal()
+		{
+			var snippet = "stroke-dasharray: 5px 5em";
+			var property = ParseDeclaration(snippet);
+			Assert.AreEqual("stroke-dasharray", property.Name);
+			Assert.IsFalse(property.IsImportant);
+			Assert.IsInstanceOf<CssStrokeDasharrayProperty>(property);
+			var concrete = (CssStrokeDasharrayProperty)property;
+			Assert.IsFalse(concrete.IsInherited);
+			Assert.IsTrue(concrete.HasValue);
+			Assert.AreEqual("5px 5em", concrete.Value);
+		}
+
+		[Test]
+		public void CssStrokeDasharrayManyLegal()
+		{
+			var snippet = "stroke-dasharray: 1px 2em 3vh 4vw 5 6";
+			var property = ParseDeclaration(snippet);
+			Assert.AreEqual("stroke-dasharray", property.Name);
+			Assert.IsFalse(property.IsImportant);
+			Assert.IsInstanceOf<CssStrokeDasharrayProperty>(property);
+			var concrete = (CssStrokeDasharrayProperty)property;
+			Assert.IsFalse(concrete.IsInherited);
+			Assert.IsTrue(concrete.HasValue);
+			Assert.AreEqual("1px 2em 3vh 4vw 5 6", concrete.Value);
+		}
+
+		[Test]
+		public void CssStrokeDasharrayNoneLegal()
+		{
+			var snippet = "stroke-dasharray: none";
+			var property = ParseDeclaration(snippet);
+			Assert.AreEqual("stroke-dasharray", property.Name);
+			Assert.IsFalse(property.IsImportant);
+			Assert.IsInstanceOf<CssStrokeDasharrayProperty>(property);
+			var concrete = (CssStrokeDasharrayProperty)property;
+			Assert.IsFalse(concrete.IsInherited);
+			Assert.IsTrue(concrete.HasValue);
+			Assert.AreEqual("none", concrete.Value);
+		}
 	}
 }
