ab414daee70ce4853c9938d0b2ea0c5b6b571a54
Jakub Swietek <FTWB36@motorolasolutions.com>
2016-09-23 14:17:57 +0200

    Added stroke-linecap css property

---
+++ src/AngleSharp/Dom/Css/Properties/Stroke/CssStrokeLinecapProperty.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharp.Dom.Css
+{
+	using AngleSharp.Css;
+
+	internal class CssStrokeLinecapProperty : CssProperty
+	{
+		#region Fields
+
+		static readonly IValueConverter StyleConverter = Converters.StrokeLinecapConverter;
+
+		#endregion
+
+		#region ctor
+
+		public CssStrokeLinecapProperty()
+			: base(PropertyNames.StrokeLinecap, PropertyFlags.Animatable)
+		{
+		}
+
+		#endregion
+
+		#region Properties;
+
+		internal override IValueConverter Converter
+		{
+			get
+			{
+				return StyleConverter;
+			}
+		}
+
+		#endregion
+	}
+}
