67cc72d27aea836fcfa9192177820135405f08e2
Florian Rappl <rappl.florian@gmail.com>
2016-10-28 09:46:07 +0200

    Added more tests

--- src/AngleSharp.Core.Tests/Xml/XmlNamespace.cs
+++ src/AngleSharp.Core.Tests/Xml/XmlNamespace.cs
@@ -26,7 +26,6 @@
             Assert.AreEqual("http://www.w3.org/1999/xhtml", root.NamespaceUri);
         }
 
-
         [Test]
         public async Task XmlWithCustomNamespaceShouldExposeThatNamespaceUri()
         {
@@ -35,6 +34,47 @@
                    .Header("Content-Type", "text/xml"));
             var root = document.DocumentElement;
             Assert.AreEqual("http://initd.org/ns/tesseract-1.0", root.NamespaceUri);
+        }
+
+        [Test]
+        public async Task XmlSubElementsGetTheRightDefaultNamespace()
+        {
+            var document = await BrowsingContext.New().OpenAsync(req =>
+                req.Content(@"<xml xmlns=""http://test.com""><child attr=""1""/></xml>")
+                   .Header("Content-Type", "text/xml"));
+            var root = document.DocumentElement;
+            Assert.AreEqual("http://test.com", root.NamespaceUri);
+            Assert.AreEqual("http://test.com", root.LookupNamespaceUri(""));
+            Assert.AreEqual("http://test.com", root.FirstElementChild.NamespaceUri);
+            Assert.AreEqual("http://test.com", root.FirstElementChild.LookupNamespaceUri(""));
+            Assert.AreEqual(null, root.FirstElementChild.Attributes["attr"].NamespaceUri);
+        }
+
+        [Test]
+        public async Task XmlPrefixRefersToDefinedNamespace()
+        {
+            var document = await BrowsingContext.New().OpenAsync(req =>
+                req.Content(@"<xml xmlns:p1=""http://p1.com"" xmlns:p2=""http://p2.com""><p1:child a=""1"" p1:attr=""1"" b=""2""/><p2:child/></xml>")
+                   .Header("Content-Type", "text/xml"));
+            var root = document.DocumentElement;
+            Assert.AreEqual("http://p1.com", root.FirstElementChild.NamespaceUri);
+            Assert.AreEqual("http://p1.com", root.LookupNamespaceUri("p1"));
+            Assert.AreEqual(null, root.FirstElementChild.GetAttribute("attr"));
+            Assert.AreEqual("http://p1.com", root.FirstElementChild.Attributes["p1:attr"].NamespaceUri);
+            Assert.AreEqual("http://p2.com", root.FirstElementChild.NextElementSibling.NamespaceUri);
+            Assert.AreEqual("http://p2.com", root.FirstElementChild.NextElementSibling.LookupNamespaceUri("p2"));
+        }
+
+        [Test]
+        public async Task XmlRedefinitionOfPrefixedNamespace()
+        {
+            var document = await BrowsingContext.New().OpenAsync(req =>
+                req.Content(@"<xml xmlns:p=""http://test.com""><p:child xmlns:p=""http://p.com""/><p:child/></xml>")
+                   .Header("Content-Type", "text/xml"));
+            var root = document.DocumentElement;
+            Assert.AreEqual("http://p.com", root.FirstElementChild.NamespaceUri);
+            Assert.AreEqual("http://test.com", root.LastElementChild.NamespaceUri);
+            Assert.AreEqual("http://test.com", root.FirstElementChild.NextElementSibling.LookupNamespaceUri("p"));
         }
     }
 }
