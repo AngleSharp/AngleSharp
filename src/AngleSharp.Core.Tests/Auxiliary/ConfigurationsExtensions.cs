de4a0d7f79c5bc188ed13285073f569c297ce93f
Georgios Diamantopoulos <georgiosd@gmail.com>
2017-05-15 17:03:41 +0300

    add test to demonstrate fault with ConfigurationExtensions.Without<TService>

---
+++ src/AngleSharp.Core.Tests/Auxiliary/ConfigurationsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using AngleSharp.Io;
+using NUnit.Framework;
+
+namespace AngleSharp.Core.Tests.Auxiliary
+{
+    [TestFixture]
+    public class ConfigurationsExtensionsTests
+    {
+        [Test]
+        public void WithoutTServiceRemovesItems()
+        {
+            var config = new Configuration(new object[0])
+                .With((IRequester)new DefaultHttpRequester())
+                .Without<IRequester>();
+
+            Assert.AreEqual(0, config.Services.Count());
+        }
+    }
+}
