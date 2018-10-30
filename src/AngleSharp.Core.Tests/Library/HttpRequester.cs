a83c95443991ddb064df55bab870b5bd52695666
Thomas Bolon <tbolon@mb3m.fr>
2018-10-30 21:33:07 +0100

    fix tests on httpbin.org

--- src/AngleSharp.Core.Tests/Library/HttpRequester.cs
+++ src/AngleSharp.Core.Tests/Library/HttpRequester.cs
@@ -150,7 +150,7 @@ namespace AngleSharp.Core.Tests.Library
 
                     var content = stream.ReadToEnd();
                     Assert.IsTrue(content.Length > 0);
-                    Assert.IsTrue(content.Contains("\"data\":\"Hello world\""));
+                    Assert.IsTrue(content.Contains("\"data\": \"Hello world\""));
                 }
             }
         }
@@ -180,7 +180,7 @@ namespace AngleSharp.Core.Tests.Library
 
                     var content = stream.ReadToEnd();
                     Assert.IsTrue(content.Length > 0);
-                    Assert.IsTrue(content.Contains("\"data\":\"PUT THIS THING BACK\""));
+                    Assert.IsTrue(content.Contains("\"data\": \"PUT THIS THING BACK\""));
                 }
             }
         }
@@ -279,7 +279,7 @@ namespace AngleSharp.Core.Tests.Library
 
                     var content = stream.ReadToEnd();
                     Assert.IsTrue(content.Length > 0);
-                    Assert.AreEqual("{\"user-agent\":\"" + agent + "\"}\n", content);
+                    Assert.AreEqual("{\n  \"user-agent\": \"" + agent + "\"\n}\n", content);
                 }
             }
         }
