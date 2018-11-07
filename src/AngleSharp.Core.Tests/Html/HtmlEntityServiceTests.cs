d823fa07d3aeac17e807167220b8b2cdbd504a28
n.ilinykh <n.ilinykh@skbkontur.ru>
2018-11-07 20:19:07 +0700

    Fixed tests.

--- src/AngleSharp.Core.Tests/Html/HtmlEntityServiceTests.cs
+++ src/AngleSharp.Core.Tests/Html/HtmlEntityServiceTests.cs
@@ -21,8 +21,6 @@ namespace AngleSharp.Core.Tests.Html
         [TestCase(0x110000, Description = "First value outside the range of unicode")]
         [TestCase(0x110001, Description = "Second value outside the range of unicode")]
         [TestCase(0x13579A, Description = "Some big positive value which outside the range of unicode")]
-        [TestCase(0x7FFF, Description = "Int16.MaxValue")]
-        [TestCase(0x8000, Description = "(Int16.MaxValue + 1) which overflow Int16, but doesn't overflow Int32")]
         [TestCase(0x7FFFFFFF, Description = "Int32.MaxValue")]
         public void IsInvalidNumber_WithPositiveInvalidCode_ShouldReturnTrue(Int32 code)
         {
@@ -31,7 +29,7 @@ namespace AngleSharp.Core.Tests.Html
 
         [TestCase(0xD800, Description = "First surrogate code")]
         [TestCase(0xD801, Description = "Second surrogate code")]
-        [TestCase(0xDCA86, Description = "Some internal surrogate code")]
+        [TestCase(0xDCA8, Description = "Some internal surrogate code")]
         [TestCase(0xDFFE, Description = "Penultimate surrogate code")]
         [TestCase(0xDFFF, Description = "Last surrogate code")]
         public void IsInvalidNumber_WithSurrogateCode_ShouldReturnTrue(Int32 code)
@@ -42,6 +40,8 @@ namespace AngleSharp.Core.Tests.Html
         [TestCase(0, Description = "Zero")]
         [TestCase(1, Description = "One")]
         [TestCase(0x6, Description = "Some small code before surrogate zone")]
+        [TestCase(0x7FFF, Description = "Int16.MaxValue")]
+        [TestCase(0x8000, Description = "(Int16.MaxValue + 1) which overflow Int16, but doesn't overflow Int32")]
         [TestCase(0xA987, Description = "Some big code before surrogate zone")]
         [TestCase(0xD799, Description = "Last code before surrogate zone")]
         [TestCase(0xE000, Description = "First code after surrogate zone")]
