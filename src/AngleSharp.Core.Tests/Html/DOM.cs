7d6924478c7cca2f44f8fc6aabb4ab9ca6e3d131
Florian Rappl <florian.rappl@smapiot.com>
2019-05-21 23:33:57 +0200

    Test for #763

--- src/AngleSharp.Core.Tests/Html/DOM.cs
+++ src/AngleSharp.Core.Tests/Html/DOM.cs
@@ -689,5 +689,16 @@ namespace AngleSharp.Core.Tests.Html
             var svg = doc.Body.FirstElementChild;
             Assert.AreEqual("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg>", svg.ToHtml());
         }
+
+        [Test]
+        public void IteratingThroughDomShouldNeverFail_Issue763()
+        {
+            var content = Assets.GetManifestResourceString("Html.BrokenMail.txt");
+            var p = new HtmlParser().ParseDocument(content);
+
+            Assert.IsNotNull(p);
+            var img = p.Images.Select(m => m).ToList();
+            Assert.AreEqual(1, img.Count);
+        }
     }
 }
