a1f148ae623118bec1afb428bdcb0b2cce04b5dc
Henry Roeland <hroeland@it-captains.nl>
2022-11-28 20:17:53 +0100

    Build fixes setup dotnet 6.0 and use RunUnitTests as default target.
    - Windows needs dotnet 6.0 for nuke to run.
    - For default build use RunUnitTests i.s.o. Compile.

--- nuke/Build.cs
+++ nuke/Build.cs
@@ -34,7 +34,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.RunUnitTests);
 
     [Nuke.Common.Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
