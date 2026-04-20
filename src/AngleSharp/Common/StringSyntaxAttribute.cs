2e60cddf27532102294c2c4dc2ad715480bde75b
Simon Cropp <simon.cropp@gmail.com>
2026-04-20 11:09:35 +1000

    Update StringSyntaxAttribute.cs

--- src/AngleSharp/Common/StringSyntaxAttribute.cs
+++ src/AngleSharp/Common/StringSyntaxAttribute.cs
@@ -1,30 +1,29 @@
 #if !NET7_0_OR_GREATER
-namespace System.Diagnostics.CodeAnalysis
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property)]
+internal sealed class StringSyntaxAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-    internal sealed class StringSyntaxAttribute : Attribute
+    public const String Regex = nameof(Regex);
+    public const String Uri = nameof(Uri);
+    public const String Json = nameof(Json);
+    public const String Xml = nameof(Xml);
+    public const String CompositeFormat = nameof(CompositeFormat);
+
+    public StringSyntaxAttribute(String syntax)
     {
-        public const String Regex = nameof(Regex);
-        public const String Uri = nameof(Uri);
-        public const String Json = nameof(Json);
-        public const String Xml = nameof(Xml);
-        public const String CompositeFormat = nameof(CompositeFormat);
-
-        public StringSyntaxAttribute(String syntax)
-        {
-            Syntax = syntax;
-            Arguments = Array.Empty<Object?>();
-        }
-
-        public StringSyntaxAttribute(String syntax, params Object?[] arguments)
-        {
-            Syntax = syntax;
-            Arguments = arguments;
-        }
-
-        public String Syntax { get; }
-
-        public Object?[] Arguments { get; }
+        Syntax = syntax;
+        Arguments = Array.Empty<Object?>();
     }
+
+    public StringSyntaxAttribute(String syntax, params Object?[] arguments)
+    {
+        Syntax = syntax;
+        Arguments = arguments;
+    }
+
+    public String Syntax { get; }
+
+    public Object?[] Arguments { get; }
 }
 #endif
